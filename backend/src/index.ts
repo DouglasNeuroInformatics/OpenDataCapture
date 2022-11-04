fbe5084180aaad5b2abf647aa85a682352cb6349
joshunrau <contact@joshuaunrau.com>
2022-11-03 21:22:48 -0400

    can connect to mongo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,33 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
 
 import router from './router';
+
+async function main() {
+  const client = new MongoClient('mongodb://mongo:27017');
+  try {
+    await client.connect();
+    console.log('Success!');
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+
+  
+}
+
+main();
+
+/*
+
+const database = client.db('development');
+const patients = database.collection('patients');
+
+patients.insertOne({
+  firstName: 'John'
+});
+*/
 
 const app = express();
 const port = 3000;
