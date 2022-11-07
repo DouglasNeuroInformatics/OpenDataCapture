6aafbbabb0a68815bbbfb3e280ac3390073a737a
joshunrau <contact@joshuaunrau.com>
2022-11-07 12:48:08 -0500

    add default error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import { errorRequestHandler } from './controllers/error-controllers';
+import HttpError from './models/HttpError';
 import patientRoutes from './routes/patient-routes';
 
 const app = express();
@@ -10,6 +11,11 @@ const port = 3000;
 
 app.use(bodyParser.json());
 app.use('/api/patient', patientRoutes);
+
+app.use(() => {
+  throw new HttpError(404, 'Could not find route');
+});
+
 app.use(errorRequestHandler);
 
 app.listen(port, () => {
