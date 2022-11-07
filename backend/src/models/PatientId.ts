8c3edb4ed04977135db1e6ac66df27a4bfbc78ee
joshunrau <contact@joshuaunrau.com>
2022-11-07 15:40:44 -0500

    add transliteration

---
+++ backend/src/models/PatientId.ts
@@ -0,0 +1,8 @@
+import { createHash } from 'crypto';
+
+class PatientId {
+  constructor(firstName: string, lastName: string, dateOfBirth: date) {
+    
+    const hash = createHash('sha256', )
+  }
+}
