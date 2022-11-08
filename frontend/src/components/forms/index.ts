7c83984f4ad4d22715ffd9aca05f465a122cff3a
joshunrau <contact@joshuaunrau.com>
2022-11-08 14:39:46 -0500

    add happy form

--- frontend/src/components/forms/index.ts
+++ frontend/src/components/forms/index.ts
@@ -1,1 +1,2 @@
 export { default as DemographicsForm } from './DemographicsForm';
+export { default as HappinessForm } from './HappinessForm';
--- frontend/src/pages/HappinessScalePage.tsx
+++ frontend/src/pages/HappinessScalePage.tsx
@@ -2,26 +2,15 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import { HappinessForm } from '@/components/forms';
 import Layout from '@/components/Layout';
 
 const HappinessScalePage = () => {
   return (
     <Layout>
-      <div className="d-flex align-items-center justify-content-center h-100">
-        <div className="text-center w-75">
-          <h1 className="display-5 fw-bold">Welcome to the Douglas Neuroinformatics Platform</h1>
-          <p className="lead mb-4">
-            This is an early prototype of our planned web portal. Here, you can add new patients to
-            our database and view a simple table of all patients. Separately, we have also deployed
-            a dashboard with Redash, which is connected to our API.
-          </p>
-          <div>
-            <Link className="btn btn-primary btn-lg" to="/add-patient">
-              Try Adding a New Patient
-            </Link>
-          </div>
-        </div>
-      </div>
+      <h1 className="text-center py-2">Submit the Happiness Questionnaire</h1>
+      <hr className="py-2" />
+      <HappinessForm />
     </Layout>
   );
 };
