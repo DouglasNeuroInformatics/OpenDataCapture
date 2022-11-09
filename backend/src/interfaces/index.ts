b3a05d88354649c7b664bf8ca1f7998bdeb40857
joshunrau <contact@joshuaunrau.com>
2022-11-09 10:40:09 -0500

    update types

--- backend/src/interfaces/index.ts
+++ backend/src/interfaces/index.ts
@@ -1,13 +1,7 @@
 import type { Request, Response, NextFunction } from 'express';
 
-export interface Controller {
-  (req: Request, res: Response, next: NextFunction): void
-}
+export type Controller = (req: Request, res: Response, next: NextFunction) => void
 
-export interface AsyncController {
-  (req: Request, res: Response, next: NextFunction): Promise<void>
-}
+export type AsyncController = (req: Request, res: Response, next: NextFunction) => Promise<void>
 
-export interface ErrorController {
-  (error: unknown, req: Request, res: Response, next: NextFunction): void
-}
+export type ErrorController = (error: unknown, req: Request, res: Response, next: NextFunction) => void
