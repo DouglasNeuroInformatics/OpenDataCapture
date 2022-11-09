25e02502091b1f5a9b2718c9360bc62061ca6fd2
joshunrau <contact@joshuaunrau.com>
2022-11-09 11:56:44 -0500

    add range

--- frontend/src/components/forms/RangeField.tsx
+++ frontend/src/components/forms/RangeField.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 
 import { type FieldProps } from 'formik';
+import Form from 'react-bootstrap/Form';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 
-interface TextFieldProps extends FieldProps<string> {
+
+interface RangeFieldProps extends FieldProps<string> {
   label: string;
 }
 
-const RangeField: React.FunctionComponent<TextFieldProps> = ({ field, form, label, ...props }) => {
+const RangeField: React.FunctionComponent<RangeFieldProps> = ({ field, form, label, ...props }) => {
+
+  const tooltip = (
+    <Tooltip id={`tooltip-field`}>
+      {field.value || "Slide to set your happiness score"}
+    </Tooltip>
+  )
+
   return (
     <div className="form-group mb-3">
-      <label htmlFor={field.name}>{label}</label>
-      <input className="form-control" {...field} {...props} />
+      <Form.Label htmlFor={field.name}>{label}</Form.Label>
+      <OverlayTrigger placement="bottom-start" overlay={tooltip}>
+        <Form.Range min="0" max="10" {...field} {...props} />
+      </OverlayTrigger>
       {form.touched[field.name] && form.errors[field.name] && (
         <div className="alert alert-danger">{form.errors[field.name] as string}</div>
       )}
