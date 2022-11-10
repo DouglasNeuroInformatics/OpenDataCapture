d5b3600cd65905580424f3909769014cca31bd8a
joshunrau <contact@joshuaunrau.com>
2022-11-10 11:24:43 -0500

    add dummy questionnaires

--- scripts/insertDummyData.js
+++ scripts/insertDummyData.js
@@ -16,24 +16,39 @@ function getRandomBirthday() {
 }
 
 const dummyPatients = []
+const dummyHappinessQuestionnaires = []
 for (let i = 0; i < 10; i++) {
   for (let j = 0; j < 10; j++) {
+    const malePatientId = createHash('sha256').update(maleNames[j] + lastNames[i]).digest('hex')
+    const femalePatientId = createHash('sha256').update(femaleNames[j] + lastNames[i]).digest('hex')
     dummyPatients.push({
-      _id: createHash('sha256').update(maleNames[j] + lastNames[i]).digest('hex'),
+      _id: malePatientId,
       firstName: maleNames[j],
       lastName: lastNames[i],
       sex: 'male',
       dateOfBirth: getRandomBirthday()
     })
     dummyPatients.push({
-      _id: createHash('sha256').update(femaleNames[j] + lastNames[i]).digest('hex'),
+      _id: femalePatientId,
       firstName: femaleNames[j],
       lastName: lastNames[i],
       sex: 'female',
       dateOfBirth: getRandomBirthday()
     })
+    for (let n = 0; n < getRandomInteger(0, 5); n++) {
+      dummyHappinessQuestionnaires.push({
+        patientId: malePatientId,
+        score: getRandomInteger(1, 11)
+      })
+      dummyHappinessQuestionnaires.push({
+        patientId: femalePatientId,
+        score: getRandomInteger(1, 11)
+      })
+    }
   }
 }
-
 db = connect('mongodb://mongo:27017/main');
+db.patients.drop()
+db.happinessquestionnaires.drop()
 db.patients.insertMany(dummyPatients);
+db.happinessquestionnaires.insertMany(dummyHappinessQuestionnaires);
