f4661c67a885d8d4c7a3fea2119152f49031c7d1
joshunrau <contact@joshuaunrau.com>
2022-11-10 19:30:42 -0500

    add dynamic db name

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -12,12 +12,21 @@ dotenv.config();
  * exist and are in the correct format, and casting them to the correct type.
  */
 class Config {
+  private environments = ['development', 'testing'];
   mongoUri: string;
   port: number;
 
   constructor() {
-    this.mongoUri = `${this.getEnvironmentVariable('MONGO_URI')}/main`;
+    this.mongoUri = this.getMongoUri();
     this.port = this.getIntegerEnvironmentVariable('PORT');
+  }
+
+  private getMongoUri(): string {
+    const env = this.getEnvironmentVariable('NODE_ENV');
+    if (!this.environments.includes(env)) {
+      throw new InvalidEnvironmentVariableError('NODE_ENV', env)
+    }
+    return `${this.getEnvironmentVariable('MONGO_URI')}/${env}`
   }
 
   private getEnvironmentVariable(key: string): string {
@@ -31,7 +40,7 @@ class Config {
   private getIntegerEnvironmentVariable(key: string): number {
     const value = this.getEnvironmentVariable(key);
     if (!StringUtils.isInteger(value)) {
-      throw new InvalidEnvironmentVariableError(key, `Value '${value} is not an integer`);
+      throw new InvalidEnvironmentVariableError(key, value);
     }
     return parseInt(value);
   }
