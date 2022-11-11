25717fde101a91cd91dd934a17222235d7d3d8e7
joshunrau <contact@joshuaunrau.com>
2022-11-10 20:40:47 -0500

    use dummy data

--- server/src/test.ts
+++ server/src/test.ts
@@ -1,18 +1,23 @@
+/* eslint-disable */
 import mongoose from 'mongoose';
 import request from 'supertest';
 
 import app from '../src/app';
 import config from '../src/config';
+import { createDummyPatients } from '../src/utils/dummy'
 
 beforeAll(async () => {
   await mongoose.connect(config.mongoUri);
+  await createDummyPatients();
 });
 
 test('GET /api/patient', async () => {
   const response = await request(app).get('/api/patient');
   expect(response.statusCode).toBe(200);
+  expect(response.body.length).toBe(200);
 });
 
 afterAll(async () => {
+  await mongoose.connection.db.dropDatabase();
   await mongoose.connection.close();
 });
