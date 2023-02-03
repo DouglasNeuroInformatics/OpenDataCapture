695ad22b6fdefecff4baf4767b2515eaa826575b
joshunrau <contact@joshuaunrau.com>
2023-02-03 17:39:07 -0500

    update home page

--- apps/client/src/features/misc/pages/HomePage.tsx
+++ apps/client/src/features/misc/pages/HomePage.tsx
@@ -4,6 +4,8 @@ import { useTranslation } from 'react-i18next';
 
 import { Disclaimer } from '../components/Disclaimer';
 
+import { Button } from '@/components/base';
+
 export const HomePage = () => {
   const { t } = useTranslation();
 
@@ -12,10 +14,14 @@ export const HomePage = () => {
       <Disclaimer />
       <div className="mx-auto flex h-full max-w-3xl flex-col items-center justify-center">
         <div className="py-6 sm:py-10">
-          <h1 className="text-center text-4xl font-extrabold text-slate-900 sm:text-5xl lg:text-6xl">
+          <h1 className="whitespace-nowrap text-3xl font-extrabold text-slate-900 sm:text-4xl lg:text-5xl">
             {t('home.pageTitle')}
           </h1>
-          <p className="mx-auto mt-6 text-center text-lg text-slate-600 ">{t('home.platformDescription')}</p>
+          <p className="mx-auto mt-5 text-lg text-slate-600 ">{t('home.platformDescription')}</p>
+          <div className="my-5 flex">
+            <Button className="mr-2" label="Get Started" />
+            <Button label="Learn More" variant="light" />
+          </div>
         </div>
       </div>
     </React.Fragment>
