a94140bd6f8101bc5c4dd4f13b9d4ea40ef688f6
joshunrau <contact@joshuaunrau.com>
2023-02-06 12:58:53 -0500

    figure out logic for arrow

--- apps/client/src/components/core/ArrowToggle/ArrowToggle.tsx
+++ apps/client/src/components/core/ArrowToggle/ArrowToggle.tsx
@@ -1,18 +1,51 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
-import clsx from 'clsx';
-import { HiChevronDown } from 'react-icons/hi2';
+import { HiChevronUp } from 'react-icons/hi2';
+
+type ArrowTogglePosition = 'up' | 'right' | 'down' | 'left';
+
+type Rotation = 0 | 90 | 180 | -90;
+
+const defaultRotations: Record<ArrowTogglePosition, Rotation> = {
+  up: 0,
+  right: 90,
+  down: 180,
+  left: -90
+};
 
 export interface ArrowToggleProps extends React.ComponentPropsWithoutRef<'button'> {
-  startPosition: 'up' | 'down';
+  position: ArrowTogglePosition;
+  rotation: Rotation;
 }
 
-export const ArrowToggle = ({ startPosition, ...props }: ArrowToggleProps) => {
-  const [isToggled, setIsToggled] = useState();
+export const ArrowToggle = ({ position, rotation, onClick, ...props }: ArrowToggleProps) => {
+  const [isToggled, setIsToggled] = useState(false);
+
+  const handleClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    setIsToggled(!isToggled);
+    if (onClick) {
+      onClick(event);
+    }
+  };
+
+  const rotationClassName = useMemo(() => {
+    let computedRotation = defaultRotations[position];
+    if (isToggled) {
+      const toggledRotation = computedRotation + rotation;
+      if (toggledRotation === -180) {
+        computedRotation = 180;
+      } else if (toggledRotation > 180) {
+        computedRotation = -(toggledRotation - 180) as Rotation;
+      } else {
+        computedRotation = toggledRotation as Rotation;
+      }
+    }
+    return computedRotation >= 0 ? `rotate-${computedRotation}` : `-rotate-${Math.abs(computedRotation)}`;
+  }, [position, rotation, isToggled]);
 
   return (
-    <button {...props}>
-      <HiChevronDown className={clsx({ 'translate-x-1/2': true })} />
+    <button onClick={handleClick} {...props}>
+      <HiChevronUp className={`transform-gpu ${rotationClassName}`} />
     </button>
   );
 };
