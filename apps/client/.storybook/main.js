2c82f17c469578c7ec713ec8aad2e7da43d37a1a
joshunrau <contact@joshuaunrau.com>
2023-02-06 11:18:39 -0500

    update to storybook v7

---
+++ apps/client/.storybook/main.js
@@ -0,0 +1,13 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
+  },
+  docs: {
+    autodocs: 'tag'
+  }
+};
+export default config;
