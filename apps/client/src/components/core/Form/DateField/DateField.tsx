a2d03d9b68172668b5f50f30ba752fb149913289
joshunrau <contact@joshuaunrau.com>
2023-02-14 16:53:49 -0500

    use custom picker for field

--- apps/client/src/components/core/Form/DateField/DateField.tsx
+++ apps/client/src/components/core/Form/DateField/DateField.tsx
@@ -1,23 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import DatePicker from 'react-datepicker';
+import { Transition } from '@headlessui/react';
+import { clsx } from 'clsx';
 import { useController } from 'react-hook-form';
 
+import { ErrorMessage } from '../ErrorMessage';
 import { FormDataType } from '../types';
 
-import 'react-datepicker/dist/react-datepicker.css';
+import { DatePicker } from './DatePicker';
 
 export interface DateFieldProps {
   name: string;
   label: string;
 }
 
-export const DateField = ({ name }: DateFieldProps) => {
+export const DateField = ({ name, label }: DateFieldProps) => {
   const { field, formState } = useController<FormDataType>({ name, defaultValue: '' });
+  const [value, setValue] = useState(field.value);
+  const [showDatePicker, setShowDatePicker] = useState(false);
 
-  const handleChange = () => {
-    field.onChange;
+  const onChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    field.onChange(event.target.value);
+    setValue(event.target.value);
   };
 
-  return <DatePicker className="field-input" dateFormat="yyyy-MM-dd" value={field.value} onChange={field.onChange} />;
+  const handleDatePickerSelection = (date: Date) => {
+    setValue(date.toISOString());
+  };
+
+  const handleInputFocus: React.FocusEventHandler = () => {
+    setShowDatePicker(true);
+  };
+
+  const handleInputBlur: React.FocusEventHandler = () => {
+    setShowDatePicker(false);
+  };
+
+  return (
+    <React.Fragment>
+      <div className="field-container">
+        <input
+          autoComplete="off"
+          className="field-input peer"
+          value={value}
+          onBlur={handleInputBlur}
+          onChange={onChange}
+          onFocus={handleInputFocus}
+        />
+        <label
+          className={clsx('field-label peer-focus:field-label-floating', {
+            'field-label-floating': value
+          })}
+          htmlFor={name}
+        >
+          {label}
+        </label>
+        <ErrorMessage error={formState.errors[name]} />
+        <Transition show={showDatePicker}>
+          <DatePicker onSelection={handleDatePickerSelection} />
+        </Transition>
+      </div>
+    </React.Fragment>
+  );
 };
