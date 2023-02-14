80f1871e7b759cebec4607c2acbcce2f9d010bcb
joshunrau <contact@joshuaunrau.com>
2023-02-14 11:46:48 -0500

    fix arrow toggle

---
+++ apps/client/src/components/core/ArrowToggle/ArrowToggle.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import { ArrowToggle } from './ArrowToggle';
+
+describe('ArrowToggle', () => {
+  it('should rotate correctly from up to down', () => {
+    render(<ArrowToggle position="up" rotation={180} />);
+
+    const btn = screen.getByRole('button');
+    const svg = screen.getByTestId('arrow-up-icon');
+
+    expect(svg).toHaveStyle({ transform: 'rotate(0deg)' });
+    fireEvent.click(btn);
+    expect(svg).toHaveStyle({ transform: 'rotate(180deg)' });
+  });
+});
