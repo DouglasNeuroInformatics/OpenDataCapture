6cacb1e79a8edab9d199e1937a2b1f903d22330b
joshunrau <contact@joshuaunrau.com>
2023-02-17 19:07:08 -0500

    understand Distributive Conditional Types

--- apps/client/src/components/form/types.ts
+++ apps/client/src/components/form/types.ts
@@ -2,10 +2,9 @@ import { DateFieldProps } from './DateField';
 import { SelectFieldProps } from './SelectField';
 import { TextFieldProps } from './TextField';
 
-type FormFieldType<T extends string> =
-  | Omit<TextFieldProps, 'name'>
-  | Omit<SelectFieldProps<T>, 'name'>
-  | Omit<DateFieldProps, 'name'>;
+type FormFieldType<T> = [T] extends [FormFieldValue]
+  ? Omit<TextFieldProps, 'name'> | Omit<SelectFieldProps<T>, 'name'> | Omit<DateFieldProps, 'name'>
+  : never;
 
 export interface BaseFieldProps {
   name: string;
@@ -17,7 +16,7 @@ export type FormFieldValue = string;
 export type FormDataType = Record<PropertyKey, FormFieldValue | Record<PropertyKey, FormFieldValue>[]>;
 
 export type FormFields<T extends FormDataType> = {
-  [K in keyof T]?: T extends Record<PropertyKey, FormFieldValue> ? FormFieldType<T[K]> : never;
+  [K in keyof T]?: FormFieldType<T[K]>;
 };
 
 export type FormStructure<T extends FormDataType = FormDataType> = Array<{
