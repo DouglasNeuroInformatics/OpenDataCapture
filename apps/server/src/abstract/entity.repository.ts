4de23e36a4766add3a7247e26d8769b5805fc26f
joshunrau <contact@joshuaunrau.com>
2023-03-01 12:05:40 -0500

    add logger

--- apps/server/src/abstract/entity.repository.ts
+++ apps/server/src/abstract/entity.repository.ts
@@ -1,25 +1,36 @@
-import { Document, FilterQuery, Model } from 'mongoose';
+import { Logger } from '@nestjs/common';
+
+import { Document, FilterQuery, Model, ProjectionType } from 'mongoose';
 
 export abstract class EntityRepository<T, U extends Document> {
-  constructor(protected readonly entityModel: Model<U>) {}
+  protected readonly logger: Logger;
+
+  constructor(protected readonly entityModel: Model<U>) {
+    this.logger = new Logger(`${EntityRepository.name}: ${entityModel.modelName}`);
+  }
 
   async create(entityDoc: T): Promise<U> {
+    this.logger.verbose('create()');
     return this.entityModel.create(entityDoc);
   }
 
-  async find(filterQuery: FilterQuery<T>, projection?: Record<string, unknown>): Promise<U[]> {
-    return this.entityModel.find(filterQuery, projection).exec();
+  async find(filterQuery: FilterQuery<T>, projection?: ProjectionType<T>, populate: string[] = []): Promise<U[]> {
+    this.logger.verbose('find()');
+    return this.entityModel.find(filterQuery, projection).populate(populate).exec();
   }
 
   async findOne(filterQuery: FilterQuery<T>, projection?: Record<string, unknown>): Promise<U | null> {
+    this.logger.verbose('findOne()');
     return this.entityModel.findOne(filterQuery, projection).exec();
   }
 
   async findAll(): Promise<U[]> {
+    this.logger.verbose('findAll()');
     return this.entityModel.find().exec();
   }
 
   async exists(filterQuery: FilterQuery<T>): Promise<boolean> {
+    this.logger.verbose('exists()');
     return (await this.entityModel.exists(filterQuery).exec()) !== null;
   }
 }
