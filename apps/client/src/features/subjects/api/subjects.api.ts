ef8b574bf7d994e73054f9ce322a049ed892338f
joshunrau <contact@joshuaunrau.com>
2023-03-02 12:46:17 -0500

    cleanup

--- apps/client/src/features/subjects/api/subjects.api.ts
+++ apps/client/src/features/subjects/api/subjects.api.ts
@@ -1,26 +1,9 @@
 import axios from 'axios';
-import { subjectSchema } from 'common';
-import { z } from 'zod';
 
 import { IdentificationFormData } from '@/components/core';
 
 export class SubjectsAPI {
   static async addSubject(data: IdentificationFormData) {
     await axios.post('/api/subjects', data);
-  }
-
-  static async getSubjects() {
-    const response = await axios.get('/api/subjects');
-    return z.array(subjectSchema).parseAsync(response.data);
-  }
-
-  static async geAvailableInstrumentRecords(subjectId: string) {
-    const response = await axios.get(`/api/instruments/records/available?subject=${subjectId}`);
-    return response.data as Array<{ title: string; count: number }>;
-  }
-
-  static async getInstrumentRecords(instrumentTitle: string, subjectId: string) {
-    const response = await axios.get(`/api/instruments/records?instrument=${instrumentTitle}&subject=${subjectId}`);
-    return response.data as { dateCollected: string; data: Record<string, number> }[];
   }
 }
