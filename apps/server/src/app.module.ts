5c5d333ec6fb83e8840381fb92b0822ed78cec3f
joshunrau <contact@joshuaunrau.com>
2023-03-04 12:32:39 -0500

    rm old app

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [AuthModule, UsersModule]
 })
 export class AppModule {}
