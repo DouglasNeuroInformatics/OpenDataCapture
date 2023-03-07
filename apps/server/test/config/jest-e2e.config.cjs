4892f6e7d57b5d14ae529018951bd05f2c63ae0f
joshunrau <contact@joshuaunrau.com>
2023-03-07 10:32:48 -0500

    rm old config

--- apps/server/test/config/jest-e2e.config.cjs
+++ apps/server/test/config/jest-e2e.config.cjs
@@ -12,15 +12,12 @@ module.exports = {
     prefix: path.resolve(__dirname, '..', '..')
   }),
   modulePaths: [compilerOptions.baseUrl],
-  // globalSetup: path.resolve(__dirname, 'jest-e2e.setup.cjs'),
-  // globalTeardown: path.resolve(__dirname, 'jest-e2e.teardown.cjs'),
   rootDir: path.resolve(__dirname, '..'),
   testEnvironment: 'node',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
-  // setupFiles: [path.resolve(__dirname, 'jest-e2e.setup.cjs')],
   setupFilesAfterEnv: [path.resolve(__dirname, 'jest-e2e.setup.ts')],
   verbose: true
 };
