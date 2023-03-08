cdbf2bd8840f9b3691715a7654d0de7d28e4212a
joshunrau <contact@joshuaunrau.com>
2023-03-07 23:48:29 -0500

    figured it out

--- apps/server/src/auth/guards/access-token.guard.ts
+++ apps/server/src/auth/guards/access-token.guard.ts
@@ -1,18 +1,20 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, Logger } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 
-import { Observable } from 'rxjs';
+import { Request } from 'express';
 
 import { AuthKeys } from '../decorators/auth.decorator';
 
 @Injectable()
 export class AccessTokenGuard extends AuthGuard('jwt') {
+  private readonly logger = new Logger(AccessTokenGuard.name);
+
   constructor(private readonly reflector: Reflector) {
     super();
   }
 
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+  async canActivate(context: ExecutionContext): Promise<any> {
     const isPublic = this.reflector.getAllAndOverride<boolean | undefined>(AuthKeys.IsPublic, [
       context.getHandler(),
       context.getClass()
@@ -22,6 +24,18 @@ export class AccessTokenGuard extends AuthGuard('jwt') {
       return true;
     }
 
+    const isValidToken = await super.canActivate(context);
+
+    if (!isValidToken) {
+      return false;
+    }
+    const request = this.getRequest(context);
+    this.logger.log(request.user);
+
     return super.canActivate(context);
+  }
+
+  private getRequest(context: ExecutionContext): Request {
+    return context.switchToHttp().getRequest();
   }
 }
