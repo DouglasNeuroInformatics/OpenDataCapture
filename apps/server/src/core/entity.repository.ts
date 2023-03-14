7a72ade0e8aaa4fc044fe8c7164a881cf48af1ab
joshunrau <contact@joshuaunrau.com>
2023-03-13 21:34:20 -0400

    import only mongoose

--- apps/server/src/core/entity.repository.ts
+++ apps/server/src/core/entity.repository.ts
@@ -1,11 +1,11 @@
 import { Logger } from '@nestjs/common';
 
-import { FilterQuery, HydratedDocument, Model, ProjectionType, Query, QueryOptions, UpdateQuery } from 'mongoose';
+import mongoose from 'mongoose';
 
-interface FindMethodArgs<Entity, EntityDocument = HydratedDocument<Entity>> {
-  filter?: FilterQuery<EntityDocument>;
-  projection?: ProjectionType<EntityDocument>;
-  options?: QueryOptions<EntityDocument>;
+interface FindMethodArgs<Entity, EntityDocument = mongoose.HydratedDocument<Entity>> {
+  filter?: mongoose.FilterQuery<EntityDocument>;
+  projection?: mongoose.ProjectionType<EntityDocument>;
+  options?: mongoose.QueryOptions<EntityDocument>;
 }
 
 /**
@@ -14,10 +14,10 @@ interface FindMethodArgs<Entity, EntityDocument = HydratedDocument<Entity>> {
  * and if mongoose implements breaking changes, or we want to migrate away from mongoose,
  * we can modify this class instead of the code in each module.
  */
-export abstract class EntityRepository<Entity, EntityDocument = HydratedDocument<Entity>> {
+export abstract class EntityRepository<Entity, EntityDocument = mongoose.HydratedDocument<Entity>> {
   protected readonly logger: Logger;
 
-  constructor(protected readonly entityModel: Model<EntityDocument>) {
+  constructor(protected readonly entityModel: mongoose.Model<EntityDocument>) {
     this.logger = new Logger(`${EntityRepository.name}: ${entityModel.modelName}`);
   }
 
@@ -26,30 +26,30 @@ export abstract class EntityRepository<Entity, EntityDocument = HydratedDocument
     return this.entityModel.create(entity);
   }
 
-  find(args: FindMethodArgs<Entity> = {}): Query<EntityDocument[], EntityDocument> {
+  find(args: FindMethodArgs<Entity> = {}): mongoose.Query<EntityDocument[], EntityDocument> {
     return this.entityModel.find(args.filter ?? {}, args.projection, args.options);
   }
 
-  findOne(args: FindMethodArgs<Entity> = {}): Query<EntityDocument | null, EntityDocument> {
+  findOne(args: FindMethodArgs<Entity> = {}): mongoose.Query<EntityDocument | null, EntityDocument> {
     return this.entityModel.findOne(args.filter ?? {}, args.projection, args.options);
   }
 
   findOneAndUpdate(
-    filter: FilterQuery<EntityDocument>,
-    update: UpdateQuery<EntityDocument>,
-    options: Omit<QueryOptions<EntityDocument>, 'new'> = {}
+    filter: mongoose.FilterQuery<EntityDocument>,
+    update: mongoose.UpdateQuery<EntityDocument>,
+    options: Omit<mongoose.QueryOptions<EntityDocument>, 'new'> = {}
   ): Promise<EntityDocument | null> {
     return this.entityModel.findOneAndUpdate(filter, update, { ...options, new: true });
   }
 
   findOneAndDelete(
-    filter: FilterQuery<EntityDocument>,
-    options: Omit<QueryOptions<EntityDocument>, 'new'> = {}
+    filter: mongoose.FilterQuery<EntityDocument>,
+    options: Omit<mongoose.QueryOptions<EntityDocument>, 'new'> = {}
   ): Promise<EntityDocument | null> {
     return this.entityModel.findOneAndDelete(filter, { ...options, new: true });
   }
 
-  async exists(filterQuery: FilterQuery<Entity>): Promise<boolean> {
+  async exists(filterQuery: mongoose.FilterQuery<Entity>): Promise<boolean> {
     this.logger.verbose(`Checking for document matching the following query: ${JSON.stringify(filterQuery)}`);
     const result = (await this.entityModel.exists(filterQuery).exec()) !== null;
     this.logger.verbose(`Result: ${result}`);
