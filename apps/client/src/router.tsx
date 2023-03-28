68bcaa91833e596191fb6c90699f433e9ab9bb5a
joshunrau <contact@joshuaunrau.com>
2023-03-28 15:20:57 -0400

    add routes

--- apps/client/src/router.tsx
+++ apps/client/src/router.tsx
@@ -5,8 +5,9 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from './components';
 import { LoginPage } from './features/auth';
 import { ContactPage } from './features/contact';
+import { AddInstrumentPage, InstrumentPage, ViewInstrumentsPage } from './features/instruments';
 import { OverviewPage } from './features/overview/pages/OverviewPage';
-import { AddSubjectPage, ViewSubjectsPage } from './features/subjects';
+import { AddSubjectPage, SubjectPage, SubjectRecordsPage, ViewSubjectsPage } from './features/subjects';
 import { UserPage } from './features/user';
 import { useAuthStore } from './stores/auth-store';
 
@@ -23,7 +24,16 @@ export const Router = () => {
             <Route element={<UserPage />} path="user" />
             <Route path="subjects">
               <Route element={<AddSubjectPage />} path="add-subject" />
-              <Route element={<ViewSubjectsPage />} path="view-subjects" />
+              <Route path="view-subjects">
+                <Route index element={<ViewSubjectsPage />} />
+                <Route element={<SubjectPage />} path=":subjectId" />
+                <Route element={<SubjectRecordsPage />} path=":subjectId/records/:instrumentTitle" />
+              </Route>
+            </Route>
+            <Route path="instruments">
+              <Route element={<AddInstrumentPage />} path="add-instrument" />
+              <Route element={<ViewInstrumentsPage />} path="view-instruments" />
+              <Route element={<InstrumentPage />} path=":id" />
             </Route>
           </Route>
         ) : (
@@ -33,49 +43,3 @@ export const Router = () => {
     </BrowserRouter>
   );
 };
-/*
-import { Navigate, Outlet, createBrowserRouter } from 'react-router-dom';
-
-import { Layout } from './components/layout';
-import { AdminPage } from './features/admin';
-import { LoginPage } from './features/auth';
-import { AddInstrumentPage, InstrumentPage, ViewInstrumentsPage } from './features/instruments';
-import { ErrorPage, HomePage } from './features/misc';
-import { AddSubjectPage, SubjectPage, SubjectRecordsPage, ViewSubjectsPage } from './features/subjects';
-import { useAuthStore } from './stores/auth-store';
-
-
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-
-
-
-      {
-        path: '/subjects/view-subjects/:subjectId',
-        element: <SubjectPage />
-      },
-      {
-        path: '/subjects/view-subjects/:subjectId/records/:instrumentTitle',
-        element: <SubjectRecordsPage />
-      },
-      {
-        path: '/instruments/add-instrument',
-        element: <AddInstrumentPage />
-      },
-      {
-        path: '/instruments/view-instruments',
-        element: <ViewInstrumentsPage />
-      },
-      {
-        path: '/instruments/:id',
-        element: <InstrumentPage />
-      }
-    ]
-  },
-
-]);
-*/
