e8a0272e10a918061a493744459589294a35717b
joshunrau <contact@joshuaunrau.com>
2023-04-03 09:45:21 -0400

    translation for stats

--- apps/client/src/features/overview/pages/OverviewPage.tsx
+++ apps/client/src/features/overview/pages/OverviewPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { useTranslation } from 'react-i18next';
-import { HiUsers } from 'react-icons/hi2';
+import { HiClipboardDocument, HiDocumentText, HiUser, HiUsers } from 'react-icons/hi2';
 
 import { StatisticCard } from '../components/StatisticCard';
 
@@ -16,12 +16,14 @@ export const OverviewPage = () => {
   return (
     <div>
       <PageHeader title={pageTitle} />
-      <section className="body-font text-gray-600">
-        <div className="container mx-auto px-5 py-24">
-          <div className="-m-4 grid grid-cols-1 gap-5 text-center md:grid-cols-3">
-            <StatisticCard icon={<HiUsers />} label="Total Users" value={5} />
-            <StatisticCard icon={<HiUsers />} label="Total Subjects" value={5} />
-            <StatisticCard icon={<HiUsers />} label="Total Instruments" value={5} />
+      <section>
+        <h3 className="mb-5 text-xl font-medium">{t('summary')}</h3>
+        <div className="body-font text-slate-600">
+          <div className="grid grid-cols-1 gap-5 text-center lg:grid-cols-2">
+            <StatisticCard icon={<HiUsers />} label={t('stats.totalUsers')} value={5} />
+            <StatisticCard icon={<HiUser />} label={t('stats.totalSubjects')} value={5} />
+            <StatisticCard icon={<HiClipboardDocument />} label={t('stats.totalInstruments')} value={5} />
+            <StatisticCard icon={<HiDocumentText />} label={t('stats.totalRecords')} value={5} />
           </div>
         </div>
       </section>
