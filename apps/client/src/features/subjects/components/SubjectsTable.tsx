f4b2fa0c31165e918b84ca2dee03dd94ca26655e
joshunrau <contact@joshuaunrau.com>
2023-04-03 10:36:52 -0400

    add translations for view subjects

--- apps/client/src/features/subjects/components/SubjectsTable.tsx
+++ apps/client/src/features/subjects/components/SubjectsTable.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Subject } from '@ddcp/common';
+import { useTranslation } from 'react-i18next';
 
 import { Dropdown, Table } from '@/components';
 import { useDownload } from '@/hooks/useDownload';
@@ -12,6 +13,8 @@ export interface SubjectTableProps {
 export const SubjectsTable = ({ data }: SubjectTableProps) => {
   const downloadJSON = useDownload('/api/v0/instruments/records/export-json', 'records.json');
   const downloadCSV = useDownload('/api/v0/instruments/records/export-csv', 'records.csv');
+
+  const { t } = useTranslation(['common', 'subjects']);
 
   const handleExportSelection = (option: string | ('JSON' | 'CSV')) => {
     switch (option) {
@@ -30,27 +33,31 @@ export const SubjectsTable = ({ data }: SubjectTableProps) => {
         <input
           required
           className="block w-full rounded-lg border border-gray-300 px-4 py-3 pl-2 text-sm"
-          placeholder="Search..."
+          placeholder={t('subjects:viewSubjects.table.search.placeholder')}
           type="search"
         />
         <div className="flex gap-2">
-          <Dropdown options={[]} title="Filters" onSelection={() => null} />
-          <Dropdown options={['CSV', 'JSON']} title="Export" onSelection={handleExportSelection} />
+          <Dropdown options={[]} title={t('subjects:viewSubjects.table.filters')} onSelection={() => null} />
+          <Dropdown
+            options={['CSV', 'JSON']}
+            title={t('subjects:viewSubjects.table.export')}
+            onSelection={handleExportSelection}
+          />
         </div>
       </div>
       <Table<Subject>
         columns={[
           {
-            name: 'Subject',
+            name: t('subjects:viewSubjects.table.columns.subject'),
             field: (subject) => subject.identifier.slice(0, 6)
           },
           {
-            name: 'Date of Birth',
+            name: t('subjects:viewSubjects.table.columns.dateOfBirth'),
             field: (subject) => subject.dateOfBirth
           },
           {
-            name: 'Sex',
-            field: (subject) => subject.sex
+            name: t('subjects:viewSubjects.table.columns.sex'),
+            field: (subject) => (subject.sex === 'female' ? t('common:sex.female') : t('common:sex.male'))
           }
         ]}
         data={data}
--- apps/client/src/features/subjects/pages/ViewSubjectsPage.tsx
+++ apps/client/src/features/subjects/pages/ViewSubjectsPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Subject } from '@ddcp/common';
+import { useTranslation } from 'react-i18next';
 
 import { SubjectsTable } from '../components/SubjectsTable';
 
@@ -9,14 +10,15 @@ import { useFetch } from '@/hooks/useFetch';
 
 export const ViewSubjectsPage = () => {
   const { data } = useFetch<Subject[]>('/subjects');
-
+  const { t } = useTranslation('subjects');
+  
   if (!data) {
     return <Spinner />;
   }
 
   return data ? (
     <div>
-      <PageHeader title="View Subjects" />
+      <PageHeader title={t('viewSubjects.pageTitle')} />
       <SubjectsTable data={data} />
     </div>
   ) : null;
