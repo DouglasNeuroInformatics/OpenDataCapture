9579cfa824a870a0fe8793f519c9691848a704da
joshunrau <contact@joshuaunrau.com>
2023-04-07 11:01:29 -0400

    remove required from searchbar

--- apps/client/src/components/SearchBar/SearchBar.tsx
+++ apps/client/src/components/SearchBar/SearchBar.tsx
@@ -5,13 +5,12 @@ import { useTranslation } from 'react-i18next';
 
 export type SearchBarProps = Omit<React.ComponentPropsWithoutRef<'input'>, 'type'>;
 
-export const SearchBar = ({ className, required = true, placeholder, ...props }: SearchBarProps) => {
+export const SearchBar = ({ className, placeholder, ...props }: SearchBarProps) => {
   const { t } = useTranslation('common');
   return (
     <input
       className={clsx('block w-full rounded-lg border border-gray-300 px-4 py-3 pl-2 text-sm', className)}
       placeholder={placeholder ?? t('searchBar.placeholder')}
-      required={required}
       type="search"
       {...props}
     />
