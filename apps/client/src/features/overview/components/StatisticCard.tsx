b9feb54e999379746752cf65c56ac27366097b93
joshunrau <contact@joshuaunrau.com>
2023-04-11 19:07:51 -0400

    add a nice animation

--- apps/client/src/features/overview/components/StatisticCard.tsx
+++ apps/client/src/features/overview/components/StatisticCard.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 
+import { animated, useSpring } from '@react-spring/web';
+
 interface StatisticCardProps {
   label: string;
   icon?: JSX.Element;
-  value: any;
+  value: number;
 }
 
 export const StatisticCard = ({ label, icon, value }: StatisticCardProps) => {
+  const springs = useSpring({
+    from: { value: 0 },
+    to: { value }
+  });
+
   return (
     <div className="flex w-full rounded-lg border bg-white p-4">
       {icon && <div className="mr-2 flex items-center justify-center text-5xl">{icon}</div>}
       <div className="w-full">
-        <h2 className="title-font text-3xl font-medium text-gray-900 sm:text-4xl">{value}</h2>
+        <animated.h3 className="title-font text-3xl font-medium text-gray-900 sm:text-4xl">
+          {springs.value.to((v) => Math.floor(v))}
+        </animated.h3>
         <p className="leading-relaxed">{label}</p>
       </div>
     </div>
