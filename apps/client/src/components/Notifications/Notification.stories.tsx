b9678ea1626acfaf0d3825f830e5083277cd56f6
joshunrau <contact@joshuaunrau.com>
2023-04-12 11:16:51 -0400

    fix deprecated story

--- apps/client/src/components/Notifications/Notification.stories.tsx
+++ apps/client/src/components/Notifications/Notification.stories.tsx
@@ -1,58 +1,62 @@
 /* eslint-disable no-alert */
-import React from 'react';
 
-import { Meta, Story } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 
-import { Notification, NotificationProps } from './Notification';
+import { Notification } from './Notification';
 
-const meta: Meta = {
+type Story = StoryObj<typeof Notification>;
+
+export default {
   component: Notification,
   parameters: {
     controls: { expanded: true }
   }
+} as Meta<typeof Notification>;
+
+export const Info: Story = {
+  args: {
+    notification: {
+      id: '1',
+      type: 'info',
+      title: 'Hello Info',
+      message: 'This is info notification'
+    },
+    onDismiss: (id) => alert(`Dismissing Notification with id: ${id}`)
+  }
 };
 
-export default meta;
+export const Success: Story = {
+  args: {
+    notification: {
+      id: '1',
+      type: 'success',
+      title: 'Hello Success',
+      message: 'This is success notification'
+    },
+    onDismiss: (id) => alert(`Dismissing Notification with id: ${id}`)
+  }
+};
 
-const Template: Story<NotificationProps> = (props) => <Notification {...props} />;
+export const Warning: Story = {
+  args: {
+    notification: {
+      id: '1',
+      type: 'warning',
+      title: 'Hello Warning',
+      message: 'This is warning notification'
+    },
+    onDismiss: (id) => alert(`Dismissing Notification with id: ${id}`)
+  }
+};
 
-export const Info = Template.bind({});
-Info.args = {
-  notification: {
-    id: '1',
-    type: 'info',
-    title: 'Hello Info',
-    message: 'This is info notification'
-  },
-  onDismiss: (id) => alert(`Dismissing Notification with id: ${id}`)
-};
-export const Success = Template.bind({});
-Success.args = {
-  notification: {
-    id: '1',
-    type: 'success',
-    title: 'Hello Success',
-    message: 'This is success notification'
-  },
-  onDismiss: (id) => alert(`Dismissing Notification with id: ${id}`)
-};
-export const Warning = Template.bind({});
-Warning.args = {
-  notification: {
-    id: '1',
-    type: 'warning',
-    title: 'Hello Warning',
-    message: 'This is warning notification'
-  },
-  onDismiss: (id) => alert(`Dismissing Notification with id: ${id}`)
-};
-export const Error = Template.bind({});
-Error.args = {
-  notification: {
-    id: '1',
-    type: 'error',
-    title: 'Hello Error',
-    message: 'This is error notification'
-  },
-  onDismiss: (id) => alert(`Dismissing Notification with id: ${id}`)
+export const Error: Story = {
+  args: {
+    notification: {
+      id: '1',
+      type: 'error',
+      title: 'Hello Error',
+      message: 'This is error notification'
+    },
+    onDismiss: (id) => alert(`Dismissing Notification with id: ${id}`)
+  }
 };
