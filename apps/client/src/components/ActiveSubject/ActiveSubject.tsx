b21fc96bc2ed1fb3b24299680aa8b2da825ac11c
joshunrau <contact@joshuaunrau.com>
2023-04-12 10:53:26 -0400

    fix active subject drag

--- apps/client/src/components/ActiveSubject/ActiveSubject.tsx
+++ apps/client/src/components/ActiveSubject/ActiveSubject.tsx
@@ -13,14 +13,12 @@ export const ActiveSubject = () => {
   const { t } = useTranslation('common');
   const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
 
-  const ref = useRef<HTMLDivElement>(null);
-
   const bind = useDrag(
     ({ down, offset: [ox, oy] }) => {
       api.start({ x: ox, y: oy, immediate: down });
     },
     {
-      bounds: ref,
+      bounds: document.body,
       rubberband: true
     }
   );
@@ -30,39 +28,37 @@ export const ActiveSubject = () => {
   }
 
   return (
-    <div className="pointer-events-none fixed z-50 h-screen w-full print:hidden" ref={ref}>
-      <animated.div
-        className="absolute z-50 flex cursor-pointer touch-none flex-col rounded-lg bg-slate-900/75 p-3 text-slate-300 backdrop-blur-sm"
-        {...bind()}
-        style={{ x, y }}
-      >
-        <div className="pointer-events-auto mb-1 flex justify-end">
-          <button onClick={() => setIsHidden(!isHidden)}>
-            <IoMdRemoveCircle />
-          </button>
-          <button onClick={() => setActiveSubject(null)}>
-            <IoMdCloseCircle />
-          </button>
-        </div>
-        {isHidden && (
-          <React.Fragment>
-            <h3 className="mb-2 text-xl font-medium text-inherit">{t('activeSubject.header')}</h3>
-            <hr className="mb-2" />
-            <span>
-              {t('identificationForm.firstName.label')}: {activeSubject.firstName}
-            </span>
-            <span>
-              {t('identificationForm.lastName.label')}: {activeSubject.lastName}
-            </span>
-            <span>
-              {t('identificationForm.dateOfBirth.label')}: {activeSubject.dateOfBirth}
-            </span>
-            <span>
-              {t('identificationForm.sex.label')}: {t(`sex.${activeSubject.sex}`)}
-            </span>
-          </React.Fragment>
-        )}
-      </animated.div>
-    </div>
+    <animated.div
+      className="absolute z-50 flex cursor-pointer touch-none flex-col rounded-lg bg-slate-900/75 p-3 text-slate-300 backdrop-blur-sm"
+      {...bind()}
+      style={{ x, y }}
+    >
+      <div className="pointer-events-auto mb-1 flex justify-end">
+        <button onClick={() => setIsHidden(!isHidden)}>
+          <IoMdRemoveCircle />
+        </button>
+        <button onClick={() => setActiveSubject(null)}>
+          <IoMdCloseCircle />
+        </button>
+      </div>
+      {isHidden && (
+        <React.Fragment>
+          <h3 className="mb-2 text-xl font-medium text-inherit">{t('activeSubject.header')}</h3>
+          <hr className="mb-2" />
+          <span>
+            {t('identificationForm.firstName.label')}: {activeSubject.firstName}
+          </span>
+          <span>
+            {t('identificationForm.lastName.label')}: {activeSubject.lastName}
+          </span>
+          <span>
+            {t('identificationForm.dateOfBirth.label')}: {activeSubject.dateOfBirth}
+          </span>
+          <span>
+            {t('identificationForm.sex.label')}: {t(`sex.${activeSubject.sex}`)}
+          </span>
+        </React.Fragment>
+      )}
+    </animated.div>
   );
 };
