864cdf3de355054319e4924febc405a5e44a0a82
joshunrau <contact@joshuaunrau.com>
2023-04-15 18:07:52 -0400

    add framer-motion

--- apps/client/src/features/overview/components/StatisticCard.tsx
+++ apps/client/src/features/overview/components/StatisticCard.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { animated, useSpring } from '@react-spring/web';
+import { type MotionValue, motion, useSpring, useTransform } from 'framer-motion';
 
 interface StatisticCardProps {
   label: string;
@@ -9,18 +9,18 @@ interface StatisticCardProps {
 }
 
 export const StatisticCard = ({ label, icon, value }: StatisticCardProps) => {
-  const springs = useSpring({
-    from: { value: 0 },
-    to: { value }
-  });
+  const spring = useSpring(0, { bounce: 0 }) as MotionValue<number>;
+  const rounded = useTransform(spring, (latest: number) => Math.floor(latest));
+
+  useEffect(() => {
+    spring.set(value);
+  }, [spring, value]);
 
   return (
     <div className="flex w-full rounded-lg border bg-white p-4">
       {icon && <div className="mr-2 flex items-center justify-center text-5xl">{icon}</div>}
       <div className="w-full">
-        <animated.h3 className="title-font text-3xl font-medium text-gray-900 sm:text-4xl">
-          {springs.value.to((v) => Math.floor(v))}
-        </animated.h3>
+        <motion.h3 className="title-font text-3xl font-medium text-gray-900 sm:text-4xl">{rounded}</motion.h3>
         <p className="leading-relaxed">{label}</p>
       </div>
     </div>
