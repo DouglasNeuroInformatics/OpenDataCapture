0df190b4654c76cb6b5b542d7fe3fd25131554f8
joshunrau <contact@joshuaunrau.com>
2023-04-19 15:37:16 -0400

    add export

--- packages/instruments/lib/index.ts
+++ packages/instruments/lib/index.ts
@@ -1,3 +1,4 @@
 export * from './forms/brief-psychiatric-rating-scale';
 export * from './forms/enhanced-demographics-questionnaire';
 export * from './forms/happiness-questionnaire';
+export * from './utils/create-translated-forms';
