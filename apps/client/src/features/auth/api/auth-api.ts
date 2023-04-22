c890771758db16b1fc11d31a170ec13274e3e803
joshunrau <contact@joshuaunrau.com>
2023-04-22 17:31:11 -0400

    add bilingual login error

--- apps/client/src/features/auth/api/auth-api.ts
+++
@@ -1,9 +0,0 @@
-import { AuthPayload, LoginCredentials } from '@douglasneuroinformatics/common';
-import axios from 'axios';
-
-export class AuthAPI {
-  static async login(credentials: LoginCredentials): Promise<AuthPayload> {
-    const response = await axios.post<AuthPayload>('/auth/login', credentials);
-    return response.data;
-  }
-}
