b022308fa408464f92ffb9b361cde9125f9e92fb
joshunrau <contact@joshuaunrau.com>
2023-05-04 18:04:34 -0400

    add friendly error for conflict

--- apps/client/src/features/subjects/api/subjects-api.ts
+++ apps/client/src/features/subjects/api/subjects-api.ts
@@ -4,10 +4,6 @@ import axios from 'axios';
 import { IdentificationFormData } from '@/components';
 
 export class SubjectsAPI {
-  static async addSubject(data: IdentificationFormData) {
-    await axios.post('/v1/subjects', data);
-  }
-
   static async lookupSubject(data: IdentificationFormData) {
     return axios.post<Subject>('/v1/subjects/lookup', data);
   }
