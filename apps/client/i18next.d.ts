4c6bf629d9cb55f8fb9d5f09123726f96027f65a
joshunrau <contact@joshuaunrau.com>
2023-05-11 14:19:49 -0400

    merge contact into common

--- apps/client/i18next.d.ts
+++ apps/client/i18next.d.ts
@@ -3,7 +3,6 @@ import 'i18next';
 
 // import all namespaces (for the default language, only)
 import common from './public/locales/en/common.json';
-import contact from './public/locales/en/contact.json';
 import instruments from './public/locales/en/instruments.json';
 import overview from './public/locales/en/overview.json';
 import subjects from './public/locales/en/subjects.json';
@@ -13,7 +12,6 @@ declare module 'i18next' {
     defaultNS: 'common';
     resources: {
       common: typeof common;
-      contact: typeof contact;
       instruments: typeof instruments;
       overview: typeof overview;
       subjects: typeof subjects;
