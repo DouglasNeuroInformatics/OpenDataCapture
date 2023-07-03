041a20ad03dd261828e08c60c29a230a9ea97198
joshunrau <contact@joshuaunrau.com>
2023-07-03 15:06:00 -0400

    fix translations for i18n 23

--- apps/client/src/test/mocks/handlers.ts
+++
@@ -1,9 +0,0 @@
-import { AuthPayload, LoginCredentials } from '@douglasneuroinformatics/common';
-import { rest } from 'msw';
-
-export const handlers = [
-  rest.post<LoginCredentials>('/v1/auth/login', (req, res, ctx) => {
-    sessionStorage.setItem('is-authenticated', 'true');
-    return res(ctx.status(200), ctx.json({ accessToken: 'foo' } satisfies AuthPayload));
-  })
-];
