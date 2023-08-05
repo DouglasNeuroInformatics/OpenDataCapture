1674d5967c0e4b36b44fcf62e7b4198f8a038868
joshunrau <contact@joshuaunrau.com>
2023-08-05 10:57:44 -0400

    rm docs url

--- web/src/components/Footer/Footer.tsx
+++ web/src/components/Footer/Footer.tsx
@@ -9,7 +9,6 @@ const CURRENT_YEAR = new Date().getFullYear();
 
 const DEV = import.meta.env.DEV;
 
-const DOCS_URL = import.meta.env.VITE_DOCS_URL;
 const LICENSE_URL = import.meta.env.VITE_LICENSE_URL;
 const SOURCE_URL = import.meta.env.DEV
   ? `${import.meta.env.VITE_GITHUB_REPO_URL}/tree/${import.meta.env.VITE_DEV_GIT_COMMIT!}`
@@ -37,14 +36,14 @@ export const Footer = ({ isLogin = false, showDevInfo = DEV }: FooterProps) => {
           <span className="mx-1">|</span>
           <LanguageToggle className="text-sm" />
           <span className="mx-1">|</span>
-          <a className="text-sm" href={DOCS_URL} rel="noreferrer" target="_blank">
+          <a className="text-sm" href="/docs" rel="noreferrer" target="_blank">
             {t('footer.documentation')}
           </a>
         </div>
       ) : (
         <div className="mb-3 flex flex-col justify-center gap-4 lg:flex-row">
           <LanguageToggle className="lg:hidden" />
-          <a className="text-center" href={DOCS_URL} rel="noreferrer" target="_blank">
+          <a className="text-center" href="/docs" rel="noreferrer" target="_blank">
             {t('footer.documentation')}
           </a>
           <a className="text-center" href={LICENSE_URL} rel="noreferrer" target="_blank">
--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -12,7 +12,6 @@ interface ImportMetaDevEnv {
 }
 
 interface ImportMetaEnv extends ImportMetaDevEnv {
-  readonly VITE_DOCS_URL: string;
   readonly VITE_LICENSE_URL: string;
   readonly VITE_GITHUB_REPO_URL: string;
   readonly VITE_CONTACT_EMAIL: string;
