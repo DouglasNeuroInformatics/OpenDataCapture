e81ec76a9d65b530b4331c3733a7e8db0debcef0
joshunrau <contact@joshuaunrau.com>
2023-08-09 10:48:40 -0400

    remove bcrypt

--- api/src/crypto/crypto.service.ts
+++ api/src/crypto/crypto.service.ts
@@ -3,8 +3,6 @@ import crypto from 'node:crypto';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
-import bcrypt from 'bcrypt';
-
 @Injectable()
 export class CryptoService {
   private readonly secretKey: string;
@@ -20,11 +18,27 @@ export class CryptoService {
       .digest('hex');
   }
 
-  async hashPassword(source: string): Promise<string> {
-    return bcrypt.hash(source, 10);
+  async hashPassword(password: string): Promise<string> {
+    return this.pbkdf2(password, this.genSalt());
   }
 
-  async comparePassword(source: string, hash: string): Promise<boolean> {
-    return bcrypt.compare(source, hash);
+  async comparePassword(password: string, hashedPassword: string): Promise<boolean> {
+    const salt = hashedPassword.split('$')[1];
+    return (await this.pbkdf2(password, salt)) === hashedPassword;
+  }
+
+  private async pbkdf2(password: string, salt: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      crypto.pbkdf2(password, salt, 100000, 32, 'sha512', (err, key) => {
+        if (err) {
+          reject(err);
+        }
+        resolve([key, salt].join('$'));
+      });
+    });
+  }
+
+  private genSalt(size = 16): string {
+    return crypto.randomBytes(size).toString('hex');
   }
 }
