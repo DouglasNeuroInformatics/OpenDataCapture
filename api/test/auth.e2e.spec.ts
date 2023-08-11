8d9cb303eee7a1cc7e27ccad7861cd9eaacdda99
joshunrau <contact@joshuaunrau.com>
2023-08-11 13:58:06 -0400

    add new e2e test for auth

--- api/test/auth.e2e.spec.ts
+++ api/test/auth.e2e.spec.ts
@@ -1,13 +1,18 @@
 import assert from 'node:assert/strict';
 import { after, before, describe, it } from 'node:test';
 
+import { HttpStatus } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { Test, TestingModule } from '@nestjs/testing';
 
+import request from 'supertest';
+
 import { AppModule } from '@/app.module.js';
+import { SetupService } from '@/setup/setup.service.js';
 
 describe('/auth', () => {
   let app: NestExpressApplication;
+  let server: any;
 
   before(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -17,11 +22,45 @@ describe('/auth', () => {
     app = moduleFixture.createNestApplication({
       logger: false
     });
+
     await app.init();
+    server = app.getHttpServer();
+
+    const setupService = app.get(SetupService);
+    await setupService.dropDatabase();
+    await setupService.createAdmin({
+      username: 'admin',
+      password: 'Password123'
+    });
   });
 
-  it('should be defined', () => {
-    assert(typeof app === 'object');
+  describe('POST /auth/login', () => {
+    it('should reject a request with an empty body', async () => {
+      const response = await request(server).post('/auth/login').send();
+      assert.strictEqual(response.status, HttpStatus.BAD_REQUEST);
+    });
+    it('should reject a request with a valid username but no password', async () => {
+      const response = await request(server).post('/auth/login').send({
+        username: 'admin'
+      });
+      assert.strictEqual(response.status, HttpStatus.BAD_REQUEST);
+    });
+    it('should reject a request that contains an invalid password', async () => {
+      const response = await request(server).post('/auth/login').send({
+        username: 'admin',
+        password: 'foo'
+      });
+      assert.strictEqual(response.status, HttpStatus.UNAUTHORIZED);
+      assert.strictEqual(response.body.message, 'Invalid password');
+    });
+    it('should return a JSON web token when provided the correct credentials', async () => {
+      const response = await request(server).post('/auth/login').send({
+        username: 'admin',
+        password: 'Password123'
+      });
+      assert.strictEqual(response.status, HttpStatus.OK);
+      assert(typeof response.body.accessToken === 'string');
+    });
   });
 
   after(async () => {
