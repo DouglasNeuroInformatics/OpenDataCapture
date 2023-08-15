178ed2ea71096e24f7c2a3682fdcbc06d28aa4d7
joshunrau <contact@joshuaunrau.com>
2023-08-15 18:19:49 -0400

    add sharp

--- marketing/astro.config.js
+++ marketing/astro.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    assets: '_assets'
+  },
+  compressHTML: true,
+  experimental: {
+    assets: true
+  },
+  image: {
+    service: sharpImageService()
+  },
   server: {
     port: parseInt(process.env.MARKETING_DEV_SERVER_PORT ?? 4000)
   }
