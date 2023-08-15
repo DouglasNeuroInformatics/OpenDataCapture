95e4403cea262f6be93b77f0cab82092b978bf11
joshunrau <contact@joshuaunrau.com>
2023-08-15 17:57:14 -0400

    adjust dev server port

--- marketing/astro.config.js
+++ marketing/astro.config.js
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: parseInt(process.env.MARKETING_DEV_SERVER_PORT ?? 4000)
+  }
+});
