fc77d6dedfc5dd7f090303c8b5bcd28cae60bb27
joshunrau <contact@joshuaunrau.com>
2023-08-16 11:31:27 -0400

    rename index

--- marketing/src/i18n/translations.ts
+++ marketing/src/i18n/translations.ts
@@ -1,16 +1,14 @@
 export const translations = {
-  index: {
-    meta: {
-      title: 'CliniCollect',
-      author: 'Douglas Neuroinformatics Platform',
-      description: {
-        en: 'An open-source, easy-to-use web application for electronic data capture',
-        fr: 'Une application web conviviale à source ouverte pour la saisie électronique des données'
-      },
-      keywords: {
-        en: 'Data Collection, Data Capture, Electronic Data Capture, Clinical, Research',
-        fr: 'Collecte de données, Saisie de données, Saisie électronique de données, Clinique, Recherche'
-      }
+  platform: {
+    title: 'CliniCollect',
+    author: 'Douglas Neuroinformatics Platform',
+    description: {
+      en: 'An open-source, easy-to-use web application for electronic data capture',
+      fr: 'Une application web conviviale à source ouverte pour la saisie électronique des données'
+    },
+    keywords: {
+      en: 'Data Collection, Data Capture, Electronic Data Capture, Clinical, Research',
+      fr: 'Collecte de données, Saisie de données, Saisie électronique de données, Clinique, Recherche'
     }
   },
   languageToggle: {
