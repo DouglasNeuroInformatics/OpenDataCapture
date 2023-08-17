0276967d8214614a4ea7b9fe4730ff8176ec8ad5
joshunrau <contact@joshuaunrau.com>
2023-08-17 14:14:46 -0400

    use insiders tailwind

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,16 +2,16 @@
 
 /** @type {import("prettier").Options} */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-astro")],
-  htmlWhitespaceSensitivity: "ignore",
+  plugins: [require.resolve('prettier-plugin-astro'), require.resolve('prettier-plugin-tailwindcss')],
+  htmlWhitespaceSensitivity: 'ignore',
   printWidth: 120,
   singleQuote: true,
-  trailingComma: "none",
+  trailingComma: 'none',
   overrides: [
     {
-      files: "*.astro",
+      files: '*.astro',
       options: {
-        parser: "astro"
+        parser: 'astro'
       }
     }
   ]
