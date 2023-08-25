f0956e6f81a0cd1e4a0c23d0a8044546dcedb572
joshunrau <contact@joshuaunrau.com>
2023-08-25 17:02:59 -0400

    refactor imports

--- web/src/Router.tsx
+++ web/src/Router.tsx
@@ -3,50 +3,42 @@ import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import { Layout } from './components';
-import { LoginPage } from './features/auth';
-import { ContactPage } from './features/contact';
-import {
-  AvailableInstrumentsPage,
-  CreateInstrumentPage,
-  FormPage,
-  ManageInstrumentsPage
-} from './features/instruments';
-import { OverviewPage } from './features/overview/pages/OverviewPage';
-import { AddVisitPage, SelectVisualizationPage, ViewSubjectsPage } from './features/subjects';
-import { UserPage } from './features/user';
+import * as AuthModule from './features/auth';
+import * as ContactModule from './features/contact';
+import * as InstrumentsModule from './features/instruments';
+import * as OverviewModule from './features/overview/pages/OverviewPage';
+import * as SubjectsModule from './features/subjects';
+import * as UserModule from './features/user';
 import { useAuthStore } from './stores/auth-store';
-
-import { SubjectRecordsGraphPage } from '@/features/subjects/pages/SubjectRecordsGraphPage';
-import { SubjectRecordsTablePage } from '@/features/subjects/pages/SubjectRecordsTablePage';
 
 export const Router = () => {
   const { accessToken } = useAuthStore();
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<LoginPage />} path="login" />
+        <Route element={<AuthModule.LoginPage />} path="login" />
         {accessToken ? (
           <Route element={<Layout />}>
-            <Route index element={<OverviewPage />} path="overview" />
-            <Route element={<ContactPage />} path="contact" />
-            <Route element={<UserPage />} path="user" />
+            <Route index element={<OverviewModule.OverviewPage />} path="overview" />
+            <Route element={<ContactModule.ContactPage />} path="contact" />
+            <Route element={<UserModule.UserPage />} path="user" />
             <Route path="subjects">
-              <Route element={<AddVisitPage />} path="add-visit" />
+              <Route element={<SubjectsModule.AddVisitPage />} path="add-visit" />
               <Route path="view-subjects">
-                <Route index element={<ViewSubjectsPage />} />
+                <Route index element={<SubjectsModule.ViewSubjectsPage />} />
                 <Route path=":subjectIdentifier">
-                  <Route index element={<SelectVisualizationPage />} />
-                  <Route element={<SubjectRecordsGraphPage />} path="graph" />
-                  <Route element={<SubjectRecordsTablePage />} path="table" />
+                  <Route index element={<SubjectsModule.SelectVisualizationPage />} />
+                  <Route element={<SubjectsModule.SubjectRecordsGraphPage />} path="graph" />
+                  <Route element={<SubjectsModule.SubjectRecordsTablePage />} path="table" />
                 </Route>
               </Route>
             </Route>
             <Route path="instruments">
-              <Route element={<AvailableInstrumentsPage />} path="available" />
-              <Route element={<ManageInstrumentsPage />} path="manage" />
-              <Route element={<CreateInstrumentPage />} path="create" />
+              <Route element={<InstrumentsModule.AvailableInstrumentsPage />} path="available" />
+              <Route element={<InstrumentsModule.ManageInstrumentsPage />} path="manage" />
+              <Route element={<InstrumentsModule.CreateInstrumentPage />} path="create" />
               <Route path="forms">
-                <Route element={<FormPage />} path=":id" />
+                <Route element={<InstrumentsModule.FormPage />} path=":id" />
               </Route>
             </Route>
           </Route>
