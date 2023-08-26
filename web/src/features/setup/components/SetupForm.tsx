e4ad6ec9e3c3f0adc8b71c045e78071154506609
joshunrau <contact@joshuaunrau.com>
2023-08-25 21:25:36 -0400

    update ui

--- web/src/features/setup/components/SetupForm.tsx
+++ web/src/features/setup/components/SetupForm.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import { Form } from '@douglasneuroinformatics/ui';
+import { useTranslation } from 'react-i18next';
 
 type SetupData = {
   adminUsername: string;
   adminPassword: string;
+  initDemo: boolean;
 };
 
 type SetupFormProps = {
@@ -12,20 +14,38 @@ type SetupFormProps = {
 };
 
 export const SetupForm = ({ onSubmit }: SetupFormProps) => {
+  const { t } = useTranslation();
   return (
     <Form<SetupData>
-      content={{
-        adminUsername: {
-          kind: 'text',
-          label: 'Admin Username',
-          variant: 'short'
+      content={[
+        {
+          title: t('setup.admin.title'),
+          description: t('setup.admin.description'),
+          fields: {
+            adminUsername: {
+              kind: 'text',
+              label: t('username'),
+              variant: 'short'
+            },
+            adminPassword: {
+              kind: 'text',
+              label: t('password'),
+              variant: 'password'
+            }
+          }
         },
-        adminPassword: {
-          kind: 'text',
-          label: 'Admin Password',
-          variant: 'password'
+        {
+          title: t('setup.demo.title'),
+          description: t('setup.demo.description'),
+          fields: {
+            initDemo: {
+              kind: 'binary',
+              label: t('setup.demo.init'),
+              variant: 'radio'
+            }
+          }
         }
-      }}
+      ]}
       errorMessages={{
         adminUsername: 'Please enter at least 1 character',
         adminPassword: 'Please enter at least 8 characters'
@@ -40,9 +60,12 @@ export const SetupForm = ({ onSubmit }: SetupFormProps) => {
           adminPassword: {
             type: 'string',
             minLength: 8
+          },
+          initDemo: {
+            type: 'boolean'
           }
         },
-        required: ['adminUsername', 'adminPassword']
+        required: ['adminUsername', 'adminPassword', 'initDemo']
       }}
       onSubmit={onSubmit}
     />
--- web/src/features/setup/pages/SetupPage.tsx
+++ web/src/features/setup/pages/SetupPage.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 import { FormPageWrapper } from '@douglasneuroinformatics/ui';
+import { useTranslation } from 'react-i18next';
 
 import { SetupForm } from '../components/SetupForm';
 
 import logo from '@/assets/logo.png';
 
 export const SetupPage = () => {
+  const { t } = useTranslation();
   return (
-    <FormPageWrapper languageOptions={['en', 'fr']} logo={logo} title="Setup">
+    <FormPageWrapper languageOptions={['en', 'fr']} logo={logo} title={t('setup.pageTitle')} widthMultiplier={1.5}>
       <SetupForm onSubmit={() => null} />
     </FormPageWrapper>
   );
