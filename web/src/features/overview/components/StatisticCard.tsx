56003cabb0736d28589260adf1f6c3091d768aa1
joshunrau <contact@joshuaunrau.com>
2023-08-27 13:51:19 -0400

    minor color changes

--- web/src/features/overview/components/StatisticCard.tsx
+++ web/src/features/overview/components/StatisticCard.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 
+import { Card } from '@douglasneuroinformatics/ui';
 import { type MotionValue, motion, useSpring, useTransform } from 'framer-motion';
 
 type StatisticCardProps = {
@@ -17,7 +18,7 @@ export const StatisticCard = ({ label, icon, value }: StatisticCardProps) => {
   }, [spring, value]);
 
   return (
-    <div className="flex w-full rounded-2xl bg-white p-4 text-slate-600 shadow-lg ring-1 ring-slate-900/5 dark:bg-slate-800 dark:text-slate-300 dark:ring-slate-300/5">
+    <Card className="flex w-full rounded-lg p-4">
       {icon && <div className="mr-2 flex items-center justify-center text-5xl">{icon}</div>}
       <div className="w-full">
         <motion.h3 className="title-font text-3xl font-medium text-slate-900 dark:text-slate-100 sm:text-4xl">
@@ -25,6 +26,6 @@ export const StatisticCard = ({ label, icon, value }: StatisticCardProps) => {
         </motion.h3>
         <p className="leading-relaxed">{label}</p>
       </div>
-    </div>
+    </Card>
   );
 };
