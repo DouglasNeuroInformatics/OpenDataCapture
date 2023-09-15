bcce8be3beac031508cde8c76018dcaeaf3e869d
joshunrau <contact@joshuaunrau.com>
2023-09-15 13:26:51 -0400

    switch back to npm and set rate throttles

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -49,6 +49,17 @@ import { UsersModule } from './users/users.module.js';
     SubjectsModule,
     ThrottlerModule.forRoot([
       {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20
+      },
+      {
+        name: 'long',
         ttl: 60000,
         limit: 100
       }
