d3e068a8013f2ab0e8f09d7b85286b2c2bd11684
joshunrau <contact@joshuaunrau.com>
2023-09-15 12:35:29 -0400

    yay

--- apps/gateway/src/web/web.controller.tsx
+++ apps/gateway/src/web/web.controller.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import { renderToStaticMarkup } from 'react-dom/server';
+import { renderToReadableStream } from 'react-dom/server';
 
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, StreamableFile } from '@nestjs/common';
+import { type Response } from 'express';
+
+import { WebService } from './web.service';
 
 @Controller()
 export class WebController {
+  // Until Bun implements metadata
+  private readonly webService;
+
+  constructor() {
+    this.webService = new WebService();
+  }
+
   @Get('*')
-  render() {
-    return renderToStaticMarkup(
-      <html lang="en">
-        <head>
-          <meta charSet="UTF-8" />
-          <link href="/favicon.ico" rel="icon" type="image/svg+xml" />
-          <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-          <title>Open Data Capture</title>
-          <link href="main.css" rel="stylesheet" />
-        </head>
-        <body className="bg-slate-100 text-slate-900 dark:bg-slate-900 dark:text-slate-100">
-          <h1>Hello World</h1>
-        </body>
-      </html>
-    );
+  async render(@Res({ passthrough: true }) response: Response) {
+    const stream = await renderToReadableStream(this.webService.root(), {
+      bootstrapScripts: ['/main.js']
+    });
+    const arrBuf = await Bun.readableStreamToArrayBuffer(stream);
+    const buffer = Buffer.from(arrBuf);
+    response.setHeader('Content-Type', ' text/html');
+    return new StreamableFile(buffer);
   }
 }
