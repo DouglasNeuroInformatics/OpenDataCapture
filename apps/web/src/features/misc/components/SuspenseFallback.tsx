110c4c2756e1256cef0480b34b7e59856b11baa1
david roper <davidroper321@gmail.com>
2023-09-19 16:11:18 -0400

    Issue #552 animation div for spinner

--- apps/web/src/features/misc/components/SuspenseFallback.tsx
+++ apps/web/src/features/misc/components/SuspenseFallback.tsx
@@ -1,7 +1,14 @@
+import { motion } from 'framer-motion'
+
 import { Spinner } from '@/components';
 
 export const SuspenseFallback = () => (
-  <div className="flex h-screen w-screen items-center justify-center">
+  <motion.div 
+  animate = {{opacity: 1}}
+  className="flex h-screen w-screen items-center justify-center"
+  exit = {{opacity: 0}}
+  initial = {{opacity: 0}}
+  transition={{ease: "ease-out", delay: 0.2, duration: 1}}>
     <Spinner />
-  </div>
+  </motion.div>
 );
