384214586c0f1db6465906b475cb44a15f321e20
david-roper <davidroper321@gmail.com>
2023-09-20 15:51:25 -0400

    #275 added picture render function

--- apps/web/src/features/instruments/components/InstrumentShowcase/InstrumentCard.tsx
+++ apps/web/src/features/instruments/components/InstrumentShowcase/InstrumentCard.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { HiPencilSquare } from 'react-icons/hi2';
 import { useNavigate } from 'react-router-dom';
 
+import handBrain from '@/assets/handbrain.svg';
 import toolBrain from '@/assets/toolbrain.svg';
 
 
@@ -18,6 +19,16 @@ export const InstrumentCard = ({ instrument }: InstrumentCardProps) => {
     navigate(`../forms/${instrument.identifier}`);
   };
 
+  const pictureRender = () => {
+    if(instrument.name.includes("Questionnaire")){
+      return <HiPencilSquare className="h-8 w-8" />
+    } else if (instrument.name.includes("Assessment")){
+      return <img alt="tool brain" className="h-16 w-16 rounded-full"  src={toolBrain}/>
+    } 
+    return <img alt="tool brain" className="h-16 w-16 rounded-full"  src={handBrain}/>
+
+  };
+
   return (
     <div
       className="relative transition-all duration-300 ease-in-out hover:scale-[1.03] hover:cursor-pointer hover:shadow-lg"
@@ -28,11 +39,7 @@ export const InstrumentCard = ({ instrument }: InstrumentCardProps) => {
     >
       <div className="flex flex-col rounded-lg border-2 border-slate-200 border-opacity-50 p-8 dark:border-slate-700 sm:flex-row">
         <div className="mb-4 inline-flex h-16 w-16 flex-shrink-0 items-center justify-center rounded-full bg-indigo-100 text-indigo-500 sm:mb-0 sm:mr-8">
-          {(instrument.name.includes("Questionnaire")) ?
-             <HiPencilSquare className="h-8 w-8" /> : <img alt="tool brain" className="h-16 w-16 rounded-full"  src={toolBrain}/>
-          }
-          
-          
+          {pictureRender()}
         </div>
         <div className="flex-grow">
           <h3 className="title-font mb-1 text-lg font-semibold text-slate-900 dark:text-slate-100">
