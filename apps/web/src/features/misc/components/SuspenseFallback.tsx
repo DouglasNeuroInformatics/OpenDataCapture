0c12215869e89eb078c41c83ae6903a313aee956
david-roper <davidroper321@gmail.com>
2023-09-22 11:49:52 -0400

    Closes #553 add animate presence to suspensefallback

--- apps/web/src/features/misc/components/SuspenseFallback.tsx
+++ apps/web/src/features/misc/components/SuspenseFallback.tsx
@@ -1,14 +1,16 @@
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 
 import { LoadingFallback } from './LoadingFallback';
 
 export const SuspenseFallback = () => (
-  <motion.div 
+  <AnimatePresence>
+    {<motion.div 
   animate = {{opacity: 1}}
   className="flex h-screen w-screen items-center justify-center"
   exit = {{opacity: 0}}
   initial = {{opacity: 0}}
-  transition={{ease: "ease-out", delay: 0.2, duration: 1}}>
+  transition={{ease: "ease-out", delay: 0.1, duration: 1}}>
     <LoadingFallback/>
-  </motion.div>
+  </motion.div>}
+  </AnimatePresence>
 );
