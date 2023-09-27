34ea1e0e45781bd8e03fcfdd37c8669fff08ff27
david-roper <davidroper321@gmail.com>
2023-09-27 16:49:35 -0400

    feat #551 fill in subject in questionnaire test

--- apps/web/cypress/e2e/test.cy.ts
+++ apps/web/cypress/e2e/test.cy.ts
@@ -1,8 +1,18 @@
-
-  describe('look at questionaire', () => {
+describe('look at questionaire', () => {
     it('passes', () => {
       
       cy.login('david','Password123')
+      cy.get('span').contains('Add Visit').click({force: true})
+      cy.get('input[name=firstName]').type('test')
+      cy.get('input[name=lastName]').type('patient')
+      cy.get('input[class=field-input]').type('2023-09-21')
+      cy.get('button[class="field-input capitalize"]').click({force:true})
+      cy.get('body').click()
+      cy.get('li').contains('Male').click()
+      cy.get('button').contains('Submit').click()
+
+      cy.wait(2000)
+
       cy.get('span').contains('View Instruments').click({force: true})
       cy.url().should('include', 'instruments/available')
       cy.wait(2000)
