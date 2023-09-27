607f0106c2b0fdaea597c46e95de4134e5c1be8b
david-roper <davidroper321@gmail.com>
2023-09-27 16:23:05 -0400

    feat: #551 login and view questionnaire test

--- apps/web/cypress/e2e/spec.cy.ts
+++ apps/web/cypress/e2e/spec.cy.ts
@@ -1,9 +1,8 @@
 describe('login test', () => {
   it('passes', () => {
-    cy.visit('http://127.0.0.1:3000/auth/login')
-    cy.get('input[name="username"]').type('david')
-    cy.get('input[name="password"]').type('Password123')
-    cy.get('button').contains('Login').click()
+    
+    cy.login('david','Password123')
+    
     cy.url().should('include','overview')
     
   })
--- apps/web/cypress/e2e/test.cy.ts
+++ apps/web/cypress/e2e/test.cy.ts
@@ -1,9 +1,11 @@
-describe('look at stuff', () => {
+
+  describe('look at questionaire', () => {
     it('passes', () => {
       
       cy.login('david','Password123')
-      
-      cy.url().should('include','overview')
-      
+      cy.get('span').contains('View Instruments').click({force: true})
+      cy.url().should('include', 'instruments/available')
+      cy.wait(2000)
+      cy.get('div').contains('Questionnaire').first().click()
     })
   })
