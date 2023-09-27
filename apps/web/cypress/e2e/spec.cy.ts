1251740194536dc9fc9e20f55c778cb21438faa4
david-roper <davidroper321@gmail.com>
2023-09-27 14:29:40 -0400

    feat #551 adding cypress for e2e tests

---
+++ apps/web/cypress/e2e/spec.cy.ts
@@ -0,0 +1,8 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://127.0.0.1:3000/auth/login')
+    cy.get('input[name="username"]').type('david')
+    cy.get('input[name="password"]').type('Password123')
+    cy.get('button').contains('Login').click()
+  })
+})
