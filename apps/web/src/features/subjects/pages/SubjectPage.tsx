acaee8209da3a530af2b480c098e658351b9f084
joshunrau <contact@joshuaunrau.com>
2023-09-29 14:55:43 -0400

    adjust subject page title

--- apps/web/src/features/subjects/pages/SubjectPage.tsx
+++ apps/web/src/features/subjects/pages/SubjectPage.tsx
@@ -1,16 +1,18 @@
 import { useTranslation } from 'react-i18next';
 import { RxBarChart, RxTable } from 'react-icons/rx';
+import { useParams } from 'react-router-dom';
 
 import { VisualizationMode } from '../components/VisualizationMode';
 
 import { PageHeader } from '@/components';
 
 export const SubjectPage = () => {
+  const params = useParams();
   const { t } = useTranslation();
 
   return (
     <div>
-      <PageHeader title={t('visualization.selectMode')} />
+      <PageHeader title={`${t('subjectPage.pageTitle')}: ${params.subjectIdentifier!.slice(0, 7)}`} />
       <div className="my-16 flex items-center justify-center gap-8">
         <VisualizationMode icon={<RxTable />} title={t('table')} to="table" />
         <VisualizationMode icon={<RxBarChart />} title={t('graph')} to="graph" />
