855a036a86cce3d22ff381ffc188754b9f1e53e1
joshunrau <contact@joshuaunrau.com>
2023-09-29 13:26:59 -0400

    increase throttle limits

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -57,17 +57,17 @@ import { UsersModule } from './users/users.module';
       {
         name: 'short',
         ttl: 1000,
-        limit: 10
+        limit: 25
       },
       {
         name: 'medium',
         ttl: 10000,
-        limit: 25
+        limit: 100
       },
       {
         name: 'long',
         ttl: 60000,
-        limit: 100
+        limit: 250
       }
     ]),
     UsersModule,
