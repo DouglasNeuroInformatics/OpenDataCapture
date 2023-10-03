42fd5e6dd24e87c20c176b9bdde67acc40701a68
david-roper <davidroper321@gmail.com>
2023-10-03 10:40:10 -0400

    feat: fix tags test

--- apps/web/cypress/e2e/spec.cy.ts
+++ apps/web/cypress/e2e/spec.cy.ts
@@ -24,12 +24,12 @@ describe('tags test', () => {
     cy.get('span').contains('Well-Being').click({force: true})
     cy.get('[data-cy="instrument-card"]').first().contains('Tags: Well-Being')
 
-    cy.get('button').contains('Tags').click()
+   
     cy.get('span').contains('Well-Being').click({force: true})
     cy.get('span').contains('Cognitive').click({force: true})
     cy.get('[data-cy="instrument-card"]').first().contains('Tags: Cognitive')
 
-    cy.get('button').contains('Tags').click()
+   
     cy.get('span').contains('Cognitive').click({force: true})
     cy.get('span').contains('Demographics').click({force: true})
     cy.get('[data-cy="instrument-card"]').first().contains('Tags: Demographics')
