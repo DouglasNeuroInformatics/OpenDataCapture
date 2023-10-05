184f300d1a278b705fe06bc75bc33993749078d4
joshunrau <contact@joshuaunrau.com>
2023-10-05 08:17:25 -0400

    add new config

---
+++ eslint.config.js
@@ -0,0 +1,64 @@
+// @ts-check
+
+import path from 'node:path';
+import url from 'node:url';
+
+import { createConfig } from '@douglasneuroinformatics/eslint-config';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const baseConfig = createConfig({
+  base: {
+    env: 'node'
+  },
+  ts: {
+    project: path.resolve(__dirname, 'tsconfig.base.json')
+  }
+});
+
+const apiConfig = createConfig({
+  base: {
+    env: 'node',
+    filesRoot: 'apps/api/src'
+  },
+  ts: {
+    project: path.resolve(__dirname, 'apps', 'api', 'tsconfig.json')
+  }
+});
+
+// const gatewayConfig = createConfig({
+//   base: {
+//     env: 'node',
+//     filesRoot: 'apps/gateway/src'
+//   },
+//   jsx: true,
+//   ts: {
+//     project: path.resolve(__dirname, 'apps', 'gateway', 'tsconfig.json')
+//   }
+// });
+
+// const marketingConfig = createConfig({
+//   astro: true,
+//   base: {
+//     env: 'node',
+//     filesRoot: 'apps/marketing/src'
+//   },
+//   jsx: true,
+//   ts: {
+//     project: path.resolve(__dirname, 'apps', 'gateway', 'tsconfig.json')
+//   }
+// });
+
+const webConfig = createConfig({
+  base: {
+    env: 'browser',
+    filesRoot: 'apps/web/src'
+  },
+  jsx: true,
+  ts: {
+    project: path.resolve(__dirname, 'apps', 'web', 'tsconfig.json')
+  }
+});
+
+export default [...baseConfig, ...apiConfig, ...webConfig];
--- packages/demo/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  extends: ['@douglasneuroinformatics'],
-  parserOptions: {
-    project: path.resolve(__dirname, 'tsconfig.json')
-  }
-};
