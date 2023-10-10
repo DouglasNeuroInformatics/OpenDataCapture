3c5e39ac13d68d2c21f001613bc3d4292df96c58
david-roper <davidroper321@gmail.com>
2023-10-10 16:18:02 -0400

    feat: #551 generalized tags test

--- apps/web/cypress/e2e/spec.cy.ts
+++ apps/web/cypress/e2e/spec.cy.ts
@@ -21,18 +21,48 @@ describe('tags test', () => {
     
     cy.get('span[data-cy="view-instrument"]').first().click({force: true})
     cy.get('div[data-cy="tags"]').click()
-    cy.get('span').contains('Well-Being').click({force: true})
-    cy.get('h5[data-cy="instrument-card"]').first().should('have.text','Tags: Well-Being')
+    cy.get('li[id*="headlessui-listbox-option-:"]').eq(0).click({force: true})
+    
+    let text1: string;
+    cy.get('span[class="ui-selected:font-medium"]').eq(0).should(($span) => {
+       text1 = $span.text()
+    })
+    cy.get('h5[data-cy="instrument-card"]').eq(0).should(($h5) =>
+    {
+      const spanText1 = $h5.text();
+      expect(spanText1).to.contain(text1);
+    })
 
    
-    cy.get('span').contains('Well-Being').click({force: true})
-    cy.get('span').contains('Cognitive').click({force: true})
+    cy.get('li[id*="headlessui-listbox-option-:"]').eq(0).click({force: true})
+    cy.get('li[id*="headlessui-listbox-option-:"]').eq(1).click({force: true})
+
+    let text2: string;
+    cy.get('span[class="ui-selected:font-medium"]').eq(1).should(($span) => {
+       text2 = $span.text()
+    })
+    cy.get('h5[data-cy="instrument-card"]').eq(0).should(($h5) =>
+    {
+      const spanText2= $h5.text();
+      expect(spanText2).to.contain(text2);
+    })
     cy.get('h5[data-cy="instrument-card"]').first().should('have.text','Tags: Cognitive')
 
    
-    cy.get('span').contains('Cognitive').click({force: true})
-    cy.get('span').contains('Demographics').click({force: true})
-    cy.get('h5[data-cy="instrument-card"]').first().should('have.text','Tags: Demographics')
+    cy.get('li[id*="headlessui-listbox-option-:"]').eq(1).click({force: true})
+    cy.get('li[id*="headlessui-listbox-option-:"]').eq(2).click({force: true})
+   
+
+    let text3: string;
+    cy.get('span[class="ui-selected:font-medium"]').eq(2).should(($span) => {
+       text3 = $span.text()
+    })
+    cy.get('h5[data-cy="instrument-card"]').eq(0).should(($h5) =>
+    {
+      const spanText3 = $h5.text();
+      expect(spanText3).to.contain(text3);
+    })
+    
 
 
   })
