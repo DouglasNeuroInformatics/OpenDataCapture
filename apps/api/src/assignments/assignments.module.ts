013330165aa3ec3ad2696cfb640526f0cab6fcd6
joshunrau <contact@joshuaunrau.com>
2023-10-11 17:28:00 -0400

    import mongoose

--- apps/api/src/assignments/assignments.module.ts
+++ apps/api/src/assignments/assignments.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AssignmentsController } from './assignments.controller';
 import { AssignmentsService } from './assignments.service';
+import { AssignmentEntity, AssignmentSchema } from './entities/assignment.entity';
 
 @Module({
   controllers: [AssignmentsController],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: AssignmentEntity.modelName,
+        schema: AssignmentSchema
+      }
+    ])
+  ],
   providers: [AssignmentsService]
 })
 export class AssignmentsModule {}
