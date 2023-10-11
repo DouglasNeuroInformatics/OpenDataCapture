8f9c1d7216cb744c67d02ef7f5719be58bfe9f1d
joshunrau <contact@joshuaunrau.com>
2023-10-11 18:08:03 -0400

    add translations on management page

--- apps/web/src/features/subjects/pages/SubjectManagementPage.tsx
+++ apps/web/src/features/subjects/pages/SubjectManagementPage.tsx
@@ -3,37 +3,37 @@ import { useState } from 'react';
 import { ClientTable, Slider } from '@douglasneuroinformatics/ui';
 import { toBasicISOString } from '@douglasneuroinformatics/utils';
 import type { Assignment } from '@open-data-capture/types';
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 export const SubjectManagementPage = () => {
-  // const { t } = useTranslation();
+  const { t } = useTranslation('subjects');
   const [selectedAssignment, setSelectedAssignment] = useState<Assignment | null>(null);
 
   return (
     <div>
       <div className="my-5">
-        <h3 className="text-lg font-semibold">Assigned Instruments</h3>
+        <h3 className="text-lg font-semibold">{t('subjectManagementPage.assignedInstruments')}</h3>
       </div>
       <ClientTable<Assignment>
         columns={[
           {
             field: 'title',
-            label: 'Title'
+            label: t('subjectManagementPage.tableColumns.title')
           },
           {
             field: 'timeAssigned',
             formatter: (value: number) => toBasicISOString(new Date(value)),
-            label: 'Date Assigned'
+            label: t('subjectManagementPage.tableColumns.timeAssigned')
           },
           {
             field: 'timeExpires',
             formatter: (value: number) => toBasicISOString(new Date(value)),
-            label: 'Date Expires'
+            label: t('subjectManagementPage.tableColumns.timeExpires')
           },
           {
             field: 'status',
             formatter: (value: string) => value.charAt(0) + value.slice(1).toLowerCase(),
-            label: 'Status'
+            label: t('subjectManagementPage.tableColumns.status')
           }
         ]}
         data={[
