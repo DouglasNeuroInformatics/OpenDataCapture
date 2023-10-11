d7236ea43d025d05c7a6860a8b19b5f7b1fe81c2
joshunrau <contact@joshuaunrau.com>
2023-10-11 14:40:51 -0400

    use config module

---
+++ apps/gateway/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    })
+  ]
+})
+export class AppModule {}
---
+++ apps/gateway/src/main.ts
@@ -0,0 +1,24 @@
+import path from 'node:path';
+
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { type NestExpressApplication } from '@nestjs/platform-express';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+  app.useStaticAssets(path.resolve(import.meta.dir, '..', 'public'));
+
+  const configService = app.get(ConfigService);
+
+  const isProduction = configService.get<string>('NODE_ENV') === 'production';
+  const port = isProduction ? 80 : parseInt(configService.getOrThrow('GATEWAY_DEV_SERVER_PORT'));
+
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+
+void bootstrap();
