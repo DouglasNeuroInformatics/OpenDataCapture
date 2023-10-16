aadf2ecc4fc8aa6f3d3bb6b112dd97e4f0e39b25
joshunrau <contact@joshuaunrau.com>
2023-10-16 15:43:31 -0400

    add login req schema

--- apps/api/src/auth/dto/login-request.dto.ts
+++ apps/api/src/auth/dto/login-request.dto.ts
@@ -1,13 +1,21 @@
+/* eslint-disable perfectionist/sort-classes */
+/* eslint-disable perfectionist/sort-objects */
+
+import { ValidationSchema } from '@douglasneuroinformatics/nestjs/core';
 import { ApiProperty } from '@nestjs/swagger';
 import type { LoginCredentials } from '@open-data-capture/types';
-import { IsString } from 'class-validator';
+import { z } from 'zod';
 
+@ValidationSchema(
+  z.object({
+    username: z.string(),
+    password: z.string()
+  })
+)
 export class LoginRequestDto implements LoginCredentials {
-  @ApiProperty({ example: 'password' })
-  @IsString()
-  password: string;
-
   @ApiProperty({ example: 'admin' })
-  @IsString()
   username: string;
+
+  @ApiProperty({ example: 'password' })
+  password: string;
 }
