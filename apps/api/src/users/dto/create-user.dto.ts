0fd5a53f3b6707efad7e4244db30f5275cc5ff44
joshunrau <contact@joshuaunrau.com>
2023-10-16 14:14:33 -0400

    add createUserDtoSchema

--- apps/api/src/users/dto/create-user.dto.ts
+++ apps/api/src/users/dto/create-user.dto.ts
@@ -1,56 +1,56 @@
+import { ValidationSchema } from '@douglasneuroinformatics/nestjs/core';
 import { ApiProperty } from '@nestjs/swagger';
 import type { BasePermissionLevel, User } from '@open-data-capture/types';
-import { ArrayMinSize, IsIn, IsOptional, IsString, Matches } from 'class-validator';
+import { type ZodType, z } from 'zod';
 
 type CreateUserData = {
   groupNames?: string[];
 } & Omit<User, 'groups' | 'preferences'>;
 
+const basePermissionLevels = ['ADMIN', 'GROUP_MANAGER', 'STANDARD'] as const;
+
 // Matches string with 8 or more characters, minimum one upper case, lowercase, and number
 const isStrongPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
-const basePermissionLevels = ['ADMIN', 'GROUP_MANAGER', 'STANDARD'] satisfies BasePermissionLevel[];
+export const createUserDtoSchema: ZodType<CreateUserData> = z.object({
+  basePermissionLevels: z.enum(basePermissionLevels).optional(),
+  firstName: z.string().optional(),
+  groupNames: z.array(z.string()).min(1).optional(),
+  lastName: z.string().optional(),
+  password: z.string().regex(isStrongPassword),
+  username: z.string().min(1)
+});
 
+@ValidationSchema(createUserDtoSchema)
 export class CreateUserDto implements CreateUserData {
   @ApiProperty({
     description: "Determines the user's base permissions, which may later be modified by an admin",
     enum: basePermissionLevels,
     type: String
   })
-  @IsOptional()
-  @IsIn(basePermissionLevels)
   basePermissionLevel?: BasePermissionLevel;
 
   @ApiProperty({ description: 'First Name' })
-  @IsOptional()
-  @IsString()
   firstName?: string;
 
   @ApiProperty({
     description: 'The names of the group(s) to which the user belongs',
     example: ['Memory Clinic', 'Depression Clinic']
   })
-  @IsOptional()
-  @IsString({ each: true })
-  @ArrayMinSize(1)
   groupNames?: string[];
 
   @ApiProperty({ description: 'Last Name' })
-  @IsOptional()
-  @IsString()
   lastName?: string;
 
   @ApiProperty({
     description: 'A minimum of 8 characters, including one number, one upper case, and one lower case letter',
     example: 'TheMinimumLengthIs8ButThereIsNotAMaximumLength'
   })
-  @Matches(isStrongPassword)
   password: string;
 
   @ApiProperty({
     description: 'A unique descriptive name associated with this user',
     example: 'JaneDoeMemoryClinic'
   })
-  @IsString()
   username: string;
 }
