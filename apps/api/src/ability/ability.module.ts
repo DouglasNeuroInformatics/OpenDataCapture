4f932a11d7b7586eaae05171911b9248501d56b9
joshunrau <contact@joshuaunrau.com>
2023-10-17 12:42:15 -0400

    make ability glboal

--- apps/api/src/ability/ability.module.ts
+++ apps/api/src/ability/ability.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 
 import { AbilityFactory } from './ability.factory';
 
+@Global()
 @Module({
   exports: [AbilityFactory],
   providers: [AbilityFactory]
