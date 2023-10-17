48871bc145951cbc4b13b56a5a8707a193cf4023
joshunrau <contact@joshuaunrau.com>
2023-10-17 12:20:32 -0400

    resolve req scope in auth

--- apps/api/src/auth/strategies/jwt.strategy.ts
+++ apps/api/src/auth/strategies/jwt.strategy.ts
@@ -1,5 +1,6 @@
 import { Injectable, Logger, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ContextIdFactory, ModuleRef } from '@nestjs/core';
 import { PassportStrategy } from '@nestjs/passport';
 import type { JwtPayload } from '@open-data-capture/types';
 import type { Request } from 'express';
@@ -14,36 +15,38 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   private readonly logger = new Logger(JwtStrategy.name);
 
   constructor(
-    config: ConfigService,
-    private readonly abilityFactory: AbilityFactory,
-    private readonly usersService: UsersService
+    configService: ConfigService,
+    private readonly moduleRef: ModuleRef
   ) {
     super({
-      ignoreExpiration: config.getOrThrow('NODE_ENV') === 'development',
+      ignoreExpiration: configService.getOrThrow('NODE_ENV') === 'development',
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: config.getOrThrow<string>('SECRET_KEY')
+      passReqToCallback: true,
+      secretOrKey: configService.getOrThrow<string>('SECRET_KEY')
     });
   }
 
   /** This method is called after the token is validated by passport  */
-  async validate({ username }: JwtPayload): Promise<Request['user']> {
-    const user = await this.getUser(username);
-    const ability = this.abilityFactory.createForUser(user);
-    this.logger.verbose(`Validated Token for User: ${username}`);
-    return { ...user, ability };
-  }
+  async validate(request: Request, { username }: JwtPayload): Promise<Request['user']> {
+    // First, we need to resolve the module for this request scope
+    // For more info, see: https://docs.nestjs.com/recipes/passport#request-scoped-strategies
+    const contextId = ContextIdFactory.getByRequest(request);
+    const abilityFactory = await this.moduleRef.resolve(AbilityFactory, contextId, { strict: false });
+    const usersService = await this.moduleRef.resolve(UsersService, contextId, { strict: false });
 
-  /** Returns the user associated with the JWT if they exist, otherwise throws UnauthorizedException */
-  private async getUser(username: string): Promise<UserEntity> {
+    // Get the user associated with the JWT if they exist, otherwise throws UnauthorizedException
     let user: UserEntity;
     try {
-      user = await this.usersService.findByUsername(username);
+      user = await usersService.findByUsername(username);
     } catch (error) {
       if (error instanceof NotFoundException) {
         throw new UnauthorizedException(`Token is valid, but user does not exist: ${username}`);
       }
       throw error;
     }
-    return user;
+
+    const ability = abilityFactory.createForUser(user);
+    this.logger.verbose(`Validated Token for User: ${username}`);
+    return { ...user, ability };
   }
 }
