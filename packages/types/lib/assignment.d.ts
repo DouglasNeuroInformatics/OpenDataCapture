5d67f066264e0706c799ab96c6fc51f9bf90e07b
joshunrau <contact@joshuaunrau.com>
2023-10-18 15:19:01 -0400

    use ids in create data

--- packages/types/lib/assignment.d.ts
+++ packages/types/lib/assignment.d.ts
@@ -10,6 +10,6 @@ export type Assignment = {
 };
 
 export type CreateAssignmentData = {
-  instrumentIdentifier: string;
-  subjectIdentifier: string;
+  instrumentId: string;
+  subjectId: string;
 };
