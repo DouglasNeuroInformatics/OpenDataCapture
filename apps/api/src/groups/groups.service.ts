a71d7aab5053f2f971f1c9b8f27d4bc8ce12d4b2
joshunrau <contact@joshuaunrau.com>
2023-10-18 14:07:29 -0400

    add validate ability option

--- apps/api/src/groups/groups.service.ts
+++ apps/api/src/groups/groups.service.ts
@@ -1,4 +1,4 @@
-import type { EntityService } from '@douglasneuroinformatics/nestjs/core';
+import { EntityService } from '@douglasneuroinformatics/nestjs/core';
 import { ConflictException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import type { Group } from '@open-data-capture/types';
 
@@ -9,11 +9,13 @@ import { UpdateGroupDto } from './dto/update-group.dto';
 import { GroupsRepository } from './groups.repository';
 
 @Injectable()
-export class GroupsService implements EntityService<Group> {
+export class GroupsService extends EntityService<Group, { validateAbility?: boolean }> {
   constructor(
     private readonly abilityService: AbilityService,
     private readonly groupsRepository: GroupsRepository
-  ) {}
+  ) {
+    super();
+  }
 
   async create(group: CreateGroupDto) {
     if (await this.groupsRepository.exists({ name: group.name })) {
@@ -22,35 +24,38 @@ export class GroupsService implements EntityService<Group> {
     return this.groupsRepository.create(group);
   }
 
-  async deleteById(id: string) {
+  async deleteById(id: string, { validateAbility = true }) {
     const group = await this.groupsRepository.findById(id);
     if (!group) {
       throw new NotFoundException(`Failed to find group with ID: ${id}`);
-    } else if (!this.abilityService.can('delete', group)) {
+    } else if (validateAbility && !this.abilityService.can('delete', group)) {
       throw new ForbiddenException(`Insufficient rights to delete group with ID: ${id}`);
     }
     return (await this.groupsRepository.deleteById(id))!;
   }
 
-  async findAll() {
+  async findAll({ validateAbility = true }) {
+    if (!validateAbility) {
+      return this.groupsRepository.find();
+    }
     return this.groupsRepository.find(this.abilityService.accessibleQuery('read'));
   }
 
-  async findById(id: string) {
+  async findById(id: string, { validateAbility = true }) {
     const group = await this.groupsRepository.findById(id);
     if (!group) {
       throw new NotFoundException(`Failed to find group with ID: ${id}`);
-    } else if (!this.abilityService.can('delete', group)) {
+    } else if (validateAbility && !this.abilityService.can('delete', group)) {
       throw new ForbiddenException(`Insufficient rights to read group with ID: ${id}`);
     }
     return group;
   }
 
-  async updateById(id: string, update: UpdateGroupDto) {
+  async updateById(id: string, update: UpdateGroupDto, { validateAbility = true }) {
     const group = await this.groupsRepository.findById(id);
     if (!group) {
       throw new NotFoundException(`Failed to find group with ID: ${id}`);
-    } else if (!this.abilityService.can('update', group)) {
+    } else if (validateAbility && !this.abilityService.can('update', group)) {
       throw new ForbiddenException(`Insufficient rights to update group with ID: ${id}`);
     }
     return (await this.groupsRepository.updateById(id, update))!;
