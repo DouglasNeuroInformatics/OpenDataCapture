90587ebfcb5ba2de24796e99a5e9de175bb62896
joshunrau <contact@joshuaunrau.com>
2023-10-18 11:33:20 -0400

    validate update dto

--- apps/api/src/groups/dto/create-group.dto.ts
+++ apps/api/src/groups/dto/create-group.dto.ts
@@ -3,11 +3,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { type Group } from '@open-data-capture/types';
 import { z } from 'zod';
 
-@ValidationSchema(
-  z.object({
-    name: z.string().min(1)
-  })
-)
+export const CreateGroupDtoSchema = z.object({
+  name: z.string().min(1)
+});
+
+@ValidationSchema(CreateGroupDtoSchema)
 export class CreateGroupDto implements Omit<Group, 'id'> {
   @ApiProperty({ example: 'Depression Clinic' })
   name: string;
--- apps/api/src/groups/dto/update-group.dto.ts
+++ apps/api/src/groups/dto/update-group.dto.ts
@@ -1,8 +1,12 @@
+import { ValidationSchema } from '@douglasneuroinformatics/nestjs/core';
 import { PartialType } from '@nestjs/swagger';
 import { type Group } from '@open-data-capture/types';
 
-import { CreateGroupDto } from './create-group.dto';
+import { CreateGroupDto, CreateGroupDtoSchema } from './create-group.dto';
 
+const UpdateGroupDtoSchema = CreateGroupDtoSchema.partial();
+
+@ValidationSchema(UpdateGroupDtoSchema)
 export class UpdateGroupDto extends PartialType(CreateGroupDto) implements Partial<Group> {
   name?: string;
 }
