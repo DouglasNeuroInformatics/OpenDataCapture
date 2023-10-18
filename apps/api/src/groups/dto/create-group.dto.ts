e9e125c57c4a90456401e7f8abab4b1d5575e6f2
joshunrau <contact@joshuaunrau.com>
2023-10-18 12:54:55 -0400

    rename schemas

--- apps/api/src/groups/dto/create-group.dto.ts
+++ apps/api/src/groups/dto/create-group.dto.ts
@@ -3,11 +3,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { type Group } from '@open-data-capture/types';
 import { z } from 'zod';
 
-export const CreateGroupDtoSchema = z.object({
+export const CreateGroupDataSchema = z.object({
   name: z.string().min(1)
 });
 
-@ValidationSchema(CreateGroupDtoSchema)
+@ValidationSchema(CreateGroupDataSchema)
 export class CreateGroupDto implements Omit<Group, 'id'> {
   @ApiProperty({ example: 'Depression Clinic' })
   name: string;
--- apps/api/src/groups/dto/update-group.dto.ts
+++ apps/api/src/groups/dto/update-group.dto.ts
@@ -2,11 +2,9 @@ import { ValidationSchema } from '@douglasneuroinformatics/nestjs/core';
 import { PartialType } from '@nestjs/swagger';
 import { type Group } from '@open-data-capture/types';
 
-import { CreateGroupDto, CreateGroupDtoSchema } from './create-group.dto';
+import { CreateGroupDataSchema, CreateGroupDto } from './create-group.dto';
 
-const UpdateGroupDtoSchema = CreateGroupDtoSchema.partial();
-
-@ValidationSchema(UpdateGroupDtoSchema)
+@ValidationSchema(CreateGroupDataSchema.partial())
 export class UpdateGroupDto extends PartialType(CreateGroupDto) implements Partial<Group> {
   name?: string;
 }
