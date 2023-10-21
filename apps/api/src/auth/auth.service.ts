3955a013e5b0bd4c923f1a4a6a185fb55ff3cdb9
joshunrau <contact@joshuaunrau.com>
2023-10-21 15:59:32 -0400

    add id to payload

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -2,8 +2,7 @@ import { CryptoService } from '@douglasneuroinformatics/nestjs/modules';
 import { Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
-import type { AuthPayload, JwtPayload } from '@open-data-capture/types';
-import type { User } from '@open-data-capture/types';
+import type { AuthPayload, JwtPayload, User } from '@open-data-capture/types';
 
 import { AbilityFactory } from '@/ability/ability.factory';
 import { UsersService } from '@/users/users.service';
@@ -46,7 +45,7 @@ export class AuthService {
   private async getUser(username: string): Promise<User> {
     let user: User;
     try {
-      user = await this.usersService.findByUsername(username);
+      user = await this.usersService.findByUsername(username).then((doc) => doc.toObject({ virtuals: true }));
     } catch (error) {
       if (error instanceof NotFoundException) {
         throw new UnauthorizedException('Invalid username');
