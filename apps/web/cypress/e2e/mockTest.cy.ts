28212ba4e86c47f6213e869669e10147387f6d65
david-roper <davidroper321@gmail.com>
2023-10-30 15:50:38 -0400

    added to mock msw cypress test

--- apps/web/cypress/e2e/mockTest.cy.ts
+++ apps/web/cypress/e2e/mockTest.cy.ts
@@ -1,5 +1,18 @@
 describe('login test', () => {
-  it('passes', () => {
+  before(() => {
     cy.visit('http://127.0.0.1:3000/auth/login');
+  });
+  it('passes', () => {
+    // cy.visit('http://127.0.0.1:3000/auth/login');
+    cy.window().then((window) => {
+      // Reference global instances set in "src/mocks.js".
+      const { worker, rest } = window.msw;
+
+      worker.use(
+        rest.post('/overview', (req, res, ctx) => {
+          return res(ctx.json({ success: true }));
+        })
+      );
+    });
   });
 });
--- apps/web/msw/mocks/browser.jsx
+++ apps/web/msw/mocks/browser.jsx
@@ -1,7 +1,7 @@
 import { rest, setupWorker } from 'msw';
 
 const worker = setupWorker(
-  rest.get('auth/login', (res, req, ctx) => {
+  rest.get('/auth/login', (res, req, ctx) => {
     return res();
   })
 );
