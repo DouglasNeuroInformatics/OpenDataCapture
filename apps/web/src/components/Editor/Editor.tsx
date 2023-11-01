9940671145c95b6f9c6e67290c1f6b4e0be990e2
joshunrau <contact@joshuaunrau.com>
2023-11-01 13:49:34 -0400

    add help

--- apps/web/src/components/Editor/Editor.tsx
+++ apps/web/src/components/Editor/Editor.tsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { ArrowToggle, Card } from '@douglasneuroinformatics/ui';
+import { QuestionMarkCircleIcon } from '@heroicons/react/24/outline';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import { twMerge } from 'tailwind-merge';
 
 import { EditorEmptyState } from './EditorEmptyState';
+import { EditorHelpModal } from './EditorHelpModal';
 import { EditorPane } from './EditorPane';
 import { EditorSidebar } from './EditorSidebar';
 import { EditorTab } from './EditorTab';
@@ -21,6 +23,7 @@ export type EditorProps = {
 };
 
 export const Editor = ({ className, files }: EditorProps) => {
+  const [isHelpModalOpen, setIsHelpModalOpen] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [models, setModels] = useState<EditorModel[]>([]);
   const [selectedModel, setSelectedModel] = useState<monaco.editor.IModel | null>(null);
@@ -37,29 +40,43 @@ export const Editor = ({ className, files }: EditorProps) => {
   }, []);
 
   return (
-    <Card className={twMerge('h-full w-full overflow-hidden', className)}>
-      <div className="flex divide-x divide-slate-900/10 border-b border-slate-900/10 text-sm dark:divide-slate-100/25 dark:border-slate-100/25">
-        <ArrowToggle
-          className="p-2"
-          position="right"
-          rotation={180}
-          type="button"
-          onClick={() => {
-            setIsSidebarOpen(!isSidebarOpen);
-          }}
-        />
-        {selectedModel && <EditorTab label={selectedModel?.uri.path.slice(1)} />}
-      </div>
-      <div className="flex min-h-[576px]">
-        <EditorSidebar
-          isOpen={isSidebarOpen}
-          models={models}
-          onSelection={(id) => {
-            setSelectedModel(models.find((model) => model.id === id) ?? null);
-          }}
-        />
-        {selectedModel ? <EditorPane model={selectedModel} /> : <EditorEmptyState />}
-      </div>
-    </Card>
+    <React.Fragment>
+      <Card className={twMerge('h-full w-full overflow-hidden', className)}>
+        <div className="flex justify-between border-b border-slate-900/10 dark:border-slate-100/25">
+          <div className="flex divide-x divide-slate-900/10 text-sm dark:divide-slate-100/25">
+            <ArrowToggle
+              className="p-2"
+              position="right"
+              rotation={180}
+              type="button"
+              onClick={() => {
+                setIsSidebarOpen(!isSidebarOpen);
+              }}
+            />
+            {selectedModel && <EditorTab label={selectedModel?.uri.path.slice(1)} />}
+          </div>
+          <button
+            className="flex items-center justify-center p-2"
+            type="button"
+            onClick={() => {
+              setIsHelpModalOpen(true);
+            }}
+          >
+            <QuestionMarkCircleIcon height={14} width={14} />
+          </button>
+        </div>
+        <div className="flex min-h-[576px]">
+          <EditorSidebar
+            isOpen={isSidebarOpen}
+            models={models}
+            onSelection={(id) => {
+              setSelectedModel(models.find((model) => model.id === id) ?? null);
+            }}
+          />
+          {selectedModel ? <EditorPane model={selectedModel} /> : <EditorEmptyState />}
+        </div>
+      </Card>
+      <EditorHelpModal isOpen={isHelpModalOpen} setIsOpen={setIsHelpModalOpen} />
+    </React.Fragment>
   );
 };
