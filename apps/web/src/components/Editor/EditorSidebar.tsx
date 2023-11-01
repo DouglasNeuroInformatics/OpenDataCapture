93df3043d0b48b4ddb63530953338cba55a7931c
joshunrau <contact@joshuaunrau.com>
2023-11-01 12:39:03 -0400

    add sidebar

--- apps/web/src/components/Editor/EditorSidebar.tsx
+++ apps/web/src/components/Editor/EditorSidebar.tsx
@@ -8,10 +8,12 @@ export const EditorSidebar = ({ isOpen }: EditorSidebarProps) => {
   return (
     <motion.div
       animate={{ width: isOpen ? 320 : 0 }}
-      className="flex-shrink-0 overflow-hidden bg-slate-100"
+      className="flex-shrink-0 overflow-hidden shadow-sm"
       initial={{ width: 0 }}
     >
-      <h1>Sidebar</h1>
+      <div className="h-full w-full border-r border-slate-900/10 dark:border-slate-100/25">
+        <h1>Sidebar</h1>
+      </div>
     </motion.div>
   );
 };
