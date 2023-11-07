ea6b9da1d17701c42eb01f596a78db6da52dacad
david-roper <davidroper321@gmail.com>
2023-11-07 16:13:43 -0500

    msw handler coverts request to json

--- apps/web/msw/mocks/handlers.js
+++ apps/web/msw/mocks/handlers.js
@@ -1,35 +1,25 @@
-import {  http, HttpResponse } from 'msw';
+import { HttpResponse, http } from 'msw';
 
 // src/mocks/handlers.js
 
 export const handlers = [
-  http.post('/auth/login', async ({request}) => {
+  http.post(import.meta.env.VITE_API_BASE_URL + '/v1/auth/login', async ({ request }) => {
     // eslint-disable-next-line perfectionist/sort-objects
-    const data = await request.formData();
-    const username = data.get('username');
-    const password = data.get('password')
+    const data = await request.json();
+    const username = data['username'];
+    const password = data['password'];
 
-    if (username === 'testUsername123' && password === 'testPassword123') {
-      return new HttpResponse.json(
-        {
-          success: true,
-          message: "Login successful"
-        }
-      );
+    if (username === 'david' && password === 'Password123') {
+      return new HttpResponse(null, {
+        message: 'Login successful',
+        status: 200,
+        success: true
+      });
     } else {
-      return  new HttpResponse.json(
-        {
-          success: false,
-          message: "Login failed"
-        }
-      );
+      return new HttpResponse(null, {
+        message: 'Login fail',
+        status: 401
+      });
     }
-  }),
-
-  http.post('/auth/login', () => {
-    // Persist user's authentication in the session
-    sessionStorage.setItem('is-authenticated', 'true');
-
-    return new HttpResponse('Login Authenticated');
   })
 ];
