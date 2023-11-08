5a4d4a9e94da4ba4924f59432f9ba334ae0590c7
joshunrau <contact@joshuaunrau.com>
2023-11-08 11:37:49 -0500

    add title to index

--- apps/gateway/src/pages/index.tsx
+++ apps/gateway/src/pages/index.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
 
-const IndexPage = () => {
+type IndexPageProps = {
+  title?: string;
+};
+
+const IndexPage = ({ title }: IndexPageProps) => {
   const [count, setCount] = useState(0);
   return (
     <div>
+      <h1>{title}</h1>
       <p>Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment Count</button>
     </div>
