8231364a8d8adff59ef69a86a5a364f881d7b3ec
david-roper <davidroper321@gmail.com>
2023-11-08 17:23:19 -0500

    inserting test set Token method and setting token in msw

--- apps/web/cypress/e2e/mockTest.cy.ts
+++ apps/web/cypress/e2e/mockTest.cy.ts
@@ -1,8 +1,9 @@
 describe('login test', () => {
   it('passes', () => {
     cy.visit('http://127.0.0.1:3000/auth/login');
-    cy.get('input[name="username"]').type('testUsername123');
-    cy.get('input[name="password"]').type('testPassword123');
+    cy.wait(1000);
+    cy.get('input[name="username"]').type('david');
+    cy.get('input[name="password"]').type('Password123');
     cy.get('button').contains('Login').click();
   });
 });
