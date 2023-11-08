e5b8367cbdd1e544a0a9e40922ffe5ec0e8451ea
david-roper <davidroper321@gmail.com>
2023-11-08 11:04:06 -0500

    edits to mock cypress test

--- apps/web/cypress/e2e/mockTest.cy.ts
+++ apps/web/cypress/e2e/mockTest.cy.ts
@@ -1,23 +1,8 @@
 describe('login test', () => {
-  before(() => {
-    cy.visit('http://127.0.0.1:3000/auth/login');
-  });
   it('passes', () => {
-    // cy.visit('http://127.0.0.1:3000/auth/login');
-    cy.window().then((window) => {
-      // Reference global instances set in "src/mocks.js".
-      const { worker, rest } = window.msw;
-
-      worker.use(
-        rest.post('/auth/login', async ({request}) => {
-          const info = await request.formData();
-
-          return info.username;
-        })
-      );
-      cy.get('input[name="username"]').type('testUsername123');
-      cy.get('input[name="password"]').type('testPassword123');
-      cy.get('button').contains('Login').click();
-    });
+    cy.visit('http://127.0.0.1:3000/auth/login');
+    cy.get('input[name="username"]').type('testUsername123');
+    cy.get('input[name="password"]').type('testPassword123');
+    cy.get('button').contains('Login').click();
   });
 });
