6ca19fac1452e645d7a266cb51de1a1cf040e681
david-roper <davidroper321@gmail.com>
2023-11-09 16:06:44 -0500

    adding more info to test auth token

--- apps/web/msw/mocks/handlers.js
+++ apps/web/msw/mocks/handlers.js
@@ -6,11 +6,13 @@ import { useAuthStore } from '@/stores/auth-store';
 // src/mocks/handlers.js
 
 const authToken =
-  'ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.ew0KICANCiAgInVzZXJuYW1lIjogImRhdmlkIiwNCiAgInBhc3N3b3JkIjogIlBhc3N3b3JkMTIzIg0KfQ0K';
+  'ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.IHsNCiAgICAgICJncm91cHMiOiBbXSwNCiAgICAgICJwZXJtaXNzaW9ucyI6IFsNCiAgICAgICAgICB7DQogICAgICAgICAgICAgICJhY3Rpb24iOiAibWFuYWdlIiwNCiAgICAgICAgICAgICAgInN1YmplY3QiOiAiYWxsIg0KICAgICAgICAgIH0NCiAgICAgIF0sDQogICAgICAidXNlcm5hbWUiOiAiZGF2aWQiLA0KICAgICAgImlhdCI6IDE2OTk0ODE1ODMsDQogICAgICAiZXhwIjogMTY5OTU2Nzk4Mw0KICB9';
+// 'ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.ew0KICANCiAgInVzZXJuYW1lIjogImRhdmlkIiwNCiAgInBhc3N3b3JkIjogIlBhc3N3b3JkMTIzIg0KfQ0K';
 const authStore = useAuthStore.getState();
+console.log('this is our test token');
 console.log(jwtDecode(authToken));
 authStore.testSetAccessToken(authToken);
-
+console.log('here is our token');
 console.log(authStore.accessToken);
 
 export const handlers = [
@@ -25,7 +27,7 @@ export const handlers = [
         message: 'Login successful',
         status: 200,
         success: true,
-        token: authStore.accessToken
+        token: 'test.token'
       });
     } else {
       return new HttpResponse(null, {
--- apps/web/src/stores/auth-store.ts
+++ apps/web/src/stores/auth-store.ts
@@ -47,8 +47,20 @@ export const useAuthStore = create<AuthStore>((set) => ({
 
   testSetAccessToken: (accessToken) => {
     const { groups, permissions, ...rest } = jwtDecode<JwtPayload>(
-      'ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.ew0KICAgICAgImdyb3VwcyI6IFtdLA0KICAgICAgInBlcm1pc3Npb25zIjogWw0KICAgICAgICAgIHsNCiAgICAgICAgICAgICAgImFjdGlvbiI6ICJtYW5hZ2UiLA0KICAgICAgICAgICAgICAic3ViamVjdCI6ICJhbGwiDQogICAgICAgICAgfQ0KICAgICAgXSwNCiAgICAgICJ1c2VybmFtZSI6ICJkYXZpZCIsDQogICAgICAiaWF0IjogMTY5OTQ4MTU4MywNCiAgICAgICJleHAiOiAxNjk5NTY3OTgzDQogIH0NCg=='
+      'ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.IHsNCiAgICAgICJncm91cHMiOiBbXSwNCiAgICAgICJwZXJtaXNzaW9ucyI6IFsNCiAgICAgICAgICB7DQogICAgICAgICAgICAgICJhY3Rpb24iOiAibWFuYWdlIiwNCiAgICAgICAgICAgICAgInN1YmplY3QiOiAiYWxsIg0KICAgICAgICAgIH0NCiAgICAgIF0sDQogICAgICAidXNlcm5hbWUiOiAiZGF2aWQiLA0KICAgICAgImlhdCI6IDE2OTk0ODE1ODMsDQogICAgICAiZXhwIjogMTY5OTU2Nzk4Mw0KICB9'
     );
+    // const { groups, permissions, ...rest } = {
+    //   groups: [],
+    //   permissions: [
+    //     {
+    //       action: 'manage',
+    //       subject: 'all'
+    //     }
+    //   ],
+    //   username: 'david',
+    //   iat: 1699481583,
+    //   exp: 1699567983
+    // };
     /* convert this from base64
     {
       "groups": [],
@@ -63,8 +75,8 @@ export const useAuthStore = create<AuthStore>((set) => ({
       "exp": 1699567983
   }
     */
-    console.log('successful login');
-    console.log(jwtDecode<JwtPayload>(accessToken));
+
+    console.log('here are the permissions');
     console.log(permissions);
     const ability = createMongoAbility<AppAbility>(permissions);
     set({
@@ -72,5 +84,6 @@ export const useAuthStore = create<AuthStore>((set) => ({
       currentGroup: groups[0],
       currentUser: { ability, groups, ...rest }
     });
+    console.log('successful login');
   }
 }));
