c60ce51a4cdc904f6b51b7de5075dd0776216650
joshunrau <contact@joshuaunrau.com>
2023-11-11 15:35:13 -0500

    add patch and logging

--- apps/gateway/src/app/api/assignments/[id]/route.ts
+++ apps/gateway/src/app/api/assignments/[id]/route.ts
@@ -1,7 +1,24 @@
+import { updateAssignmentDataSchema } from '@open-data-capture/common/assignment';
+
 import prisma from '@/lib/prisma';
 
 export async function GET(_: Request, { params }: { params: { id: string } }) {
   const assignment = await prisma.assignment.findFirst({
+    where: { id: params.id }
+  });
+  return Response.json(assignment);
+}
+
+export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+  console.log('Recieved patch!!');
+  const data: unknown = await request.json();
+  const result = await updateAssignmentDataSchema.safeParseAsync(data);
+  if (!result.success) {
+    return Response.json({ issues: result.error.issues, message: 'Bad Request' }, { status: 400 });
+  }
+
+  const assignment = await prisma.assignment.update({
+    data: result.data,
     where: { id: params.id }
   });
   return Response.json(assignment);
