db582e06ea2578ed5072f29a73b4d0ec7647fd3f
joshunrau <contact@joshuaunrau.com>
2023-11-11 16:55:14 -0500

    add error message component

--- apps/gateway/src/app/not-found.tsx
+++ apps/gateway/src/app/not-found.tsx
@@ -1,13 +1,11 @@
-const NotFound = () => {
-  return (
-    <div className="flex flex-grow flex-col items-center justify-center gap-1 p-3 text-center">
-      <h1 className="text-muted text-sm font-semibold uppercase tracking-wide">404 Error</h1>
-      <h3 className="text-3xl font-extrabold tracking-tight sm:text-4xl md:text-5xl">Page Not Found</h3>
-      <p className="text-muted mt-2 max-w-prose text-sm sm:text-base">
-        {"Sorry, we couldn't find the page you're looking for."}
-      </p>
-    </div>
-  );
-};
+import { ErrorMessage } from '@/components/ErrorMessage';
+
+const NotFound = () => (
+  <ErrorMessage
+    message="Sorry, we couldn't find the page you're looking for"
+    subtitle="Page Not Found"
+    title="404 Error"
+  />
+);
 
 export default NotFound;
