1b7ee49393516e478259a2b5564f8609433c1564
joshunrau <contact@joshuaunrau.com>
2023-11-11 14:36:01 -0500

    add header components

--- apps/gateway/src/components/Header.tsx
+++ apps/gateway/src/components/Header.tsx
@@ -1,10 +1,16 @@
+import { LanguageToggle, ThemeToggle } from '@douglasneuroinformatics/ui';
+
 import { Branding } from './Branding';
 
 export const Header = () => {
   return (
     <header className="bg-white text-slate-700 shadow dark:bg-slate-800 dark:text-slate-300">
-      <div className="container py-2">
+      <div className="container flex items-center justify-between bg-inherit py-2">
         <Branding />
+        <div className="flex gap-3 bg-inherit">
+          <ThemeToggle />
+          <LanguageToggle options={['en', 'fr']} />
+        </div>
       </div>
     </header>
   );
