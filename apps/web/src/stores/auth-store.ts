a6ba3a61626fdca17c33ec6285adfa54c1ace433
Joshua Unrau <joshua@joshuaunrau.com>
2023-11-14 11:42:17 -0500

    Discard changes to apps/web/src/stores/auth-store.ts

--- apps/web/src/stores/auth-store.ts
+++ apps/web/src/stores/auth-store.ts
@@ -18,7 +18,6 @@ export type AuthStore = {
   logout: () => void;
   setAccessToken: (accessToken: string) => void;
   setCurrentGroup: (group: Group) => void;
-  testSetAccessToken: (accessToken: string) => void;
 };
 
 export const useAuthStore = create<AuthStore>((set) => ({
@@ -31,9 +30,6 @@ export const useAuthStore = create<AuthStore>((set) => ({
   },
   setAccessToken: (accessToken) => {
     const { groups, permissions, ...rest } = jwtDecode<JwtPayload>(accessToken);
-    console.log('successful login');
-    console.log(jwtDecode<JwtPayload>(accessToken));
-    console.log(permissions);
     const ability = createMongoAbility<AppAbility>(permissions);
     set({
       accessToken,
@@ -43,47 +39,5 @@ export const useAuthStore = create<AuthStore>((set) => ({
   },
   setCurrentGroup: (group) => {
     set({ currentGroup: group });
-  },
-
-  testSetAccessToken: (accessToken) => {
-    const { groups, permissions, ...rest } = jwtDecode<JwtPayload>(
-      'ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.IHsNCiAgICAgICJncm91cHMiOiBbXSwNCiAgICAgICJwZXJtaXNzaW9ucyI6IFsNCiAgICAgICAgICB7DQogICAgICAgICAgICAgICJhY3Rpb24iOiAibWFuYWdlIiwNCiAgICAgICAgICAgICAgInN1YmplY3QiOiAiYWxsIg0KICAgICAgICAgIH0NCiAgICAgIF0sDQogICAgICAidXNlcm5hbWUiOiAiZGF2aWQiLA0KICAgICAgImlhdCI6IDE2OTk0ODE1ODMsDQogICAgICAiZXhwIjogMTY5OTU2Nzk4Mw0KICB9'
-    );
-    // const { groups, permissions, ...rest } = {
-    //   groups: [],
-    //   permissions: [
-    //     {
-    //       action: 'manage',
-    //       subject: 'all'
-    //     }
-    //   ],
-    //   username: 'david',
-    //   iat: 1699481583,
-    //   exp: 1699567983
-    // };
-    /* convert this from base64
-    {
-      "groups": [],
-      "permissions": [
-          {
-              "action": "manage",
-              "subject": "all"
-          }
-      ],
-      "username": "david",
-      "iat": 1699481583,
-      "exp": 1699567983
-  }
-    */
-
-    console.log('here are the permissions');
-    console.log(permissions);
-    const ability = createMongoAbility<AppAbility>(permissions);
-    set({
-      accessToken,
-      currentGroup: groups[0],
-      currentUser: { ability, groups, ...rest }
-    });
-    console.log('successful login');
   }
 }));
