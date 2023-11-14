c0d62ea637bae364a5770197a8b527f446b2b83c
joshunrau <contact@joshuaunrau.com>
2023-11-14 17:00:15 -0500

    lint marketing

--- apps/marketing/src/i18n/translations.ts
+++ apps/marketing/src/i18n/translations.ts
@@ -1,133 +1,33 @@
 export const translations = {
-  platform: {
-    title: 'Open Data Capture',
-    author: 'Douglas Neuroinformatics Platform',
-    description: {
-      en: 'An open-source, easy-to-use web application for electronic data capture',
-      fr: 'Une application web conviviale à source ouverte pour la saisie électronique des données'
-    },
-    keywords: {
-      en: 'Data Collection, Data Capture, Electronic Data Capture, Clinical, Research',
-      fr: 'Collecte de données, Saisie de données, Saisie électronique de données, Clinique, Recherche'
-    }
-  },
-  landing: {
-    title: {
-      en: 'Overview',
-      fr: 'Accueil'
-    },
-    hero: {
-      heading: {
-        en: 'Modern, Secure, and User-Friendly Electronic Data Capture',
-        fr: 'Une plateforme de saisie électronique des données moderne, sécurisée et conviviale'
-      },
-      subheading: {
-        en: 'Finally, an easy-to-use, reliable, and open-source solution for clinical researchers',
-        fr: 'Enfin une solution facile à utiliser, fiable et open-source pour les chercheurs cliniques'
-      },
-      viewDemo: {
-        en: 'View Demo',
-        fr: 'Voir la démo'
-      },
-      learnMore: {
-        en: 'Learn More',
-        fr: 'En savoir plus'
-      },
-      happyPerson: {
-        en: 'Happy Person',
-        fr: 'Personne heureuse'
-      }
-    },
-    addVisit: {
-      heading: {
-        en: 'Hash-Based Patient Identification',
-        fr: 'Identification des patients par hachage'
-      },
-      subheading: {
-        en: 'Precision in Anonymity',
-        fr: "La précision dans l'anonymat"
-      },
-      description: {
-        en: 'Using state-of-the-art cryptographic algorithms, we compute patient identifiers based on core demographic characteristics. This innovative approach ensures zero personal data retention while enabling consistent cross-visit association.',
-        fr: "Grâce à des algorithmes cryptographiques de pointe, nous calculons les identifiants des patients en fonction de leurs caractéristiques démographiques de base. Cette approche innovante garantit qu'aucune donnée personnelle n'est conservée tout en permettant une association cohérente entre les visites."
-      }
-    },
-    dataManagement: {
-      heading: {
-        en: 'Effortless Data Management',
-        fr: 'Rationalisez la gestion de vos données'
-      },
-      subheading: {
-        en: 'Excel at Data Management, Spreadsheets Not Required',
-        fr: 'Excel pour la gestion des données, feuilles de calcul non requises'
-      },
-      description: {
-        en: "When you're prepared to export, choose between CSV or TSV formats. With Open Data Capture, you're guaranteed access to the most recent version of your data. Move beyond scattered spreadsheets and embrace centralized, secure data storage.",
-        fr: "Lorsque vous êtes prêt à exporter, choisissez entre les formats CSV ou TSV. Avec Open Data Capture, vous avez la garantie d'accéder à la version la plus récente de vos données. Laissez derrière vous les feuilles de calcul éparpillées et adoptez un stockage de données centralisé et sécurisé."
-      },
-      viewSubjectsScreenshot: {
-        en: 'Screenshot of view subjects page',
-        fr: "Capture d'écran de la page de présentation de tous les sujets"
-      }
-    },
-    testimonials: {
-      heading: {
-        en: 'What People Say About Us',
-        fr: 'Ce que les gens disent de nous'
-      }
-    }
-  },
-  team: {
-    title: {
-      en: 'Team',
-      fr: "L'équipe"
-    },
-    heading: {
-      en: 'Meet Our Team',
-      fr: 'Rencontrez notre équipe'
-    }
-  },
   blog: {
-    title: {
-      en: 'Blog',
-      fr: 'Blogue'
-    },
-    heading: {
-      en: 'Our Blog',
-      fr: 'Notre blogue'
-    },
-    subheading: {
-      en: 'All the latest news on Open Data Capture and the open-source electronic data capture community',
-      fr: 'Toutes les dernières nouvelles sur Open Data Capture et la communauté de la saisie électronique de données open-source'
-    },
-    noContent: {
-      en: 'There is no content here at the moment',
-      fr: "Il n'y a pas de contenu ici pour le moment"
-    },
-    viewAltLang: {
-      en: 'You can also view our content in French',
-      fr: 'Vous pouvez également consulter notre contenu en anglais'
-    },
     article: 'Article',
     caseStudy: {
       en: 'Case Study',
       fr: 'Étude de cas'
     },
+    heading: {
+      en: 'Our Blog',
+      fr: 'Notre blogue'
+    },
+    noContent: {
+      en: 'There is no content here at the moment',
+      fr: "Il n'y a pas de contenu ici pour le moment"
+    },
+    subheading: {
+      en: 'All the latest news on Open Data Capture and the open-source electronic data capture community',
+      fr: 'Toutes les dernières nouvelles sur Open Data Capture et la communauté de la saisie électronique de données open-source'
+    },
+    title: {
+      en: 'Blog',
+      fr: 'Blogue'
+    },
     video: {
       en: 'Video',
       fr: 'Vidéo'
-    }
-  },
-  languageToggle: {
-    label: {
-      en: 'Change Language',
-      fr: 'Changer la langue'
-    }
-  },
-  themeToggle: {
-    label: {
-      en: 'Change Theme',
-      fr: 'Changer le thème'
+    },
+    viewAltLang: {
+      en: 'You can also view our content in French',
+      fr: 'Vous pouvez également consulter notre contenu en anglais'
     }
   },
   footer: {
@@ -136,5 +36,105 @@ export const translations = {
       fr: 'À propos de nous'
     },
     gitHub: 'GitHub'
+  },
+  landing: {
+    addVisit: {
+      description: {
+        en: 'Using state-of-the-art cryptographic algorithms, we compute patient identifiers based on core demographic characteristics. This innovative approach ensures zero personal data retention while enabling consistent cross-visit association.',
+        fr: "Grâce à des algorithmes cryptographiques de pointe, nous calculons les identifiants des patients en fonction de leurs caractéristiques démographiques de base. Cette approche innovante garantit qu'aucune donnée personnelle n'est conservée tout en permettant une association cohérente entre les visites."
+      },
+      heading: {
+        en: 'Hash-Based Patient Identification',
+        fr: 'Identification des patients par hachage'
+      },
+      subheading: {
+        en: 'Precision in Anonymity',
+        fr: "La précision dans l'anonymat"
+      }
+    },
+    dataManagement: {
+      description: {
+        en: "When you're prepared to export, choose between CSV or TSV formats. With Open Data Capture, you're guaranteed access to the most recent version of your data. Move beyond scattered spreadsheets and embrace centralized, secure data storage.",
+        fr: "Lorsque vous êtes prêt à exporter, choisissez entre les formats CSV ou TSV. Avec Open Data Capture, vous avez la garantie d'accéder à la version la plus récente de vos données. Laissez derrière vous les feuilles de calcul éparpillées et adoptez un stockage de données centralisé et sécurisé."
+      },
+      heading: {
+        en: 'Effortless Data Management',
+        fr: 'Rationalisez la gestion de vos données'
+      },
+      subheading: {
+        en: 'Excel at Data Management, Spreadsheets Not Required',
+        fr: 'Excel pour la gestion des données, feuilles de calcul non requises'
+      },
+      viewSubjectsScreenshot: {
+        en: 'Screenshot of view subjects page',
+        fr: "Capture d'écran de la page de présentation de tous les sujets"
+      }
+    },
+    hero: {
+      happyPerson: {
+        en: 'Happy Person',
+        fr: 'Personne heureuse'
+      },
+      heading: {
+        en: 'Modern, Secure, and User-Friendly Electronic Data Capture',
+        fr: 'Une plateforme de saisie électronique des données moderne, sécurisée et conviviale'
+      },
+      learnMore: {
+        en: 'Learn More',
+        fr: 'En savoir plus'
+      },
+      subheading: {
+        en: 'Finally, an easy-to-use, reliable, and open-source solution for clinical researchers',
+        fr: 'Enfin une solution facile à utiliser, fiable et open-source pour les chercheurs cliniques'
+      },
+      viewDemo: {
+        en: 'View Demo',
+        fr: 'Voir la démo'
+      }
+    },
+    testimonials: {
+      heading: {
+        en: 'What People Say About Us',
+        fr: 'Ce que les gens disent de nous'
+      }
+    },
+    title: {
+      en: 'Overview',
+      fr: 'Accueil'
+    }
+  },
+  languageToggle: {
+    label: {
+      en: 'Change Language',
+      fr: 'Changer la langue'
+    }
+  },
+  platform: {
+    author: 'Douglas Neuroinformatics Platform',
+    description: {
+      en: 'An open-source, easy-to-use web application for electronic data capture',
+      fr: 'Une application web conviviale à source ouverte pour la saisie électronique des données'
+    },
+    keywords: {
+      en: 'Data Collection, Data Capture, Electronic Data Capture, Clinical, Research',
+      fr: 'Collecte de données, Saisie de données, Saisie électronique de données, Clinique, Recherche'
+    },
+    title: 'Open Data Capture'
+  },
+  team: {
+    heading: {
+      en: 'Meet Our Team',
+      fr: 'Rencontrez notre équipe'
+    },
+    title: {
+      en: 'Team',
+      fr: "L'équipe"
+    }
+  },
+  themeToggle: {
+    label: {
+      en: 'Change Theme',
+      fr: 'Changer le thème'
+    }
   }
 } as const;
