50646db236d21291e09eca4b243b0f4756966307
joshunrau <contact@joshuaunrau.com>
2023-11-14 20:39:52 -0500

    use ui config

--- apps/marketing/tailwind.config.js
+++ apps/marketing/tailwind.config.js
@@ -1,25 +1,9 @@
+import config from '@douglasneuroinformatics/ui/tailwind.config';
 import typographyPlugin from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,ts}'],
-  darkMode: 'class',
   plugins: [typographyPlugin],
-  theme: {
-    extend: {
-      container: {
-        center: true,
-        padding: {
-          DEFAULT: '1rem',
-          lg: '4rem',
-          sm: '2rem',
-          xl: '5rem'
-        }
-      },
-      transitionProperty: {
-        mh: 'max-height',
-        'opacity-transform': 'opacity, transform'
-      }
-    }
-  }
+  presets: [config]
 };
