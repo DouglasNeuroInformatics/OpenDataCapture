318c74d0395e1bd1ed32b2542f4e5d68ec29aca0
joshunrau <contact@joshuaunrau.com>
2023-11-15 13:11:24 -0500

    refactor

--- apps/marketing/tailwind.config.js
+++ apps/marketing/tailwind.config.js
@@ -1,9 +1,9 @@
-import config from '@douglasneuroinformatics/ui/tailwind.config';
+import config from '@open-data-capture/react-core/tailwind.config';
 import typographyPlugin from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,ts}'],
+  content: [...config.content, './src/**/*.{astro,html,js,ts}'],
   plugins: [typographyPlugin],
   presets: [config]
 };
