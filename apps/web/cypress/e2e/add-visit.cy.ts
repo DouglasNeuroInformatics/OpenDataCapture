573bf4b1058d9fe14d233aa7c1209ecfec9e992d
david-roper <davidroper321@gmail.com>
2023-11-15 15:40:29 -0500

    create new add visit test

--- apps/web/cypress/e2e/add-visit.cy.ts
+++ apps/web/cypress/e2e/add-visit.cy.ts
@@ -1,1 +1,41 @@
 import { adminUser } from '@/test/server/stubs';
+
+// //randomizer function to create random test patient name
+function makeid(length: number) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
+const name = makeid(6);
+
+describe('look at questionaire', () => {
+  it('passes', () => {
+    //navigate to add visit page, fill in subject form
+    cy.login(adminUser.username, adminUser.password);
+    cy.get('span[data-cy="add-visit"]').first().click({ force: true });
+    cy.get('input[name=firstName]').type(name);
+    cy.get('input[name=lastName]').type('testPatient');
+    cy.get('input[class=field-input]').first().type('test');
+    cy.get('button').contains('14').click();
+
+    cy.get('nav').click();
+    cy.get('button[class="field-input capitalize"]').click({ force: true });
+    cy.get('li[id*="headlessui-listbox-option-:"]').first().click();
+    cy.get('button[type="submit"]').click({ force: true });
+
+    // cy.wait(1000);
+
+    //navigate to view instrument page, select a form and confirm subject info is autofilled
+    cy.get('span[data-cy="view-instrument"]').first().click({ force: true });
+    cy.url().should('include', 'instruments/available');
+    cy.wait(1000);
+    cy.get('h3[data-cy="instrument-title"]').first().click();
+  });
+});
