0e94e634abcc621e90e3a4edc728ee7120bbc4e3
david-roper <davidroper321@gmail.com>
2023-11-16 15:53:05 -0500

    added msw handler for summary page

--- apps/web/src/test/server/browser.ts
+++ apps/web/src/test/server/browser.ts
@@ -2,5 +2,6 @@ import { setupWorker } from 'msw/browser';
 
 import { authHandlers } from './handlers/auth.handlers';
 import { setupHandlers } from './handlers/setup.handlers';
+import { SummaryHandlers } from './handlers/summary.handlers';
 
-export const worker = setupWorker(...authHandlers, ...setupHandlers);
+export const worker = setupWorker(...authHandlers, ...setupHandlers, ...SummaryHandlers);
