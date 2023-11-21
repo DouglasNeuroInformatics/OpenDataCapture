6c7841f27234a25ae4699e319a8667d226f120dc
david-roper <davidroper321@gmail.com>
2023-11-21 16:20:44 -0500

    edits to handler instrument summary

--- apps/web/src/test/server/handlers/form.handlers.ts
+++ apps/web/src/test/server/handlers/form.handlers.ts
@@ -7,6 +7,8 @@ export const FormHandlers = [
       details: {
         description:
           '\n      The Brief Psychiatric Rating Scale is a rating scale which a clinician or researcher may use to\n      measure psychiatric symptoms such as depression, anxiety, hallucinations and unusual behavior.\n      The scale is one of the oldest, most widely used scales to measure psychotic symptoms and was\n      first published in 1962.',
+        // estimatedDuration: 30,
+        // instructions:  "\n      Please enter the score for the term which best describes the patient's condition. 0 = not assessed, 1 = not\n      present, 2 = very mild, 3 = mild, 4 = moderate, 5 = moderately severe, 6 = severe, 7 = extremely severe.",
         title: 'Brief Psychiatric Rating Scale'
       },
       id: '6557bcdc930d9604d6355516',
