fff574f600807b8d6add4b697196d8a2d6e47ccb
joshunrau <contact@joshuaunrau.com>
2023-11-22 17:40:13 -0500

    add instrument-runtime

---
+++ packages/instrument-runtime/src/index.ts
@@ -0,0 +1,15 @@
+import type { InstrumentLanguage } from '@open-data-capture/common';
+
+/**
+ * This is a variable
+ *
+ * @public
+ */
+export const x = 5;
+
+/**
+ * This is a variable
+ *
+ * @public
+ */
+export type L = InstrumentLanguage;
