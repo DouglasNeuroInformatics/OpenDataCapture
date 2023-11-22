0f4e31cc124b6ec1a3ad564519e35264cf7628f7
david-roper <davidroper321@gmail.com>
2023-11-22 15:26:52 -0500

    testing value type in form handler

--- apps/web/src/test/server/handlers/form.handlers.ts
+++ apps/web/src/test/server/handlers/form.handlers.ts
@@ -1,15 +1,15 @@
-import type { FormInstrumentSummary } from '@open-data-capture/common/instrument';
+import { formDataTypeSchema, type FormInstrumentSummary } from '@open-data-capture/common/instrument';
 import type { FormInstrument } from '@open-data-capture/common/instrument';
 import { HttpResponse, http } from 'msw';
 
 export const FormHandlers = [
   http.get('/v1/instruments/available?kind=form', () => {
-    return HttpResponse.json<FormInstrumentSummary>({
+    return HttpResponse.json<FormInstrument>({
       details: {
         description:
           '\n      The Brief Psychiatric Rating Scale is a rating scale which a clinician or researcher may use to\n      measure psychiatric symptoms such as depression, anxiety, hallucinations and unusual behavior.\n      The scale is one of the oldest, most widely used scales to measure psychotic symptoms and was\n      first published in 1962.',
-        // estimatedDuration: 30,
-        // instructions:  "\n      Please enter the score for the term which best describes the patient's condition. 0 = not assessed, 1 = not\n      present, 2 = very mild, 3 = mild, 4 = moderate, 5 = moderately severe, 6 = severe, 7 = extremely severe.",
+        estimatedDuration: 30,
+        instructions:  "\n      Please enter the score for the term which best describes the patient's condition. 0 = not assessed, 1 = not\n      present, 2 = very mild, 3 = mild, 4 = moderate, 5 = moderately severe, 6 = severe, 7 = extremely severe.",
         title: 'Brief Psychiatric Rating Scale'
       },
       id: '6557bcdc930d9604d6355516',
@@ -17,8 +17,8 @@ export const FormHandlers = [
       language: 'en',
       measures: {
         totalScore: {
-          label: 'Total Score'
-          //   value: 1
+          label: 'Total Score',
+          value: (data: FormDataType) => 1
         }
       },
       name: 'BriefPsychiatricRatingScale',
