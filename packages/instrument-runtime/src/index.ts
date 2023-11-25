72386b2815e4db530e9661a1da134c368a98b8da
joshunrau <contact@joshuaunrau.com>
2023-11-24 21:21:18 -0500

    remove build from common

--- packages/instrument-runtime/src/index.ts
+++ packages/instrument-runtime/src/index.ts
@@ -1,15 +1,6 @@
-import type { InstrumentLanguage } from '@open-data-capture/common';
-
 /**
  * This is a variable
  *
  * @public
  */
 export const x = 5;
-
-/**
- * This is a variable
- *
- * @public
- */
-export type L = InstrumentLanguage;
