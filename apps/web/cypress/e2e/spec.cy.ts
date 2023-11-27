07e99e6aedef1bf04b0117ad1c62126a930e0554
david-roper <davidroper321@gmail.com>
2023-11-27 16:31:41 -0500

    refactor subject view test

--- apps/web/cypress/e2e/spec.cy.ts
+++ apps/web/cypress/e2e/spec.cy.ts
@@ -3,17 +3,19 @@ import { adminUser } from '@/test/server/stubs';
 // const username = 'david';
 // const password = 'Password123';
 
-describe('Graph test', () => {
+describe('Subject view test', () => {
   it('passes', () => {
 
     cy.login(adminUser.username, adminUser.password);
 
     //navigates the view sujects, selects the first subject and views their graph
-    cy.get('button[data-cy="view-subjects"]').first().click({force: true})
+    cy.get('button[data-cy="view-subjects"]').first().click({force: true});
 
     //select first user and their instrument dropdown
-    cy.get('td').first().click()
+    cy.get('td').first().click();
     // cy.get('div[data-cy="instruement-select"]').click();
+    cy.get('a').contains('Table').click();
+    cy.get('button').contains('Instrument').click();
 
 
 
