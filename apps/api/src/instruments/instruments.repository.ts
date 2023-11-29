3a28b9c75abaa3eba38bf15b5dce5325f7182fa9
joshunrau <contact@joshuaunrau.com>
2023-11-29 00:15:35 -0500

    remove custom

--- apps/api/src/instruments/instruments.repository.ts
+++ apps/api/src/instruments/instruments.repository.ts
@@ -1,14 +1,5 @@
 import { EntityRepository } from '@douglasneuroinformatics/nestjs/core';
-import { NotImplementedException } from '@nestjs/common';
 
-import { type InstrumentDocument, InstrumentEntity } from './entities/instrument.entity';
+import { InstrumentEntity } from './entities/instrument.entity';
 
-export class InstrumentsRepository extends EntityRepository(InstrumentEntity) {
-  create(): never {
-    throw new NotImplementedException();
-  }
-
-  createFromSource(source: string) {
-    return this.model.create({ source }) as Promise<InstrumentDocument>;
-  }
-}
+export class InstrumentsRepository extends EntityRepository(InstrumentEntity) {}
