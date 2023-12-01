e3f9db40156a67658ae3b23a35a054040309dc7a
joshunrau <contact@joshuaunrau.com>
2023-12-01 16:19:16 -0500

    add custom model provider

--- apps/api/src/prisma/prisma.module.ts
+++ apps/api/src/prisma/prisma.module.ts
@@ -1,10 +1,26 @@
 import { type DynamicModule, Module } from '@nestjs/common';
-import { PrismaClient } from '@open-data-capture/database';
+import { Prisma, PrismaClient } from '@open-data-capture/database';
 
 import { PRISMA_CLIENT_TOKEN } from './prisma.constants';
+import { getModelReferenceName } from './prisma.utils';
 
 @Module({})
 export class PrismaModule {
+  static forFeature<T extends Prisma.ModelName>(modelName: T): DynamicModule {
+    return {
+      exports: [modelName],
+      module: PrismaModule,
+      providers: [
+        {
+          inject: [PRISMA_CLIENT_TOKEN],
+          provide: modelName,
+          useFactory: (client: PrismaClient) => {
+            return client[getModelReferenceName(modelName)];
+          }
+        }
+      ]
+    };
+  }
   static forRoot(): DynamicModule {
     return {
       exports: [PRISMA_CLIENT_TOKEN],
