7aa4bb319a9823eee21e6d0199c9d50fe7861fb2
joshunrau <contact@joshuaunrau.com>
2023-12-01 13:17:15 -0500

    add type safe ability

--- apps/api/src/ability/ability.types.ts
+++ apps/api/src/ability/ability.types.ts
@@ -1,10 +1,22 @@
 import type { PureAbility } from '@casl/ability';
 import type { PrismaQuery, Subjects } from '@casl/prisma';
 import type { AppAction, AppSubjectName } from '@open-data-capture/common';
-import type { Group } from '@open-data-capture/database';
+import type {
+  GroupModel,
+  InstrumentRecordModel,
+  SubjectModel,
+  UserModel,
+  VisitModel
+} from '@open-data-capture/database';
 
-type AppSubject = AppSubjectName | Subjects<{
-  Group: Group;
-}>
+type AppSubject =
+  | AppSubjectName
+  | Subjects<{
+      Group: GroupModel;
+      InstrumentRecord: InstrumentRecordModel;
+      Subject: SubjectModel;
+      User: UserModel;
+      Visit: VisitModel;
+    }>;
 
 export type AppAbility = PureAbility<[AppAction, AppSubject], PrismaQuery>;
