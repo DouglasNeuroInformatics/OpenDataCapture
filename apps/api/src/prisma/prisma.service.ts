48e42f03aab31fea141bc9e217698cecce782511
joshunrau <contact@joshuaunrau.com>
2023-12-01 09:43:04 -0500

    add db as dep

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, type OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@open-data-capture/database';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
