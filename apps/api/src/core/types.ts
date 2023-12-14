e05dde08417b46f74711239a332f278d426b4f7c
joshunrau <contact@joshuaunrau.com>
2023-12-14 10:24:57 -0500

    cleanup core

--- apps/api/src/core/types.ts
+++ apps/api/src/core/types.ts
@@ -1,5 +1,5 @@
 import type { PureAbility } from '@casl/ability';
-import type { PrismaQuery, Subjects } from '@casl/prisma';
+import type { PrismaQuery } from '@casl/prisma';
 import type { AppAction, AppSubjectName } from '@open-data-capture/common';
 import type {
   GroupModel,
@@ -8,11 +8,6 @@ import type {
   UserModel,
   VisitModel
 } from '@open-data-capture/database';
-import type { Merge } from 'type-fest';
-
-type AppModel<T extends object, U extends string> = T extends any ? Merge<T, { __model__: U }> : never;
-
-type T = AppModel<{ foo: string }, 'Foo'>;
 
 type AppSubject = AppSubjectName | GroupModel | InstrumentRecordModel | SubjectModel | UserModel | VisitModel;
 
