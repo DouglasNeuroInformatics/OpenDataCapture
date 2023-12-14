a0bef0912cf6260e81a8909a84b4246e3dd52648
joshunrau <contact@joshuaunrau.com>
2023-12-14 12:00:44 -0500

    use real app ability

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,10 +1,10 @@
 import { CurrentUser, type EntityController, ParseIdPipe } from '@douglasneuroinformatics/nestjs/core';
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import type { BaseAppAbility } from '@open-data-capture/common/core';
 import type { User } from '@open-data-capture/common/user';
 
 import { RouteAccess } from '@/core/decorators/route-access.decorator';
+import type { AppAbility } from '@/core/types';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -18,28 +18,28 @@ export class UsersController implements EntityController<User> {
   @ApiOperation({ summary: 'Create User' })
   @Post()
   @RouteAccess({ action: 'create', subject: 'User' })
-  create(@Body() user: CreateUserDto, @CurrentUser('ability') ability: BaseAppAbility) {
+  create(@Body() user: CreateUserDto, @CurrentUser('ability') ability: AppAbility) {
     return this.usersService.create(user, { ability });
   }
 
   @ApiOperation({ summary: 'Delete User' })
   @Delete(':id')
   @RouteAccess({ action: 'delete', subject: 'User' })
-  deleteById(@Param('id', ParseIdPipe) id: string, @CurrentUser('ability') ability: BaseAppAbility) {
+  deleteById(@Param('id', ParseIdPipe) id: string, @CurrentUser('ability') ability: AppAbility) {
     return this.usersService.deleteById(id, { ability });
   }
 
   @ApiOperation({ summary: 'Get All Users' })
   @Get()
   @RouteAccess({ action: 'read', subject: 'User' })
-  findAll(@CurrentUser('ability') ability: BaseAppAbility, @Query('group') groupName?: string) {
+  findAll(@CurrentUser('ability') ability: AppAbility, @Query('group') groupName?: string) {
     return groupName ? this.usersService.findByGroup(groupName, { ability }) : this.usersService.findAll({ ability });
   }
 
   @ApiOperation({ summary: 'Get User' })
   @Get(':id')
   @RouteAccess({ action: 'read', subject: 'User' })
-  findById(@Param('id', ParseIdPipe) id: string, @CurrentUser('ability') ability: BaseAppAbility) {
+  findById(@Param('id', ParseIdPipe) id: string, @CurrentUser('ability') ability: AppAbility) {
     return this.usersService.findById(id, { ability });
   }
 
@@ -49,7 +49,7 @@ export class UsersController implements EntityController<User> {
   updateById(
     @Param('id', ParseIdPipe) id: string,
     @Body() update: UpdateUserDto,
-    @CurrentUser('ability') ability: BaseAppAbility
+    @CurrentUser('ability') ability: AppAbility
   ) {
     return this.usersService.updateById(id, update, { ability });
   }
