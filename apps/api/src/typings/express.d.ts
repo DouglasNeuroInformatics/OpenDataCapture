5be783400bc74366b6c02b229761cf924c88d2bc
joshunrau <contact@joshuaunrau.com>
2023-12-14 10:21:52 -0500

    clean

--- apps/api/src/typings/express.d.ts
+++ apps/api/src/typings/express.d.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+
 import type { BaseAppAbility } from '@open-data-capture/common/core';
 import type { User as AppUser } from '@open-data-capture/common/user';
 
 declare global {
   namespace Express {
-    // https://stackoverflow.com/questions/66312048/cant-override-express-request-user-type-but-i-can-add-new-properties-to-reques
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/consistent-type-definitions
+    // https://stackoverflow.com/questions/66312048/cant-override-express-request-user-type-but-i-can-add-new-properties-to-request
     interface User extends AppUser {
       ability: BaseAppAbility;
     }
