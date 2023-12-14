35f46ffb61eabb8670b390e1e7f0378027c3d7c9
joshunrau <contact@joshuaunrau.com>
2023-12-14 15:24:29 -0500

    use json

--- packages/common/src/instrument-records/instrument-records.schemas.ts
+++ packages/common/src/instrument-records/instrument-records.schemas.ts
@@ -1,8 +1,16 @@
+import _ from 'lodash';
+import type { Jsonifiable } from 'type-fest';
 import { z } from 'zod';
 
 export const createInstrumentRecordDataSchema = z.object({
   assignmentId: z.string().optional(),
-  data: z.unknown(),
+  data: z.custom<Jsonifiable>((data) => {
+    try {
+      return _.isEqual(data, JSON.parse(JSON.stringify(data)));
+    } catch {
+      return false;
+    }
+  }),
   date: z.coerce.date(),
   groupId: z.string().optional(),
   instrumentId: z.string(),
--- packages/common/src/instrument/instrument.types.ts
+++ packages/common/src/instrument/instrument.types.ts
@@ -3,6 +3,7 @@ import type { FormDataType } from '@douglasneuroinformatics/form-types';
 import type { Language } from '../core/core.types';
 import type { FormInstrument } from './types/form-instrument.types';
 import type { InteractiveInstrument } from './types/interactive-instrument.types';
+import type { Jsonifiable } from 'type-fest';
 
 export type InstrumentBundle = {
   bundle: string;
@@ -14,7 +15,9 @@ export type InstrumentSource = {
 
 export type Instrument = FormInstrument | InteractiveInstrument;
 
-export type UnilingualInstrument = FormInstrument<FormDataType, Language> | InteractiveInstrument<unknown, Language>;
+export type UnilingualInstrument =
+  | FormInstrument<FormDataType, Language>
+  | InteractiveInstrument<Jsonifiable, Language>;
 
 export * from './types/base-instrument.types';
 export * from './types/form-instrument.types';
