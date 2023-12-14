51dd2b45ed21c58c587e998b23d787488bb2b962
joshunrau <contact@joshuaunrau.com>
2023-12-14 10:26:17 -0500

    rename file to follow convention

--- apps/api/src/gateway/gateway.module.ts
+++ apps/api/src/gateway/gateway.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 
 import { InstrumentRecordsModule } from '@/instrument-records/instrument-records.module';
 
-import { GatewaySynchronizer } from './gateway-synchronizer';
+import { GatewaySynchronizer } from './gateway.synchronizer';
 
 @Module({
   imports: [HttpModule, InstrumentRecordsModule],
