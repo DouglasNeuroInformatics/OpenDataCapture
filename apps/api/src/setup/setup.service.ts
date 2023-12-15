e3f09b83a3e42e497ca7cd3dc01805f7794f8d04
joshunrau <contact@joshuaunrau.com>
2023-12-15 13:47:43 -0500

    remove mongoose from setup

--- apps/api/src/setup/setup.service.ts
+++ apps/api/src/setup/setup.service.ts
@@ -1,9 +1,8 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
-import { InjectConnection } from '@nestjs/mongoose';
 import type { CreateAdminData, SetupState } from '@open-data-capture/common/setup';
-import mongoose from 'mongoose';
+import { PrismaClient } from '@open-data-capture/database';
 
-import { UserEntity } from '@/users/entities/user.entity';
+import { InjectPrismaClient } from '@/prisma/prisma.decorators';
 import { UsersService } from '@/users/users.service';
 
 import { DemoService } from './demo.service';
@@ -12,24 +11,26 @@ import { SetupDto } from './dto/setup.dto';
 @Injectable()
 export class SetupService {
   constructor(
-    @InjectConnection() private readonly connection: mongoose.Connection,
+    @InjectPrismaClient() private readonly prismaClient: PrismaClient,
     private readonly demoService: DemoService,
     private readonly usersService: UsersService
   ) {}
 
-  async createAdmin(admin: CreateAdminData): Promise<UserEntity> {
+  async createAdmin(admin: CreateAdminData) {
     return this.usersService.create({ ...admin, basePermissionLevel: 'ADMIN' });
   }
 
-  async dropDatabase(): Promise<void> {
-    return this.connection.dropDatabase();
+  async dropDatabase() {
+    return this.prismaClient.$runCommandRaw({
+      dropDatabase: 1
+    });
   }
 
-  async getState(): Promise<SetupState> {
-    return { isSetup: await this.isSetup() };
+  async getState() {
+    return { isSetup: await this.isSetup() } satisfies SetupState;
   }
 
-  async initApp({ admin, initDemo }: SetupDto): Promise<void> {
+  async initApp({ admin, initDemo }: SetupDto) {
     if (await this.isSetup()) {
       throw new ForbiddenException();
     }
@@ -41,13 +42,9 @@ export class SetupService {
   }
 
   private async isSetup(): Promise<boolean> {
-    const collections = await this.connection.db.listCollections().toArray();
-    for (const collection of collections) {
-      const count = await this.connection.collection(collection.name).countDocuments();
-      if (count > 0) {
-        return true;
-      }
-    }
-    return false;
+    const collections: { cursor?: { firstBatch?: unknown[] } } = await this.prismaClient.$runCommandRaw({
+      listCollections: 1
+    });
+    return collections.cursor?.firstBatch?.length !== 0;
   }
 }
