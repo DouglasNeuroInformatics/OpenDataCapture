d7f16dcb7ed3dd3c8b31955cbaade1a389637c9a
joshunrau <contact@joshuaunrau.com>
2024-01-08 18:24:41 -0500

    fix visit dto

--- apps/api/src/visits/dto/create-visit.dto.ts
+++ apps/api/src/visits/dto/create-visit.dto.ts
@@ -1,10 +1,10 @@
 import { ValidationSchema } from '@douglasneuroinformatics/nestjs/core';
 import type { SubjectIdentificationData } from '@open-data-capture/common/subject';
-import { type CreateVisitData, createVisitDataSchema } from '@open-data-capture/common/visit';
+import { $CreateVisitData } from '@open-data-capture/common/visit';
 
-@ValidationSchema<CreateVisitData>(createVisitDataSchema)
+@ValidationSchema($CreateVisitData)
 export class CreateVisitDto {
   date: Date;
-  groupId?: string;
+  groupId: null | string;
   subjectIdData: SubjectIdentificationData;
 }
