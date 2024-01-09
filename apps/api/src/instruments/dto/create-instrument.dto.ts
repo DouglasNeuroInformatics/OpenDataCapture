20c39ec8d8f45bba4b00c7934de607fa3c14bfd0
joshunrau <contact@joshuaunrau.com>
2024-01-09 17:57:47 -0500

    update dto

--- apps/api/src/instruments/dto/create-instrument.dto.ts
+++ apps/api/src/instruments/dto/create-instrument.dto.ts
@@ -1,7 +1,5 @@
 import { ValidationSchema } from '@douglasneuroinformatics/nestjs/core';
-import { $InstrumentSourceContainer } from '@open-data-capture/common/instrument';
-
-const $CreateInstrumentData = $InstrumentSourceContainer.pick({ source: true });
+import { $CreateInstrumentData } from '@open-data-capture/common/instrument';
 
 @ValidationSchema($CreateInstrumentData)
 export class CreateInstrumentDto {
