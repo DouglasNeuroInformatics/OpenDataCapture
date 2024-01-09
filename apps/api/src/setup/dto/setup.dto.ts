37204d5ddb8a50218f11c128ee22a9b3373fac95
joshunrau <contact@joshuaunrau.com>
2024-01-09 14:37:19 -0500

    update setup types

--- apps/api/src/setup/dto/setup.dto.ts
+++ apps/api/src/setup/dto/setup.dto.ts
@@ -1,9 +1,9 @@
 import { ValidationSchema } from '@douglasneuroinformatics/nestjs/core';
 import { ApiProperty } from '@nestjs/swagger';
-import { setupOptionsSchema } from '@open-data-capture/common/setup';
+import { $SetupOptions } from '@open-data-capture/common/setup';
 import type { CreateAdminData, SetupOptions } from '@open-data-capture/common/setup';
 
-@ValidationSchema(setupOptionsSchema)
+@ValidationSchema($SetupOptions)
 export class SetupDto implements SetupOptions {
   @ApiProperty()
   admin: CreateAdminData;
--- packages/common/src/setup.ts
+++ packages/common/src/setup.ts
@@ -2,20 +2,19 @@ import { z } from 'zod';
 
 import { $CreateUserData } from './user';
 
-export type SetupState = {
-  isSetup: boolean | null;
-};
-
-export const $CreateAdminData = $CreateUserData.omit({
-  basePermissionLevel: true,
-  groupNames: true
+export type SetupState = z.infer<typeof $SetupState>;
+export const $SetupState = z.object({
+  isSetup: z.boolean().nullable()
 });
 
 export type CreateAdminData = z.infer<typeof $CreateAdminData>;
+export const $CreateAdminData = $CreateUserData.omit({
+  basePermissionLevel: true,
+  groupIds: true
+});
 
+export type SetupOptions = z.infer<typeof $SetupOptions>;
 export const $SetupOptions = z.object({
   admin: $CreateAdminData,
   initDemo: z.boolean()
 });
-
-export type SetupOptions = z.infer<typeof $SetupOptions>;
