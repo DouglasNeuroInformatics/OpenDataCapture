8ab1f3cb469f49d1d7aa34af75b4aa50e26fc953
joshunrau <contact@joshuaunrau.com>
2024-01-12 17:53:35 -0500

    add id opt to bundle

--- packages/common/src/instrument.utils.ts
+++ packages/common/src/instrument.utils.ts
@@ -5,6 +5,8 @@ import { $FormInstrument, $Instrument, $InteractiveInstrument, type Instrument }
 import type { InstrumentKind } from './instrument.base';
 
 export type EvaluateInstrumentOptions<TKind extends InstrumentKind> = {
+  /** The value to assign to the id property of the instrument */
+  id?: string;
   /** The kind of instrument being evaluated. If validate is set to true, this will be enforced at runtime. Otherwise, it will just be asserted */
   kind?: TKind;
   /** Whether to validate the structure of the instrument at runtime (expensive) */
@@ -13,25 +15,26 @@ export type EvaluateInstrumentOptions<TKind extends InstrumentKind> = {
 
 export async function evaluateInstrument<TKind extends InstrumentKind>(
   bundle: string,
-  { kind, validate }: EvaluateInstrumentOptions<TKind> = { validate: false }
+  { id, kind, validate }: EvaluateInstrumentOptions<TKind> = { validate: false }
 ) {
-  let instrument: Extract<Instrument, { kind: TKind }>;
+  let instrument: Instrument;
   try {
     const factory = new Function(`return ${bundle}`);
     const value = (await factory()) as unknown;
     if (!validate) {
       instrument = value as Extract<Instrument, { kind: TKind }>;
     } else if (kind === 'FORM') {
-      instrument = (await $FormInstrument.parseAsync(value)) as Extract<Instrument, { kind: TKind }>;
+      instrument = await $FormInstrument.parseAsync(value);
     } else if (kind === 'INTERACTIVE') {
-      instrument = (await $InteractiveInstrument.parseAsync(value)) as Extract<Instrument, { kind: TKind }>;
+      instrument = await $InteractiveInstrument.parseAsync(value);
     } else if (kind === undefined) {
-      instrument = (await $Instrument.parseAsync(value)) as Extract<Instrument, { kind: TKind }>;
+      instrument = await $Instrument.parseAsync(value);
     } else {
       throw new Error(`Unexpected kind: ${kind}`);
     }
   } catch (error) {
     throw new Error(`Failed to evaluate instrument bundle`, { cause: { bundle, error } });
   }
-  return instrument;
+  instrument.id = id;
+  return instrument as Extract<Instrument, { kind: TKind }>;
 }
