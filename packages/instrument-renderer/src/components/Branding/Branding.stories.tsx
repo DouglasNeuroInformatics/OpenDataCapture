8442d6a049b9ea29bda833862e60b0b67e097404
joshunrau <contact@joshuaunrau.com>
2024-01-15 16:37:27 -0500

    remove irrelevent things

--- packages/instrument-renderer/src/components/Branding/Branding.stories.tsx
+++
@@ -1,9 +0,0 @@
-import type { Meta, StoryObj } from '@storybook/react';
-
-import { Branding } from './Branding';
-
-type Story = StoryObj<typeof Branding>;
-
-export default { component: Branding } satisfies Meta<typeof Branding>;
-
-export const Default: Story = {};
--- packages/instrument-renderer/src/components/Logo/Logo.stories.tsx
+++
@@ -1,9 +0,0 @@
-import type { Meta, StoryObj } from '@storybook/react';
-
-import { Logo } from './Logo';
-
-type Story = StoryObj<typeof Logo>;
-
-export default { component: Logo } satisfies Meta<typeof Logo>;
-
-export const Default: Story = {};
