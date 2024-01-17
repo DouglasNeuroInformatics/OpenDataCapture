52511877821c9f66a16a31db772a9db0ce6bfb95
joshunrau <contact@joshuaunrau.com>
2024-01-17 15:08:02 -0500

    update setup for i18next

--- packages/i18next/src/index.ts
+++ packages/i18next/src/index.ts
@@ -1,32 +1,32 @@
 import { i18n as i18nUi } from '@douglasneuroinformatics/ui';
-import type { i18n } from 'i18next';
+import { createInstance } from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
-import { i18n as i18nCore } from './core';
+import core from './translations/core.json';
+import { createResources } from './utils';
 
-export async function initI18next(i18n: i18n) {
-  await i18n
-    .use(Backend)
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-      debug: false,
-      defaultNS: 'common',
-      fallbackLng: 'en',
-      interpolation: {
-        escapeValue: false
-      },
-      ns: ['auth', 'common', 'contact', 'instruments', 'layout', 'overview', 'setup', 'subjects', 'user', 'visits'],
-      returnObjects: true,
-      supportedLngs: ['en', 'fr']
+export const defaultNS = 'core';
+
+const resources = createResources({ core });
+
+export const i18next = {
+  i18n: createInstance({
+    defaultNS,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    },
+    resources,
+    returnObjects: true,
+    supportedLngs: ['en', 'fr']
+  }),
+  async init() {
+    await this.i18n.use(LanguageDetector).use(initReactI18next).init();
+    this.i18n.on('languageChanged', (lang) => {
+      i18nUi.changeLanguage(lang).catch(console.error);
     });
-
-  i18n.on('languageChanged', (lang) => {
-    i18nCore.changeLanguage(lang).catch(console.error);
-    i18nUi.changeLanguage(lang).catch(console.error);
-  });
-}
+  }
+};
 
 export type { TranslatedResource } from './types';
--- packages/i18next/src/types.ts
+++ packages/i18next/src/types.ts
@@ -1,6 +1,8 @@
 import type { Language } from '@open-data-capture/common/core';
 import type { EmptyObject, ValueOf } from 'type-fest';
 
+export type TranslationsDef = Record<string, Record<string, unknown>>;
+
 export type TranslatedResource<T = EmptyObject> = {
   [K in keyof T]: T[K] extends Record<string, unknown>
     ? T[K] extends Record<Language, unknown>
