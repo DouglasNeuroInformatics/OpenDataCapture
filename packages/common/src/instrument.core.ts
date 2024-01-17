8bb09bc66edccc03540892851f82dcca2d64e9c9
joshunrau <contact@joshuaunrau.com>
2024-01-17 11:21:03 -0500

    refactor

---
+++ packages/common/src/instrument.core.ts
@@ -0,0 +1,59 @@
+import type { FormDataType } from '@douglasneuroinformatics/form-types';
+import { z } from 'zod';
+
+import {
+  $FormInstrument,
+  type FormInstrument,
+  type FormInstrumentSummary,
+  type StrictFormInstrument,
+  type UnilingualFormInstrumentSummary
+} from './instrument.form';
+import {
+  $InteractiveInstrument,
+  type InteractiveInstrument,
+  type InteractiveInstrumentSummary
+} from './instrument.interactive';
+
+import type { Json, Language } from './core';
+import type { InstrumentKind, InstrumentLanguage } from './instrument.base';
+
+export type Instrument = FormInstrument | InteractiveInstrument;
+
+export const $Instrument = z.union([$FormInstrument, $InteractiveInstrument]) satisfies z.ZodType<Instrument>;
+
+export type InstrumentSummary = FormInstrumentSummary | InteractiveInstrumentSummary;
+
+export type UnilingualInstrument = FormInstrument<FormDataType, Language> | InteractiveInstrument<Json, Language>;
+
+export type UnilingualInstrumentSummary = InteractiveInstrumentSummary | UnilingualFormInstrumentSummary;
+
+export type DiscriminatedInstrumentData<TKind extends InstrumentKind> = [TKind] extends ['FORM']
+  ? FormDataType
+  : [TKind] extends ['INTERACTIVE']
+    ? Json
+    : never;
+
+export type DiscriminatedInstrument<
+  TKind extends InstrumentKind,
+  TData extends DiscriminatedInstrumentData<TKind>,
+  TLanguage extends InstrumentLanguage,
+  TOptions extends { strict: boolean } = { strict: false }
+> = [TKind] extends ['FORM']
+  ? TData extends FormDataType
+    ? TOptions['strict'] extends true
+      ? StrictFormInstrument<TData, TLanguage>
+      : FormInstrument<TData, TLanguage>
+    : never
+  : [TKind] extends ['INTERACTIVE']
+    ? TData extends Json
+      ? TLanguage extends Language
+        ? InteractiveInstrument<TData, TLanguage>
+        : never
+      : never
+    : never;
+
+export type InstrumentDef<
+  TKind extends InstrumentKind,
+  TData extends DiscriminatedInstrumentData<TKind>,
+  TLanguage extends InstrumentLanguage
+> = Omit<DiscriminatedInstrument<TKind, TData, TLanguage, { strict: true }>, 'kind' | 'language' | 'validationSchema'>;
