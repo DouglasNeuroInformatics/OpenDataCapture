388f39aa564b96dc451f47ee9320049f340640be
joshunrau <contact@joshuaunrau.com>
2024-01-18 12:37:17 -0500

    update typings

---
+++ packages/instrument-library/src/typings/forms.d.ts
@@ -0,0 +1,2 @@
+const content: import('@open-data-capture/common/instrument').FormInstrument;
+export default content;
--- packages/instrument-library/src/typings/index.d.ts
+++ packages/instrument-library/src/typings/index.d.ts
@@ -1,15 +1,5 @@
 /// <reference types="@open-data-capture/rollup-plugin-instrument/client" />
 
-declare module '@open-data-capture/instrument-library/forms/*.js' {
-  const content: any;
-  export default content;
-}
-
-declare module '@open-data-capture/instrument-library/interactive/*.jsx' {
-  const content: any;
-  export default content;
-}
-
 export const briefPsychiatricRatingScale: InstrumentLibraryItem;
 export const enhancedDemographicsQuestionnaire: InstrumentLibraryItem;
 export const happinessQuestionnaire: InstrumentLibraryItem;
