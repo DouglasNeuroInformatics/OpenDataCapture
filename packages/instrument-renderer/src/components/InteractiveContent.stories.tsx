85fff8faadeccbe2fe467783eb02fab5b836c0c0
joshunrau <contact@joshuaunrau.com>
2024-01-19 14:36:06 -0500

    add encodeAssets

--- packages/instrument-renderer/src/components/InteractiveContent.stories.tsx
+++ packages/instrument-renderer/src/components/InteractiveContent.stories.tsx
@@ -1,5 +1,5 @@
 import { InstrumentInterpreter } from '@open-data-capture/instrument-interpreter';
-import { clickTask } from '@open-data-capture/instrument-library';
+import { clickTask, reactionTimeTask } from '@open-data-capture/instrument-library';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { InteractiveContent } from './InteractiveContent';
@@ -8,12 +8,16 @@ type Story = StoryObj<typeof InteractiveContent>;
 
 const interpreter = new InstrumentInterpreter();
 
-const instrument = await interpreter.interpret(clickTask.bundle, { kind: 'INTERACTIVE' });
-
 export default { component: InteractiveContent } as Meta<typeof InteractiveContent>;
 
 export const ClickTask: Story = {
   args: {
-    instrument
+    instrument: await interpreter.interpret(clickTask.bundle, { kind: 'INTERACTIVE' })
+  }
+};
+
+export const ReactionTime: Story = {
+  args: {
+    instrument: await interpreter.interpret(reactionTimeTask.bundle, { kind: 'INTERACTIVE' })
   }
 };
--- packages/rollup-plugin-instrument/src/index.js
+++ packages/rollup-plugin-instrument/src/index.js
@@ -1,6 +1,49 @@
+import { lstatSync, readFileSync } from 'fs';
 import fs from 'fs/promises';
+import path from 'path';
 
 import { InstrumentTransformer } from '@open-data-capture/instrument-transformer';
+import mime from 'mime/lite';
+
+/**
+ * Replace all import.meta.encode(filepath) calls with the base64-encoded string
+ * @param {string} source - the source code
+ * @param {string} parent - the directory from which all relative paths should be resolved (or a file, in which case the directory name where the file is located will be used)
+ * @returns {Promise<string>} the file contents encoded according as base64 in the following format: data:[<mediatype>][;base64],<data>
+ */
+async function encodeAssets(source, parent) {
+  const parentStats = await fs.lstat(parent);
+
+  /** @type {string} */
+  let baseDir;
+  if (parentStats.isDirectory()) {
+    baseDir = parent;
+  } else if (parentStats.isFile()) {
+    baseDir = path.dirname(parent);
+  }
+
+  const regex = /import\.meta\.encode\((.*?)\)/g;
+  return source.replaceAll(regex, (substring) => {
+    const relPath = substring.match(/import\.meta\.encode\((['"`])(.*?)\1\)/)?.at(2);
+    if (!relPath) {
+      throw new TypeError(`Illegal function call \`${substring}\`: expected one argument (a non-empty string)`);
+    }
+    const filepath = path.resolve(baseDir, relPath);
+    const stats = lstatSync(filepath);
+    if (stats.isDirectory()) {
+      throw new Error(
+        `Invalid argument "${relPath}" for import.meta.encode: resolved filepath "${filepath}" is a directory`
+      );
+    } else if (!stats.isFile()) {
+      throw new Error(
+        `Invalid argument "${relPath}" for import.meta.encode: resolved filepath "${filepath}" is not a file`
+      );
+    }
+    const data = readFileSync(filepath, 'base64');
+    const mimeType = mime.getType(filepath);
+    return `'data:${mimeType};base64,${data}'`;
+  });
+}
 
 /** @returns {import('rollup').Plugin} */
 const instrument = () => {
@@ -13,7 +56,8 @@ const instrument = () => {
       if (id.endsWith(suffix)) {
         const filepath = removeSuffix(id);
         const source = await fs.readFile(filepath, 'utf-8');
-        const bundle = await transformer.generateBundle(source);
+        const encoded = await encodeAssets(source, filepath);
+        const bundle = await transformer.generateBundle(encoded);
         this.addWatchFile(filepath);
         return { code: `export default { bundle: ${JSON.stringify(bundle)}, source: ${JSON.stringify(source)} }` };
       }
