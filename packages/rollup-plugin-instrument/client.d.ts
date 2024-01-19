f3aefe364a6b07cbb41ea7d691470d3c109e9d17
joshunrau <contact@joshuaunrau.com>
2024-01-19 13:02:36 -0500

    add encode

--- packages/rollup-plugin-instrument/client.d.ts
+++ packages/rollup-plugin-instrument/client.d.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+
 declare type InstrumentLibraryItem = {
   bundle: string;
   source: string;
@@ -6,4 +8,8 @@ declare type InstrumentLibraryItem = {
 declare module '*?instrument' {
   const content: InstrumentLibraryItem;
   export default content;
+}
+
+interface ImportMeta {
+  encode: (filepath: string) => string;
 }
