e266a6a8f533c3651aadec34a87ff3535dfbd734
joshunrau <contact@joshuaunrau.com>
2024-01-21 17:23:20 -0500

    pg event

--- apps/playground/src/vite-env.d.ts
+++ apps/playground/src/vite-env.d.ts
@@ -1,3 +1,4 @@
 /// <reference lib="dom" />
 /// <reference lib="dom.iterable" />
 /// <reference types="vite/client" />
+/// <reference types="@open-data-capture/instrument-renderer/client" />
--- packages/instrument-renderer/src/env.d.ts
+++ packages/instrument-renderer/src/env.d.ts
@@ -3,7 +3,3 @@
 /// <reference types="vite/client" />
 /// <reference types="@open-data-capture/i18next/client" />
 
-// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-interface GlobalEventHandlersEventMap {
-  done: CustomEvent;
-}
