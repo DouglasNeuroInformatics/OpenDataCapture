8b3f39b33124af7f28889c4a6201041add472273
joshunrau <contact@joshuaunrau.com>
2024-01-22 13:24:24 -0500

    add prisma debug

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,29 +1,36 @@
-import { Inject, Injectable, type OnModuleInit } from '@nestjs/common';
+import { Inject, Injectable, InternalServerErrorException, Logger, type OnModuleInit } from '@nestjs/common';
 
 import { type ExtendedPrismaClient, PRISMA_CLIENT_TOKEN } from './prisma.factory';
 
 @Injectable()
 export class PrismaService implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
 
   constructor(@Inject(PRISMA_CLIENT_TOKEN) public readonly client: ExtendedPrismaClient) {}
 
   async dropDatabase() {
-    return this.client.$runCommandRaw({
+    this.logger.debug('Attempting to drop database...');
+    const result = await this.client.$runCommandRaw<{ ok: number }>({
       dropDatabase: 1
     });
+    if (result.ok !== 1) {
+      throw new InternalServerErrorException('Failed to drop database: raw mongodb command returned unexpected value', {
+        cause: result
+      })
+    }
+    this.logger.debug('Successfully dropped database');
   }
 
   async getDbName() {
-    return await this.client.$runCommandRaw<{ db: string }>({ dbStats: 1 }).then((stats) => stats.db);
-  }
-
-  async listCollections() {
-    return this.client.$runCommandRaw({
-      listCollections: 1
-    });
+    this.logger.debug('Attempting to get database name...');
+    const dbName = await this.client.$runCommandRaw<{ db: string }>({ dbStats: 1 }).then((stats) => stats.db);
+    this.logger.debug(`Resolved database name: ${dbName}`);
+    return dbName;
   }
 
   async onModuleInit() {
+    this.logger.debug('Attempting to connect to database...');
     await this.client.$connect();
+    this.logger.debug('Successfully connected to database');
   }
 }
