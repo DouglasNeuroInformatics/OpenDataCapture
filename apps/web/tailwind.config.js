ced8d79c1b6846e4c22da3eaeb50e3639ca980b1
joshunrau <contact@joshuaunrau.com>
2024-01-22 11:59:19 -0500

    add tailwind plugin

--- apps/web/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-import config from '@open-data-capture/react-core/tailwind.config';
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [...config.content, 'index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  presets: [config]
-};
--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -4,9 +4,8 @@ import url from 'url';
 import importMetaEnv from '@import-meta-env/unplugin';
 import plausible from '@open-data-capture/vite-plugin-plausible';
 import runtime from '@open-data-capture/vite-plugin-runtime';
+import tailwind from '@open-data-capture/vite-plugin-tailwind'
 import react from '@vitejs/plugin-react-swc';
-import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 import viteCompression from 'vite-plugin-compression';
 
@@ -19,11 +18,6 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000,
     emptyOutDir: false
   },
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer]
-    }
-  },
   plugins: [
     plausible({
       baseUrl: process.env.PLAUSIBLE_BASE_URL,
@@ -35,7 +29,12 @@ export default defineConfig({
       example: path.resolve(projectDir, '.env.public')
     }),
     translations(),
-    runtime()
+    runtime(),
+    tailwind({
+      content: ['index.html', './src/**/*.{js,ts,jsx,tsx}'],
+      include: ['@open-data-capture/editor', '@open-data-capture/instrument-renderer', '@open-data-capture/react-core'],
+      root: import.meta.file
+    })
   ],
   resolve: {
     alias: {
