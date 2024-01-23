d2c88e1bf2a473f2ee3ea8078069d011cef594cc
joshunrau <contact@joshuaunrau.com>
2024-01-23 08:28:02 -0500

    add useInstrumentSummaries

--- apps/web/src/hooks/useInstrumentSummaries.ts
+++ apps/web/src/hooks/useInstrumentSummaries.ts
@@ -1,33 +1,21 @@
-import { $InstrumentBundleContainer } from '@open-data-capture/common/instrument';
-import type { AnyInstrument, AnyUnilingualInstrument, InstrumentKind } from '@open-data-capture/common/instrument';
-import { InstrumentInterpreter } from '@open-data-capture/instrument-interpreter';
-import { translateInstrument } from '@open-data-capture/instrument-utils';
+import { $InstrumentSummary } from '@open-data-capture/common/instrument';
+import type { InstrumentKind } from '@open-data-capture/common/instrument';
+import { translateInstrumentSummary } from '@open-data-capture/instrument-utils';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { useTranslation } from 'react-i18next';
 
-const interpreter = new InstrumentInterpreter();
-
-export const useInstrumentSummaries = <TKind extends InstrumentKind>({ params }: { params?: { kind?: TKind } } = {}) => {
+export const useInstrumentSummaries = <TKind extends InstrumentKind>({
+  params
+}: { params?: { kind?: TKind } } = {}) => {
   const { i18n } = useTranslation();
   return useQuery({
     queryFn: async () => {
       const response = await axios.get('/v1/instruments/summaries', {
         params
       });
-
-      const bundles = await $InstrumentSu.array().parseAsync(response.data);
-      const instruments = await Promise.all(
-        bundles.map(async ({ bundle, id }) => {
-          const instrument: Extract<AnyInstrument, { kind: TKind }> = await interpreter.interpret(bundle, {
-            id,
-            kind: params?.kind,
-            validate: import.meta.env.DEV
-          });
-          return translateInstrument(instrument, i18n.resolvedLanguage ?? 'en');
-        })
-      );
-      return instruments as (Extract<AnyUnilingualInstrument, { kind: TKind }> & { id: string })[];
+      const summaries = await $InstrumentSummary.array().parseAsync(response.data);
+      return summaries.map((summary) => translateInstrumentSummary(summary, i18n.resolvedLanguage ?? 'en'));
     },
     queryKey: ['instruments', params]
   });
