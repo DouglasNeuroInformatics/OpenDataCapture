4a5ab0f2a1638a6cf6ab9a23f8e22a69af62690e
joshunrau <contact@joshuaunrau.com>
2024-01-24 17:54:07 -0500

    fix ff bug

--- apps/gateway/src/entry-client.tsx
+++ apps/gateway/src/entry-client.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
 
+import '@open-data-capture/react-core/styles.css';
+
 ReactDOM.hydrateRoot(
   document.getElementById('root')!,
   <React.StrictMode>
