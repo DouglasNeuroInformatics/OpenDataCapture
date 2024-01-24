97dfc2a963a27625277b333fa7e373f209861969
joshunrau <contact@joshuaunrau.com>
2024-01-24 10:25:36 -0500

    add translate to useInstrument

--- apps/web/src/hooks/useInstrument.ts
+++ apps/web/src/hooks/useInstrument.ts
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 
-import type { InstrumentKind, SomeInstrument } from '@open-data-capture/common/instrument';
+import type { InstrumentKind, SomeUnilingualInstrument } from '@open-data-capture/common/instrument';
+import { translateInstrument } from '@open-data-capture/instrument-utils';
+import { useTranslation } from 'react-i18next';
 
 import { useInstrumentBundle } from './useInstrumentBundle';
 import { useInstrumentInterpreter } from './useInstrumentInterpreter';
@@ -8,14 +10,15 @@ import { useInstrumentInterpreter } from './useInstrumentInterpreter';
 export function useInstrument<TKind extends InstrumentKind>(id: string, options?: { kind?: TKind }) {
   const instrumentBundleQuery = useInstrumentBundle(id);
   const interpreter = useInstrumentInterpreter();
-  const [instrument, setInstrument] = useState<SomeInstrument<TKind> | null>(null);
+  const [instrument, setInstrument] = useState<SomeUnilingualInstrument<TKind> | null>(null);
+  const { i18n } = useTranslation();
 
   useEffect(() => {
     if (instrumentBundleQuery.data) {
       const { bundle, id } = instrumentBundleQuery.data;
       interpreter
         .interpret(bundle, { id, validate: import.meta.env.DEV, ...options })
-        .then(setInstrument)
+        .then((instrument) => setInstrument(translateInstrument(instrument, i18n.resolvedLanguage!)))
         .catch((err) => {
           console.error(err);
           setInstrument(null);
