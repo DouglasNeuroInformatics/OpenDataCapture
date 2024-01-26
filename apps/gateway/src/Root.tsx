e6950dcd11aea4cac9e0d15219564899e452321c
joshunrau <contact@joshuaunrau.com>
2024-01-26 10:09:24 -0500

    update styling

--- apps/gateway/src/Root.tsx
+++ apps/gateway/src/Root.tsx
@@ -1,4 +1,7 @@
+import { NotificationHub, useNotificationsStore } from '@douglasneuroinformatics/ui';
 import { InstrumentRenderer } from '@open-data-capture/instrument-renderer';
+import { Navbar } from '@open-data-capture/react-core';
+import { useTranslation } from 'react-i18next';
 
 import './services/i18n';
 
@@ -7,13 +10,22 @@ export type RootProps = {
 };
 
 export const Root = ({ bundle }: RootProps) => {
-  const handleSubmit = (data: unknown) => {
-    // eslint-disable-next-line no-alert
-    alert(JSON.stringify(data, null, 2));
+  const { i18n } = useTranslation('core');
+  const notifications = useNotificationsStore();
+
+  const handleSubmit = () => {
+    notifications.addNotification({ message: 'Success', type: 'success' });
   };
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <InstrumentRenderer bundle={bundle} className="min-h-full" onSubmit={handleSubmit} />
+    <div className="flex h-screen flex-col bg-slate-100 text-slate-900 dark:bg-slate-900 dark:text-slate-100">
+      <header>
+        <Navbar i18n={i18n} />;
+      </header>
+      <main className="container flex max-w-3xl flex-grow flex-col pt-16">
+        <InstrumentRenderer bundle={bundle} className="min-h-full w-full" onSubmit={handleSubmit} />
+      </main>
+      <NotificationHub />
     </div>
   );
 };
