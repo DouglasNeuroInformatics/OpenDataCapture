d01c3ce06ccbc037c57b384600ec8dec3e661590
joshunrau <contact@joshuaunrau.com>
2024-01-25 20:35:33 -0500

    add client hook

--- apps/gateway/src/App.tsx
+++ apps/gateway/src/App.tsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
+
 export type AppProps = Record<string, unknown>;
 
 export const App = (props: AppProps) => {
+  const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Hello World</h1>
+      <div>
+        <p>Count: {count}</p>
+        <button type="button" onClick={() => setCount(count + 1)}>
+          Increment Count
+        </button>
+      </div>
       <p>Props: {JSON.stringify(props, null, 2)}</p>
     </div>
   );
