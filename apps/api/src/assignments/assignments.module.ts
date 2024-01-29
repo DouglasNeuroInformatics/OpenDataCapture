582f99d7dbbe16aeb1774b7d90009fb6558b5035
joshunrau <contact@joshuaunrau.com>
2024-01-29 13:14:39 -0500

    import prisma module in assignment module

--- apps/api/src/assignments/assignments.module.ts
+++ apps/api/src/assignments/assignments.module.ts
@@ -2,6 +2,7 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 
 import { InstrumentsModule } from '@/instruments/instruments.module';
+import { PrismaModule } from '@/prisma/prisma.module';
 import { SubjectsModule } from '@/subjects/subjects.module';
 
 import { AssignmentsController } from './assignments.controller';
@@ -10,7 +11,7 @@ import { AssignmentsService } from './assignments.service';
 @Module({
   controllers: [AssignmentsController],
   exports: [AssignmentsService],
-  imports: [HttpModule, InstrumentsModule, SubjectsModule],
+  imports: [HttpModule, InstrumentsModule, PrismaModule.forFeature('Assignment'), SubjectsModule],
   providers: [AssignmentsService]
 })
 export class AssignmentsModule {}
