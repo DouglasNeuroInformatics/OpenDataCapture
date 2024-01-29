ad0c7db97f5e2e58e9ae2f95adc4e0357e281993
joshunrau <contact@joshuaunrau.com>
2024-01-29 15:12:02 -0500

    add GATEWAY_AUTH_TOKEN

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -10,14 +10,19 @@ const __dirname = path.dirname(__filename);
 
 const projectRoot = path.resolve(__dirname, '..');
 
-const secretKey = await new Promise((resolve, reject) => {
-  crypto.generateKey('hmac', { length: 256 }, (err, key) => {
-    if (err) {
-      return reject(err);
-    }
-    resolve(key.export().toString('hex'));
+/** @returns {Promise<string>} */
+const generateSecureToken = async () => {
+  return new Promise((resolve, reject) => {
+    crypto.generateKey('hmac', { length: 256 }, (err, key) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(key.export().toString('hex'));
+    });
   });
-});
+};
+
+const secretKey = await generateSecureToken();
 
 const gatewayDataDir = path.resolve(projectRoot, 'apps/gateway/data');
 fs.mkdir(gatewayDataDir, { recursive: true });
@@ -27,6 +32,7 @@ let env = await fs.readFile(path.resolve(projectRoot, '.env.template'), 'utf-8')
 env = env.replace('PROJECT_ROOT=', `PROJECT_ROOT=${projectRoot}`);
 env = env.replace('SECRET_KEY=', `SECRET_KEY=${secretKey}`);
 env = env.replace('GATEWAY_DATABASE_URL=', `GATEWAY_DATABASE_URL=${gatewayDbUrl}`);
+env = env.replace('GATEWAY_AUTH_TOKEN=', 'GATEWAY_AUTH_TOKEN=' + (await generateSecureToken()));
 
 const filepath = path.resolve(projectRoot, '.env');
 await fs.writeFile(filepath, env, 'utf-8').then(() => {
