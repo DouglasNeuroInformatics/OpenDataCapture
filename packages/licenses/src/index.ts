889f310430df59769b7a98e42c32ad9ac006f43d
joshunrau <contact@joshuaunrau.com>
2024-01-31 09:21:27 -0500

    forgot to export

--- packages/licenses/src/index.ts
+++ packages/licenses/src/index.ts
@@ -1,12 +1,12 @@
 import _licenses from '../assets/licenses.json';
 
-type LicenseIdentifier = 'UNLICENSED' | Extract<keyof typeof _licenses, string>;
-
 type License = {
   isOpenSource: boolean;
   name: string;
   reference: string;
 };
+
+type LicenseIdentifier = 'UNLICENSED' | Extract<keyof typeof _licenses, string>;
 
 const licenses = new Map(Object.entries(_licenses)) as Map<LicenseIdentifier, License>;
 
@@ -15,3 +15,5 @@ licenses.set('UNLICENSED', {
   name: 'Unlicensed/Unknown',
   reference: 'https://choosealicense.com'
 });
+
+export { type License, type LicenseIdentifier, licenses };
