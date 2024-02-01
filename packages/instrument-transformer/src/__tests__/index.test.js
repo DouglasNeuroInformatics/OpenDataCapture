4fa71ae72f86a5b2d8402d80e416f03b4f2786f9
joshunrau <contact@joshuaunrau.com>
2024-02-01 10:44:16 -0500

    add test for instrument transformer

---
+++ packages/instrument-transformer/src/__tests__/index.test.js
@@ -0,0 +1,42 @@
+/// <reference types="bun-types" />
+
+import { beforeEach, describe, expect, it } from 'bun:test';
+import fs from 'fs/promises';
+import module from 'module';
+import path from 'path';
+import url from 'url';
+
+import { InstrumentTransformer } from '../index.js';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const require = module.createRequire(__dirname);
+
+const loadSource = (id) => fs.readFile(require.resolve(id), 'utf-8');
+
+const sources = {
+  clickTask: await loadSource('@open-data-capture/instrument-library/interactive/click-task.tsx'),
+  happinessQuestionnaire: await loadSource('@open-data-capture/instrument-library/forms/happiness-questionnaire.ts')
+};
+
+describe('InstrumentTransformer', () => {
+  /** @type {InstrumentTransformer} */
+  let transformer;
+
+  beforeEach(() => {
+    transformer = new InstrumentTransformer();
+  });
+
+  describe('Click Task', () => {
+    it('should successfully transpile', async () => {
+      expect(transformer.generateBundle(sources.clickTask)).resolves.toBeString();
+    });
+  });
+
+  describe('Happiness Questionnaire', () => {
+    it('should successfully transpile', async () => {
+      expect(transformer.generateBundle(sources.happinessQuestionnaire)).resolves.toBeString();
+    });
+  });
+});
