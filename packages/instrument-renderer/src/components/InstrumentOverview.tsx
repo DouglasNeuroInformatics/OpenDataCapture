52f078b754f672fe07301577810f44c66a15e251
joshunrau <contact@joshuaunrau.com>
2024-02-05 09:42:38 -0500

    add new items to summary

--- packages/instrument-renderer/src/components/InstrumentOverview.tsx
+++ packages/instrument-renderer/src/components/InstrumentOverview.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 
 import { Button, StepperContext } from '@douglasneuroinformatics/ui';
 import type { AnyUnilingualInstrument } from '@open-data-capture/common/instrument';
+import { licenses } from '@open-data-capture/licenses';
 import { useTranslation } from 'react-i18next';
 
 import { InstrumentOverviewItem } from './InstrumentOverviewItem';
@@ -24,12 +25,18 @@ export const InstrumentOverview = ({ instrument }: InstrumentOverviewProps) => {
     language = instrument.language;
   }
 
+  const license = licenses.get(instrument.details.license);
+
+  instrument.details.referenceUrl;
+
   return (
     <div className="mb-2">
       <h3 className="text-xl font-semibold">{t('steps.overview')}</h3>
       <div className="mb-8">
         <InstrumentOverviewItem heading={t('description')} text={instrument.details.description} />
         <InstrumentOverviewItem heading={t('language')} text={language} />
+        <InstrumentOverviewItem heading={t('authors')} text={instrument.details.authors ?? 'NA'} />
+        <InstrumentOverviewItem heading={t('license')} text={license?.name ?? 'NA'} />
         <InstrumentOverviewItem
           heading={t('estimatedDuration')}
           text={t('minutes', {
@@ -37,6 +44,12 @@ export const InstrumentOverview = ({ instrument }: InstrumentOverviewProps) => {
           })}
         />
         <InstrumentOverviewItem heading={t('instructions')} text={instrument.details.instructions} />
+        {instrument.details.referenceUrl && (
+          <InstrumentOverviewItem heading={t('referenceLink')} text={instrument.details.referenceUrl} />
+        )}
+        {instrument.details.sourceUrl && (
+          <InstrumentOverviewItem heading={t('sourceLink')} text={instrument.details.sourceUrl} />
+        )}
       </div>
       <Button
         className="w-full"
