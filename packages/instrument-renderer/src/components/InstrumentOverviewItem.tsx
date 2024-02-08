cf64b1bb5d41472934b1ecd50d225cb76a127a67
joshunrau <contact@joshuaunrau.com>
2024-02-08 17:45:17 -0500

    add link kind

--- packages/instrument-renderer/src/components/InstrumentOverviewItem.tsx
+++ packages/instrument-renderer/src/components/InstrumentOverviewItem.tsx
@@ -1,10 +1,11 @@
 export type InstrumentOverviewItemProps = {
   afterText?: React.ReactNode;
   heading: string;
+  kind?: 'default' | 'link';
   text: string | string[];
 };
 
-export const InstrumentOverviewItem = ({ afterText, heading, text }: InstrumentOverviewItemProps) => {
+export const InstrumentOverviewItem = ({ afterText, heading, kind, text }: InstrumentOverviewItemProps) => {
   return (
     <div className="my-5 text-slate-700 dark:text-slate-300">
       <h5 className="mb-1 text-lg font-medium text-slate-900 dark:text-slate-100">{heading}</h5>
@@ -18,7 +19,18 @@ export const InstrumentOverviewItem = ({ afterText, heading, text }: InstrumentO
         </ul>
       ) : (
         <div className="flex items-center gap-2">
-          <p>{text}</p>
+          {kind === 'link' ? (
+            <a
+              className="underline underline-offset-1 hover:text-slate-950 hover:dark:text-slate-50"
+              href={text}
+              rel="noreferrer"
+              target="_blank"
+            >
+              {text}
+            </a>
+          ) : (
+            <p>{text}</p>
+          )}
           {afterText}
         </div>
       )}
