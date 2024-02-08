33701d9481be92bbb226e5c5c28e9791d3d6da25
joshunrau <contact@joshuaunrau.com>
2024-02-08 10:06:11 -0500

    add InstrumentMeasures

--- apps/api/src/instrument-records/instrument-measures.service.ts
+++ apps/api/src/instrument-records/instrument-measures.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import type { FormDataType } from '@douglasneuroinformatics/form-types';
+import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import type { Json } from '@open-data-capture/common/core';
+import type {
+  InstrumentMeasure,
+  InstrumentMeasureValue,
+  InstrumentMeasures
+} from '@open-data-capture/common/instrument';
+import type { Prisma } from '@prisma/client';
+import { match } from 'ts-pattern';
 
 @Injectable()
-export class InstrumentMeasuresService {}
+export class InstrumentMeasuresService {
+  private readonly logger = new Logger(InstrumentMeasuresService.name);
+
+  computeMeasure(measure: InstrumentMeasure, data: FormDataType | Json | Prisma.JsonValue) {
+    return match(measure)
+      .with({ kind: 'computed' }, (measure) => {
+        return measure.value(data);
+      })
+      .with({ kind: 'const' }, (measure) => {
+        if (!(data && typeof data === 'object')) {
+          this.logger.error({ data, message: 'Invalid Data' });
+          const label = typeof measure.label === 'string' ? measure.label : (measure.label?.en ?? measure.label?.fr)!;
+          throw new InternalServerErrorException(`Failed to compute measure '${label}': data must be object'`);
+        }
+        return Reflect.get(data, measure.ref);
+      })
+      .exhaustive();
+  }
+
+  computeMeasures(measures: InstrumentMeasures, data: FormDataType | Json | Prisma.JsonValue) {
+    const computedMeasures: Record<string, InstrumentMeasureValue> = {};
+    for (const key in measures) {
+      computedMeasures[key] = this.computeMeasure(measures[key], data);
+    }
+    return computedMeasures;
+  }
+}
