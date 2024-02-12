090c7c7aa5884541d2dbaddcecf832334420f013
joshunrau <contact@joshuaunrau.com>
2024-02-12 12:47:50 -0500

    add loading screen

--- apps/web/src/features/setup/components/SetupLoadingScreen.tsx
+++ apps/web/src/features/setup/components/SetupLoadingScreen.tsx
@@ -1,19 +1,7 @@
-import { Spinner } from '@douglasneuroinformatics/ui';
+import { LoadingScreen } from '@open-data-capture/react-core';
 import { useTranslation } from 'react-i18next';
 
 export const SetupLoadingScreen = () => {
   const { t } = useTranslation('setup');
-  return (
-    <div className="my-auto flex h-screen w-screen flex-col items-center justify-center">
-      <div className="mt-24 text-center">
-        <p className="text-sm font-semibold uppercase tracking-wide text-slate-700 dark:text-slate-300">
-          {t('loadingSubtitle')}
-        </p>
-        <h1 className="text-3xl font-extrabold tracking-tight sm:text-4xl md:text-5xl">{t('loadingTitle')}</h1>
-      </div>
-      <div className="h-min p-48">
-        <Spinner />
-      </div>
-    </div>
-  );
+  return <LoadingScreen subtitle={t('loadingSubtitle')} title={t('loadingTitle')} />;
 };
