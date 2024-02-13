6cfe71b9485760a9c886db55a50639e46e4fe668
joshunrau <contact@joshuaunrau.com>
2024-02-13 10:12:01 -0500

    use lodash es in i18n

--- apps/marketing/src/i18n/index.ts
+++ apps/marketing/src/i18n/index.ts
@@ -1,5 +1,5 @@
 import type { Language } from '@open-data-capture/common/core';
-import _ from 'lodash';
+import { get } from 'lodash-es';
 
 import { translations } from './translations';
 
@@ -16,7 +16,7 @@ type Translations = typeof translations;
 type TranslationKey = ExtractTranslationKey<Translations>;
 
 function getTranslation(key: TranslationKey, language: Language) {
-  const value = _.get(translations, key);
+  const value = get(translations, key);
   if (typeof value === 'string') {
     return value;
   }
