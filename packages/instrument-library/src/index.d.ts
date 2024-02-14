c9413896597a31d71be02d35a079769df3ce1716
joshunrau <contact@joshuaunrau.com>
2024-02-14 12:55:22 -0500

    export empty examples

--- packages/instrument-library/src/index.d.ts
+++ packages/instrument-library/src/index.d.ts
@@ -5,6 +5,9 @@ export const enhancedDemographicsQuestionnaire: InstrumentLibraryItem;
 export const happinessQuestionnaire: InstrumentLibraryItem;
 export const miniMentalStateExamination: InstrumentLibraryItem;
 export const montrealCognitiveAssessment: InstrumentLibraryItem;
+export const emptyBilingualForm: InstrumentLibraryItem;
+export const emptyUnilingualForm: InstrumentLibraryItem;
+export const genericFormInstrument: InstrumentLibraryItem;
 
 export const clickTask: InstrumentLibraryItem;
 export const reactionTimeTask: InstrumentLibraryItem;
