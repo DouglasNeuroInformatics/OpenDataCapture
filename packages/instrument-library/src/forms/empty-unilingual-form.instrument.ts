bc3bd222a02dc13a38eea68e11cd92cd070bd30d
joshunrau <contact@joshuaunrau.com>
2024-02-14 12:52:06 -0500

    add empty examples

---
+++ packages/instrument-library/src/forms/empty-unilingual-form.instrument.ts
@@ -0,0 +1,24 @@
+/* eslint-disable perfectionist/sort-objects */
+
+const { InstrumentFactory } = await import('/runtime/v0.0.1/core.js');
+const { z } = await import('/runtime/v0.0.1/zod.js');
+
+const instrumentFactory = new InstrumentFactory({
+  kind: 'FORM',
+  language: 'en',
+  validationSchema: z.object({})
+});
+
+export default instrumentFactory.defineInstrument({
+  name: '',
+  tags: [],
+  version: 1.0,
+  content: {},
+  details: {
+    description: '',
+    estimatedDuration: 1,
+    instructions: [],
+    license: 'UNLICENSED',
+    title: ''
+  }
+});
