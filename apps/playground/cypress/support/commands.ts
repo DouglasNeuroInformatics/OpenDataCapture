047ad3eb79821231cc15d54d71ce5dd304c426e2
joshunrau <contact@joshuaunrau.com>
2024-02-20 11:00:45 -0500

    add load editor

--- apps/playground/cypress/support/commands.ts
+++ apps/playground/cypress/support/commands.ts
@@ -1,25 +1,18 @@
-/// <reference types="cypress" />
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+/* eslint-disable @typescript-eslint/no-namespace */
+/* eslint-disable @typescript-eslint/ban-types */
 
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(username: string, password: string): Chainable<void>
-//     //   drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//     //   dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//     //   visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      loadEditor(): Chainable<void>;
+    }
+  }
+}
+
+Cypress.Commands.add('loadEditor', () => {
+  cy.visit('/');
+  cy.get('h1').contains('Loading Editor and Toolchain').as('loading-heading');
+  cy.get('@loading-heading').should('exist');
+  cy.get('@loading-heading').should('not.exist');
+});
