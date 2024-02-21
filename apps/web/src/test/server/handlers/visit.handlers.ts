4bccc459799c73fa62d248a0ed8226f1b5ca6a95
David Roper <davidroper321@gmail.com>
2024-02-21 11:50:16 -0500

    valid date for visit handler

--- apps/web/src/test/server/handlers/visit.handlers.ts
+++ apps/web/src/test/server/handlers/visit.handlers.ts
@@ -7,7 +7,7 @@ export const VisitHandler = [
   http.post('/v1/visits', () => {
     return HttpResponse.json<Visit>({
       createdAt: new Date(),
-      date: new Date('2023-11-14'),
+      date: new Date('2000-11-14'),
       groupId: '1',
       id: '12345',
       subject: testSubject,
