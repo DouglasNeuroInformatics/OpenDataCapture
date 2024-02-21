6e6df8d5e68ad85697931fdb3f839a15da5f6603
joshunrau <contact@joshuaunrau.com>
2024-02-21 09:00:34 -0500

    add clear editor test

--- apps/playground/cypress/e2e/index.cy.ts
+++ apps/playground/cypress/e2e/index.cy.ts
@@ -1,8 +1,6 @@
 describe('/', () => {
   it('should load the editor', () => {
-    cy.loadEditor();
-    // cy.get('.monaco-editor').as('editor');
-    // cy.visit('/');
-    // cy.get('@editor').click().focused().type('Hello World');
+    cy.editor('load');
+    cy.editor('clear');
   });
 });
