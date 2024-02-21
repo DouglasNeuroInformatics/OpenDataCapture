4c188e654a924025c4a931b0c6bd4b305976818b
joshunrau <contact@joshuaunrau.com>
2024-02-21 10:35:20 -0500

    use error in useTranspiler

--- apps/playground/src/components/CompileErrorFallback.tsx
+++ apps/playground/src/components/CompileErrorFallback.tsx
@@ -4,12 +4,14 @@ import { StackTrace } from './StackTrace';
 
 export const CompileErrorFallback: React.FC<{ className?: string; error: Error }> = ({ className, error }) => {
   return (
-    <Card className={cn('flex h-min flex-col p-3 tracking-tight', className)}>
-      <div className="space-y-1 py-3">
-        <h3 className="text-lg font-bold">Failed to Compile</h3>
-        <span className="font-semibold text-red-500">Error: {error.message}</span>
+    <Card className={cn('flex max-h-full flex-col p-3 tracking-tight', className)}>
+      <div className="h-full overflow-scroll">
+        <div className="space-y-1 py-3">
+          <h3 className="text-lg font-bold">Failed to Compile</h3>
+          <span className="font-semibold text-red-500">Error: {error.message}</span>
+        </div>
+        {error.stack && <StackTrace stack={error.stack} />}
       </div>
-      {error.stack && <StackTrace stack={error.stack} />}
     </Card>
   );
 };
