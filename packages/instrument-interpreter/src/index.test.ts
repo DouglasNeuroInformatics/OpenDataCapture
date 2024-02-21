3107984cfc8f19574be8a6d84444efb7e77940f2
joshunrau <contact@joshuaunrau.com>
2024-02-21 14:33:54 -0500

    restore test

--- packages/instrument-interpreter/src/index.test.ts
+++ packages/instrument-interpreter/src/index.test.ts
@@ -1,38 +1,35 @@
-// Issue with circular dependency
-// @open-data-capture/runtime-v0.0.1, @open-data-capture/instrument-library, @open-data-capture/instrument-interpreter
+import { describe, expect, it } from 'bun:test';
 
-// import { describe, expect, it } from 'bun:test';
+import {
+  briefPsychiatricRatingScale,
+  clickTask,
+  enhancedDemographicsQuestionnaire,
+  happinessQuestionnaire,
+  miniMentalStateExamination,
+  montrealCognitiveAssessment
+} from '@open-data-capture/instrument-library';
 
-// import {
-//   briefPsychiatricRatingScale,
-//   clickTask,
-//   enhancedDemographicsQuestionnaire,
-//   happinessQuestionnaire,
-//   miniMentalStateExamination,
-//   montrealCognitiveAssessment
-// } from '@open-data-capture/instrument-library';
+import { InstrumentInterpreter } from './index';
 
-// import { InstrumentInterpreter } from './index';
+describe('InstrumentInterpreter', () => {
+  const interpreter = new InstrumentInterpreter();
 
-// describe('InstrumentInterpreter', () => {
-//   const interpreter = new InstrumentInterpreter();
-
-//   it('should evaluate the brief psychiatric rating scale', () => {
-//     expect(interpreter.interpret(briefPsychiatricRatingScale.bundle)).resolves.toBeObject();
-//   });
-//   it('should evaluate the enhanced demographics questionnaire', () => {
-//     expect(interpreter.interpret(enhancedDemographicsQuestionnaire.bundle)).resolves.toBeObject();
-//   });
-//   it('should evaluate the happiness questionnaire', () => {
-//     expect(interpreter.interpret(happinessQuestionnaire.bundle)).resolves.toBeObject();
-//   });
-//   it('should evaluate the mini mental state examination', () => {
-//     expect(interpreter.interpret(miniMentalStateExamination.bundle)).resolves.toBeObject();
-//   });
-//   it('should evaluate the montreal cognitive assessment', () => {
-//     expect(interpreter.interpret(montrealCognitiveAssessment.bundle)).resolves.toBeObject();
-//   });
-//   it('should evaluate the click task', () => {
-//     expect(interpreter.interpret(clickTask.bundle)).resolves.toBeObject();
-//   });
-// });
+  it('should evaluate the brief psychiatric rating scale', () => {
+    expect(interpreter.interpret(briefPsychiatricRatingScale.bundle)).resolves.toBeObject();
+  });
+  it('should evaluate the enhanced demographics questionnaire', () => {
+    expect(interpreter.interpret(enhancedDemographicsQuestionnaire.bundle)).resolves.toBeObject();
+  });
+  it('should evaluate the happiness questionnaire', () => {
+    expect(interpreter.interpret(happinessQuestionnaire.bundle)).resolves.toBeObject();
+  });
+  it('should evaluate the mini mental state examination', () => {
+    expect(interpreter.interpret(miniMentalStateExamination.bundle)).resolves.toBeObject();
+  });
+  it('should evaluate the montreal cognitive assessment', () => {
+    expect(interpreter.interpret(montrealCognitiveAssessment.bundle)).resolves.toBeObject();
+  });
+  it('should evaluate the click task', () => {
+    expect(interpreter.interpret(clickTask.bundle)).resolves.toBeObject();
+  });
+});
