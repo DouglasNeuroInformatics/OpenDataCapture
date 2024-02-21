6b6cb5bc7d79bb0e994ea6ff4fe86d2cff6c4d9d
David Roper <davidroper321@gmail.com>
2024-02-21 15:45:16 -0500

    new mock GET api call for subject page

--- apps/web/cypress/e2e/subject-view.cy.ts
+++ apps/web/cypress/e2e/subject-view.cy.ts
@@ -5,18 +5,17 @@ import { adminUser } from '@/test/server/stubs';
 
 describe('Subject view test', () => {
   it('passes', () => {
-
     cy.login(adminUser.username, adminUser.password);
 
     //navigates the view sujects, selects the first subject and views their graph
-    cy.get('button[data-cy="view-subjects"]').first().click({force: true});
+    cy.get('button[data-cy="view-subjects"]').first().click({ force: true });
 
     //select first user and their instrument dropdown
     cy.get('td').first().click();
     // cy.get('div[data-cy="instruement-select"]').click();
     cy.get('a[data-cy="subject-table"]').click();
     cy.get('div[data-cy="select-instrument"]').click();
-    cy.get('button[id*="headlessui-menu-item-:r1"]').eq(2).click();
+    cy.get('button[id*="headlessui-menu-item-:r1"]').eq(0).click();
 
     cy.get('div[data-cy="time-dropdown"]').click();
     cy.get('button[id*="headlessui-menu-item-:r1"]').eq(2).click();
@@ -30,13 +29,10 @@ describe('Subject view test', () => {
 
     cy.get('div[data-cy="measure-select"]').click();
     cy.get('li[id*="headlessui-listbox-option-:r2"]').click();
-    
+
     cy.get('div[class="recharts-wrapper"]').click();
 
     cy.get('div[data-cy="time-select"]').click();
     cy.get('button[id*="headlessui-menu-item-:r2"]').eq(1).click();
-    
-
-
-  })
-})
+  });
+});
