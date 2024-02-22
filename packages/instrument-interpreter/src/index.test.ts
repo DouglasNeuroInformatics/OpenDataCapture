9c2e6a4cc1e92096aa0ca4a4f3c6cecab02a27ab
joshunrau <contact@joshuaunrau.com>
2024-02-22 15:09:23 -0500

    setup vitest with browser

--- packages/instrument-interpreter/src/index.test.ts
+++ packages/instrument-interpreter/src/index.test.ts
@@ -1,13 +1,5 @@
-import { describe, expect, it } from 'bun:test';
-
-import {
-  briefPsychiatricRatingScale,
-  clickTask,
-  enhancedDemographicsQuestionnaire,
-  happinessQuestionnaire,
-  miniMentalStateExamination,
-  montrealCognitiveAssessment
-} from '@open-data-capture/instrument-library';
+import * as lib from '@open-data-capture/instrument-library';
+import { describe, expect, it } from 'vitest';
 
 import { InstrumentInterpreter } from './index.js';
 
@@ -15,21 +7,21 @@ describe('InstrumentInterpreter', () => {
   const interpreter = new InstrumentInterpreter();
 
   it('should evaluate the brief psychiatric rating scale', () => {
-    expect(interpreter.interpret(briefPsychiatricRatingScale.bundle)).resolves.toBeObject();
+    expect(interpreter.interpret(lib.briefPsychiatricRatingScale.bundle)).resolves.toBeInstanceOf(Object);
   });
   it('should evaluate the enhanced demographics questionnaire', () => {
-    expect(interpreter.interpret(enhancedDemographicsQuestionnaire.bundle)).resolves.toBeObject();
+    expect(interpreter.interpret(lib.enhancedDemographicsQuestionnaire.bundle)).resolves.toBeInstanceOf(Object);
   });
   it('should evaluate the happiness questionnaire', () => {
-    expect(interpreter.interpret(happinessQuestionnaire.bundle)).resolves.toBeObject();
+    expect(interpreter.interpret(lib.happinessQuestionnaire.bundle)).resolves.toBeInstanceOf(Object);
   });
   it('should evaluate the mini mental state examination', () => {
-    expect(interpreter.interpret(miniMentalStateExamination.bundle)).resolves.toBeObject();
+    expect(interpreter.interpret(lib.miniMentalStateExamination.bundle)).resolves.toBeInstanceOf(Object);
   });
   it('should evaluate the montreal cognitive assessment', () => {
-    expect(interpreter.interpret(montrealCognitiveAssessment.bundle)).resolves.toBeObject();
+    expect(interpreter.interpret(lib.montrealCognitiveAssessment.bundle)).resolves.toBeInstanceOf(Object);
   });
   it('should evaluate the click task', () => {
-    expect(interpreter.interpret(clickTask.bundle)).resolves.toBeObject();
+    expect(interpreter.interpret(lib.clickTask.bundle)).resolves.toBeInstanceOf(Object);
   });
 });
