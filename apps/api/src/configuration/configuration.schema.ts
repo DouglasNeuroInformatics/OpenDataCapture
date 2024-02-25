fa64232161645cd3236b738a348e5c53dc70b9e8
joshunrau <contact@joshuaunrau.com>
2024-02-25 13:19:56 -0500

    add new vars

--- apps/api/src/configuration/configuration.schema.ts
+++ apps/api/src/configuration/configuration.schema.ts
@@ -1,22 +1,54 @@
 import { $BooleanString } from '@open-data-capture/common/core';
 import { z } from 'zod';
 
-export const $Configuration = z.object({
-  API_DEV_SERVER_PORT: z.coerce.number().positive().int().optional(),
-  API_PROD_SERVER_PORT: z.coerce.number().positive().int().default(80),
-  DEBUG: $BooleanString,
-  GATEWAY_API_KEY: z.string().min(32),
-  GATEWAY_BASE_URL: z.string().url(),
-  GATEWAY_ENABLED: $BooleanString,
-  GATEWAY_REFRESH_INTERVAL: z.coerce.number().positive().int(),
-  MONGO_DIRECT_CONNECTION: z.string().optional(),
-  MONGO_REPLICA_SET: z.string().optional(),
-  MONGO_RETRY_WRITES: z.string().optional(),
-  MONGO_URI: z.string().url(),
-  MONGO_WRITE_CONCERN: z.string().optional(),
-  NODE_ENV: z.enum(['development', 'production', 'test']),
-  SECRET_KEY: z.string().min(32),
-  VERBOSE: $BooleanString
-});
+export const $Configuration = z
+  .object({
+    API_DEV_SERVER_PORT: z.coerce.number().positive().int().optional(),
+    API_PROD_SERVER_PORT: z.coerce.number().positive().int().default(80),
+    DEBUG: $BooleanString,
+    GATEWAY_API_KEY: z.string().min(32),
+    GATEWAY_DEV_SERVER_PORT: z.coerce.number().positive().int().optional(),
+    GATEWAY_ENABLED: $BooleanString,
+    GATEWAY_INTERNAL_NETWORK_URL: z.string().url().optional(),
+    GATEWAY_REFRESH_INTERVAL: z.coerce.number().positive().int(),
+    GATEWAY_SITE_ADDRESS: z.string().url().optional(),
+    MONGO_DIRECT_CONNECTION: z.string().optional(),
+    MONGO_REPLICA_SET: z.string().optional(),
+    MONGO_RETRY_WRITES: z.string().optional(),
+    MONGO_URI: z.string().url(),
+    MONGO_WRITE_CONCERN: z.string().optional(),
+    NODE_ENV: z.enum(['development', 'production', 'test']),
+    SECRET_KEY: z.string().min(32),
+    VERBOSE: $BooleanString
+  })
+  .superRefine((env, ctx) => {
+    if (env.NODE_ENV === 'production') {
+      if (!env.GATEWAY_SITE_ADDRESS) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'GATEWAY_SITE_ADDRESS must be defined in production'
+        });
+      }
+      if (!env.GATEWAY_INTERNAL_NETWORK_URL) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'GATEWAY_INTERNAL_NETWORK_URL must be defined in production'
+        });
+      }
+    } else if (env.NODE_ENV === 'development') {
+      if (!env.API_DEV_SERVER_PORT) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'API_DEV_SERVER_PORT must be defined in production'
+        });
+      }
+      if (!env.GATEWAY_DEV_SERVER_PORT) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'GATEWAY_DEV_SERVER_PORT must be defined in production'
+        });
+      }
+    }
+  });
 
 export type Configuration = z.infer<typeof $Configuration>;
