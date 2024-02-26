b02365d46a2fbe520fe02c2fd34d8fc92521f6a9
joshunrau <contact@joshuaunrau.com>
2024-02-26 13:46:51 -0500

    move available-instruments

--- apps/web/src/components/Layout/Layout.tsx
+++ apps/web/src/components/Layout/Layout.tsx
@@ -35,14 +35,6 @@ export const Layout = () => {
         label: t(`navLinks.viewSubjects`)
       });
     }
-    if (currentUser?.ability.can('create', 'InstrumentRecord')) {
-      globalItems.push({
-        'data-cy': 'view-instrument',
-        icon: EyeIcon,
-        id: '/instruments/available-instruments',
-        label: t('navLinks.availableInstruments')
-      });
-    }
     const visitItems: NavItem[] = [];
     if (currentUser?.ability.can('create', 'Visit')) {
       visitItems.push({
@@ -50,6 +42,14 @@ export const Layout = () => {
         icon: UserPlusIcon,
         id: '/visits/add-visit',
         label: t('navLinks.addVisit')
+      });
+    }
+    if (currentUser?.ability.can('create', 'InstrumentRecord')) {
+      visitItems.push({
+        'data-cy': 'view-instrument',
+        icon: EyeIcon,
+        id: '/instruments/available-instruments',
+        label: t('navLinks.availableInstruments')
       });
     }
     setNavItems([globalItems, visitItems]);
