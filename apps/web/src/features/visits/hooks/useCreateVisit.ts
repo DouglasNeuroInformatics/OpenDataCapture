4054956505ec6dffb3de21de21fc7f1deb70513f
joshunrau <contact@joshuaunrau.com>
2024-02-28 11:04:07 -0500

    extract out visit and get full name from form

---
+++ apps/web/src/features/visits/hooks/useCreateVisit.ts
@@ -0,0 +1,17 @@
+import { useNotificationsStore } from '@douglasneuroinformatics/ui';
+import { $Visit, type CreateVisitData } from '@open-data-capture/common/visit';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+
+export function useCreateVisit() {
+  const { addNotification } = useNotificationsStore();
+  return useMutation({
+    mutationFn: async (data: CreateVisitData) => {
+      const response = await axios.post('/v1/visits', data);
+      return $Visit.parseAsync(response.data);
+    },
+    onSuccess() {
+      addNotification({ type: 'success' });
+    }
+  });
+}
