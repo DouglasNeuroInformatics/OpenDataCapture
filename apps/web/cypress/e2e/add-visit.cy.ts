17e5e0cd1c57f806270698e6fda60787f6c9a008
david-roper <davidroper321@gmail.com>
2024-03-01 17:33:06 -0500

    now finds begin form with regex

--- apps/web/cypress/e2e/add-visit.cy.ts
+++ apps/web/cypress/e2e/add-visit.cy.ts
@@ -41,8 +41,10 @@ describe('look at questionaire', () => {
     //navigate to view instrument page, select a form and confirm subject info is autofilled
     cy.get('button[data-cy="view-instrument"]').click({ force: true });
     cy.url().should('include', '/instruments/available-instruments');
-    cy.wait(100);
+    cy.wait(1000);
     cy.get('h3[data-cy="instrument-title"]').contains('Happiness Questionnaire').click();
-    cy.get('button').contains('Begin').click({ force: true });
+    cy.get('button')
+      .contains(new RegExp('^' + 'Begin' + '$', 'g'))
+      .click({ force: true });
   });
 });
