9561d007911449bd9dd72e95306431c501779319
david-roper <davidroper321@gmail.com>
2024-03-04 11:34:35 -0500

    removed unused handler

--- apps/web/src/test/server/handlers/form.handlers.ts
+++ apps/web/src/test/server/handlers/form.handlers.ts
@@ -1,51 +1,5 @@
 import { HttpResponse, http } from 'msw';
 
-//create an example for with data type DataForm as its input labels
-// type DataForm = {
-//   a: number
-// }
-
-//export the form object upon the get request
-// export const FormHandlers = [
-//   http.get('/v1/instruments/available?kind=form', () => {
-//     return HttpResponse.json<FormInstrument<DataForm>>({
-//       content: {
-//         a: {
-//           kind: 'numeric',
-//           label: 'My NUmber',
-//           variant: 'slider',
-//           min: 1,
-//           max: 4
-//         }
-//       },
-//       details: {
-//         description:
-//           '\n      The Brief Psychiatric Rating Scale is a rating scale which a clinician or researcher may use to\n      measure psychiatric symptoms such as depression, anxiety, hallucinations and unusual behavior.\n      The scale is one of the oldest, most widely used scales to measure psychotic symptoms and was\n      first published in 1962.',
-//         estimatedDuration: 30,
-//         instructions:  "\n      Please enter the score for the term which best describes the patient's condition. 0 = not assessed, 1 = not\n      present, 2 = very mild, 3 = mild, 4 = moderate, 5 = moderately severe, 6 = severe, 7 = extremely severe.",
-//         title: 'Brief Psychiatric Rating Scale'
-//       },
-//       id: '6557bcdc930d9604d6355516',
-//       kind: 'form',
-//       language: 'en',
-//       measures: {
-//         totalScore: {
-//           label: 'Total Score',
-//           value: (data) => {
-//             return data.a
-//           }
-//         }
-//       },
-//       name: 'BriefPsychiatricRatingScale',
-//       tags: ['Schizophrenia', 'Psychosis'],
-//       version: 1,
-//       validationSchema: z.object({
-//         a: z.number()
-//       })
-//     });
-//   })
-// ];
-
 export const FormHandlers = [
   //gather all the forms (happiness form, psychosis form, etc.)
   http.get('/v1/instruments/available?kind=form', () => {
