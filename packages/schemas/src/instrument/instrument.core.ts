f52826c766dd42acebe80683c73f3ef0abc9815d
joshunrau <contact@joshuaunrau.com>
2024-03-25 13:55:14 -0400

    refactor: prune unused dependencies and exports

--- packages/schemas/src/instrument/instrument.core.ts
+++ packages/schemas/src/instrument/instrument.core.ts
@@ -1,17 +1,11 @@
-import type { FormDataType } from '@douglasneuroinformatics/form-types';
 import { z } from 'zod';
 
 import { $FormInstrument } from './instrument.form.js';
 import { $InteractiveInstrument } from './instrument.interactive.js';
 
 import type { Json, Language } from '../core/core.js';
-import type { InstrumentKind, InstrumentLanguage } from './instrument.base.js';
-import type {
-  AnyMultilingualFormInstrument,
-  AnyUnilingualFormInstrument,
-  FormInstrument,
-  StrictFormInstrument
-} from './instrument.form.js';
+import type { InstrumentKind } from './instrument.base.js';
+import type { AnyMultilingualFormInstrument, AnyUnilingualFormInstrument, FormInstrument } from './instrument.form.js';
 import type { InteractiveInstrument } from './instrument.interactive.js';
 
 export const $AnyInstrument = z.union([$FormInstrument, $InteractiveInstrument]) satisfies z.ZodType<AnyInstrument>;
@@ -27,36 +21,3 @@ export type SomeMultilingualInstrument<TKind extends InstrumentKind> = Extract<
   AnyMultilingualFormInstrument,
   { kind: TKind }
 >;
-
-// FOR AUTOCOMPLETE
-
-export type DiscriminatedInstrumentData<TKind extends InstrumentKind> = [TKind] extends ['FORM']
-  ? FormDataType
-  : [TKind] extends ['INTERACTIVE']
-    ? Json
-    : never;
-
-export type DiscriminatedInstrument<
-  TKind extends InstrumentKind,
-  TData extends DiscriminatedInstrumentData<TKind>,
-  TLanguage extends InstrumentLanguage,
-  TOptions extends { strict: boolean } = { strict: false }
-> = [TKind] extends ['FORM']
-  ? TData extends FormDataType
-    ? TOptions['strict'] extends true
-      ? StrictFormInstrument<TData, TLanguage>
-      : FormInstrument<TData, TLanguage>
-    : never
-  : [TKind] extends ['INTERACTIVE']
-    ? TData extends Json
-      ? TLanguage extends Language
-        ? InteractiveInstrument<TData, TLanguage>
-        : never
-      : never
-    : never;
-
-export type InstrumentDef<
-  TKind extends InstrumentKind,
-  TData extends DiscriminatedInstrumentData<TKind>,
-  TLanguage extends InstrumentLanguage
-> = Omit<DiscriminatedInstrument<TKind, TData, TLanguage, { strict: true }>, 'kind' | 'language' | 'validationSchema'>;
