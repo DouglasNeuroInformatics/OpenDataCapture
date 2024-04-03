d3bcefc9523001b99a7556a3f1893be35b7305d5
joshunrau <contact@joshuaunrau.com>
2024-04-03 12:36:20 -0400

    refactor: automate bundle input

--- runtime/v1/rollup.config.js
+++ runtime/v1/rollup.config.js
@@ -1,3 +1,6 @@
+// @ts-check
+
+import fs from 'fs/promises';
 import path from 'path';
 
 import commonjs from '@rollup/plugin-commonjs';
@@ -10,87 +13,96 @@ import { defineConfig } from 'rollup';
 import dts from 'rollup-plugin-dts';
 
 const OUT_DIR = path.resolve(import.meta.dirname, 'dist');
+const SRC_DIR = path.resolve(import.meta.dirname, 'src');
 const ROOT_DIR = path.resolve(import.meta.dirname, '..', '..');
 
 const MODE = /** @type {'development' | 'production'} */ ('development');
 
-export default defineConfig([
-  {
-    input: {
-      '@opendatacapture/instrument-renderer': path.resolve(
-        import.meta.dirname,
-        'src/@opendatacapture/instrument-renderer.ts'
-      ),
-      '@opendatacapture/instrument-transformer': path.resolve(
-        import.meta.dirname,
-        'src/@opendatacapture/instrument-transformer.ts'
-      ),
-      '_internal/bootstrap': path.resolve(import.meta.dirname, 'src', '_internal', 'bootstrap.ts'),
-      '_legacy/jquery-1.12.4': path.resolve(import.meta.dirname, 'src', '_legacy', 'jquery-1.12.4.js'),
-      core: path.resolve(import.meta.dirname, 'src', 'core.ts'),
-      jspsych: path.resolve(import.meta.dirname, 'src', 'jspsych.ts'),
-      react: path.resolve(import.meta.dirname, 'src', 'react.ts'),
-      'react-dom/client': path.resolve(import.meta.dirname, 'src', 'react-dom', 'client.ts'),
-      zod: path.resolve(import.meta.dirname, 'src', 'zod.ts')
-    },
-    output: {
-      dir: OUT_DIR,
-      format: 'es',
-      generatedCode: 'es2015',
-      interop: 'auto',
-      plugins: [terser()],
-      sourcemap: true
-    },
-    plugins: [
-      commonjs({
-        exclude: ['**/*/jquery.js']
-      }),
-      resolve({
-        browser: true,
-        extensions: ['.js', '.ts'],
-        rootDir: ROOT_DIR
-      }),
-      replace({
-        preventAssignment: false,
-        'process.env.NODE_ENV': `"${MODE}"`
-      }),
-      json(),
-      typescript()
-    ]
+/** @typedef {import('rollup').RollupOptions & { input: Record<string, string> }} RollupOptions */
+
+/** @type {RollupOptions} */
+const sourceOptions = {
+  input: {},
+  output: {
+    dir: OUT_DIR,
+    format: 'es',
+    generatedCode: 'es2015',
+    interop: 'auto',
+    plugins: MODE === 'production' ? [terser()] : [],
+    sourcemap: true
   },
-  {
-    input: {
-      '@opendatacapture/instrument-renderer': path.resolve(
-        import.meta.dirname,
-        'src/@opendatacapture/instrument-renderer.ts'
-      ),
-      '@opendatacapture/instrument-transformer': path.resolve(
-        import.meta.dirname,
-        'src/@opendatacapture/instrument-transformer.ts'
-      ),
-      '_legacy/jquery-1.12.4': path.resolve(import.meta.dirname, 'src', '_legacy', 'jquery-1.12.4.d.ts'),
-      core: path.resolve(import.meta.dirname, 'src', 'core.ts'),
-      jspsych: path.resolve(import.meta.dirname, 'src', 'jspsych.ts'),
-      react: path.resolve(import.meta.dirname, 'src', 'react.ts'),
-      'react-dom/client': path.resolve(import.meta.dirname, 'src', 'react-dom', 'client.ts'),
-      zod: path.resolve(import.meta.dirname, 'src', 'zod.ts')
-    },
-    output: [
-      {
-        dir: OUT_DIR,
-        format: 'es'
-      }
-    ],
-    plugins: [
-      resolve({
-        browser: true,
-        extensions: ['.js', '.ts'],
-        rootDir: ROOT_DIR
-      }),
-      json(),
-      dts({
-        respectExternal: true
-      })
-    ]
+  plugins: [
+    commonjs({
+      exclude: ['**/*/jquery.js']
+    }),
+    resolve({
+      browser: true,
+      extensions: ['.js', '.ts'],
+      rootDir: ROOT_DIR
+    }),
+    replace({
+      preventAssignment: false,
+      'process.env.NODE_ENV': `"${MODE}"`
+    }),
+    json(),
+    typescript()
+  ]
+};
+
+/** @type {RollupOptions} */
+const declarationOptions = {
+  input: {},
+  output: {
+    dir: OUT_DIR,
+    format: 'es'
+  },
+  plugins: [
+    resolve({
+      browser: true,
+      extensions: ['.js', '.ts'],
+      rootDir: ROOT_DIR
+    }),
+    json(),
+    dts({
+      respectExternal: true
+    })
+  ]
+};
+
+/** @type {(dir: string) => AsyncGenerator<string>} */
+async function* walk(dir) {
+  for await (const dirent of await fs.opendir(dir)) {
+    const entry = path.join(dir, dirent.name);
+    if (dirent.isDirectory()) {
+      yield* walk(entry);
+    } else if (dirent.isFile()) {
+      yield entry;
+    }
   }
-]);
+}
+
+/** @type {(filepath: string, suffixes: string[]) => string} */
+function createInputKey(filepath, suffixes) {
+  for (const suffix of suffixes) {
+    if (filepath.endsWith(suffix)) {
+      // SRC_DIR.length + 1 is to include the trailing slash
+      return filepath.slice(SRC_DIR.length + 1, filepath.length - suffix.length);
+    }
+  }
+  throw new Error(`Failed to match any target suffixes '${suffixes.join(', ')}' for target: ${filepath}`);
+}
+
+for await (const filepath of walk(SRC_DIR)) {
+  if (filepath.endsWith('.d.ts')) {
+    declarationOptions.input[createInputKey(filepath, ['.d.ts'])] = filepath;
+  } else if (filepath.endsWith('.ts') || filepath.endsWith('.tsx')) {
+    declarationOptions.input[createInputKey(filepath, ['.ts', '.tsx'])] = filepath;
+    sourceOptions.input[createInputKey(filepath, ['.ts', '.tsx'])] = filepath;
+  } else if (filepath.endsWith('.js') || filepath.endsWith('.jsx')) {
+    sourceOptions.input[createInputKey(filepath, ['.js', '.jsx'])] = filepath;
+  } else {
+    throw new Error(`Unexpected file extension '${path.extname(filepath)}' for file: ${filepath}`);
+  }
+}
+
+export default defineConfig([sourceOptions, declarationOptions]);
