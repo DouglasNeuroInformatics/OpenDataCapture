a658b44b40a3b28405ed13871e7876d9c9b6f812
joshunrau <contact@joshuaunrau.com>
2024-04-04 16:11:53 -0400

    fix: set partialBundledLanguages to true

--- apps/web/src/services/i18n.ts
+++ apps/web/src/services/i18n.ts
@@ -2,7 +2,8 @@ import { i18n } from '@opendatacapture/i18next';
 import Backend from 'i18next-http-backend';
 
 await i18n.use(Backend).init({
-  ns: ['auth', 'common', 'contact', 'instruments', 'layout', 'overview', 'setup', 'subjects', 'user', 'visits']
+  ns: ['auth', 'common', 'contact', 'instruments', 'layout', 'overview', 'setup', 'subjects', 'user', 'visits'],
+  partialBundledLanguages: true
 });
 
 export default i18n;
