0104ef38f19d7c926d2f9b54d5be2e5979cbc9bc
joshunrau <contact@joshuaunrau.com>
2024-04-09 12:00:03 -0400

    test: cleanup dead code

--- apps/web/cypress/support/commands.ts
+++ apps/web/cypress/support/commands.ts
@@ -1,18 +1,15 @@
-/// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+    interface Chainable {
+      addAVisit(firstName: string, lastName: string): Chainable<void>;
+      dragTo(prevSubject: string): Chainable<void>;
+      login(username: string, password: string): Chainable<void>;
+    }
+  }
+}
 
-//This is a login script for the
 Cypress.Commands.add('login', (username, password) => {
   cy.visit('/auth/login');
   cy.intercept('GET', '/src/assets/logo.png').as('finalElement');
@@ -55,38 +52,3 @@ Cypress.Commands.add('dragTo', { prevSubject: 'element' }, (subject, targetSelec
   cy.get(targetSelector).trigger('drop');
   cy.get(targetSelector).trigger('dragend');
 });
-
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace Cypress {
-    // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-    interface Chainable {
-      addAVisit(firstName: string, lastName: string): Chainable<void>;
-      dragTo(prevSubject: string): Chainable<void>;
-      login(username: string, password: string): Chainable<void>;
-    }
-  }
-}
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(username: string, password: string): Chainable<void>
-//     //   drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//     //   dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//     //   visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
