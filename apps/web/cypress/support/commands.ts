93af27aee627789f7343d4b9cf02ebfff8653cdb
joshunrau <contact@joshuaunrau.com>
2024-04-09 13:54:56 -0400

    test: refactor and fix commands

--- apps/web/cypress/support/commands.ts
+++ apps/web/cypress/support/commands.ts
@@ -3,7 +3,7 @@ declare global {
   namespace Cypress {
     // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
     interface Chainable {
-      addAVisit(firstName: string, lastName: string): Chainable<void>;
+      addVisit(firstName: string, lastName: string): Chainable<void>;
       dragTo(prevSubject: string): Chainable<void>;
       login(username: string, password: string): Chainable<void>;
     }
@@ -12,39 +12,21 @@ declare global {
 
 Cypress.Commands.add('login', (username, password) => {
   cy.visit('/auth/login');
-  cy.intercept('GET', '/src/assets/logo.png').as('finalElement');
-
-  //logo.png the last element acquired through GET request before loading full screen thus wait for it to be completed
-  //before entering login info
-  cy.wait('@finalElement');
   const form = cy.get('form[data-cy="login-form"]');
   form.get('input[name="username"]').type(username);
   form.get('input[name="password"]').type(password);
   form.get('button[data-cy="submit-form"]').click();
 });
 
-Cypress.Commands.add('addAVisit', (firstName, lastName) => {
+Cypress.Commands.add('addVisit', (firstName, lastName) => {
   cy.get('button[data-cy="add-visit"]').first().click();
   cy.url().should('include', '/visits/add-visit');
   cy.get('input[name=firstName]').type(firstName);
   cy.get('input[name=lastName]').type(lastName);
-
-  //activate a fill in DOB date reader
-  cy.get('input[data-cy="date-input"]').first().type('test');
-  cy.get('button[data-cy="year-select"]').click();
-  cy.get('button[data-cy="year-option"]').contains('1999').click();
-  cy.get('button[data-cy="day-option"]', { timeout: 10000 }).contains(14).should('be.visible');
-  cy.get('button[data-cy="day-option"]').contains('14').click();
-
-  cy.get('button[data-cy="form-list"]').first().click();
-  cy.get('li[data-cy="form-list-option"]').contains('Female').click();
-  //submit form
+  cy.get('input[name="dateOfBirth"]').first().type('1999-04-01');
+  cy.get('button[data-cy="sex-select-trigger"]').first().click();
+  cy.get('div[data-cy="sex-select-content"]').get('span').contains('Female').click();
   cy.get('button[data-cy="submit-form"]').click();
-
-  //navigate to view instrument page, select a form and confirm subject info is autofilled
-  cy.get('button[data-cy="view-instrument"]').click();
-
-  cy.url().should('include', '/instruments/available-instruments');
 });
 
 Cypress.Commands.add('dragTo', { prevSubject: 'element' }, (subject, targetSelector) => {
