06a3a892148979761269ef24a72c3a67b153dd9f
joshunrau <contact@joshuaunrau.com>
2024-04-11 09:50:38 -0400

    refactor: use ctx for context

--- packages/vite-plugin-plausible/src/index.test.js
+++ packages/vite-plugin-plausible/src/index.test.js
@@ -2,7 +2,7 @@ import { test } from 'vitest';
 
 import { plausible } from './index.js';
 
-test('plausible', (t) => {
+test('plausible', (ctx) => {
   const options = {
     baseUrl: 'https://example.com',
     dataDomain: 'example.com'
@@ -11,15 +11,15 @@ test('plausible', (t) => {
   /** @type {any} */
   const plugin = plausible(options);
 
-  t.expect(plugin.apply === 'build', 'Should have apply property set to "build"');
-  t.expect(plugin.name === 'vite-plugin-plausible', 'Should have name property set to "vite-plugin-plausible"');
+  ctx.expect(plugin.apply === 'build', 'Should have apply property set to "build"');
+  ctx.expect(plugin.name === 'vite-plugin-plausible', 'Should have name property set to "vite-plugin-plausible"');
 
   const transform = plugin.transformIndexHtml();
 
-  t.expect(transform.length === 1);
-  t.expect(transform[0].tag === 'script');
-  t.expect(transform[0].attrs['data-api']);
-  t.expect(transform[0].attrs['data-domain'] === 'example.com');
-  t.expect(transform[0].attrs.defer === true);
-  t.expect(transform[0].attrs.src === 'https://example.com/js/script.js');
+  ctx.expect(transform.length === 1);
+  ctx.expect(transform[0].tag === 'script');
+  ctx.expect(transform[0].attrs['data-api']);
+  ctx.expect(transform[0].attrs['data-domain'] === 'example.com');
+  ctx.expect(transform[0].attrs.defer === true);
+  ctx.expect(transform[0].attrs.src === 'https://example.com/js/script.js');
 });
