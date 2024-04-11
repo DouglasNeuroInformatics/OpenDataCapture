1b000007a50dba799a005aaab4c2af8178a04478
joshunrau <contact@joshuaunrau.com>
2024-04-11 00:07:04 -0400

    test: add tests to vite-plugin-plausible

---
+++ packages/vite-plugin-plausible/src/__tests__/plausible-plugin.test.js
@@ -0,0 +1,25 @@
+import { test } from 'vitest';
+
+import { plausiblePlugin } from '../plausible-plugin.js';
+
+test('plausiblePlugin', (t) => {
+  const options = {
+    baseUrl: 'https://example.com',
+    dataDomain: 'example.com'
+  };
+
+  /** @type {any} */
+  const plugin = plausiblePlugin(options);
+
+  t.expect(plugin.apply === 'build', 'Should have apply property set to "build"');
+  t.expect(plugin.name === 'vite-plugin-plausible', 'Should have name property set to "vite-plugin-plausible"');
+
+  const transform = plugin.transformIndexHtml();
+
+  t.expect(transform.length === 1);
+  t.expect(transform[0].tag === 'script');
+  t.expect(transform[0].attrs['data-api']);
+  t.expect(transform[0].attrs['data-domain'] === 'example.com');
+  t.expect(transform[0].attrs.defer === true);
+  t.expect(transform[0].attrs.src === 'https://example.com/js/script.js');
+});
---
+++ packages/vite-plugin-plausible/src/plausible-plugin.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * @param {object} options
+ * @param {string} options.baseUrl
+ * @param {string} options.dataDomain
+ * @returns {import('vite').PluginOption}
+ */
+export const plausiblePlugin = (options) => ({
+  apply: 'build',
+  name: 'vite-plugin-plausible',
+  transformIndexHtml: () => [
+    {
+      attrs: {
+        'data-api': options.baseUrl + '/api/event',
+        'data-domain': options.dataDomain,
+        defer: true,
+        src: options.baseUrl + '/js/script.js'
+      },
+      tag: 'script'
+    }
+  ]
+});
