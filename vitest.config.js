f57c7713b61c4dcb0638216fb4bdb8970b8a62b3
joshunrau <contact@joshuaunrau.com>
2024-04-11 02:20:33 -0400

    test: exclude config files and stories from coverage

--- vitest.config.js
+++ vitest.config.js
@@ -8,7 +8,18 @@ export default defineConfig({
       '/runtime/v1': path.resolve(import.meta.dirname, './runtime/v1/dist')
     },
     coverage: {
-      exclude: ['coverage/**', 'cypress/**', 'dist/**', '**/*.d.?(c|m)ts', '**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)'],
+      exclude: [
+        '**/.storybook/**',
+        '**/coverage/**',
+        '**/cypress/**',
+        '**/dist/**',
+        '**/public/**',
+        '**/scripts/**',
+        '**/*.d.?(c|m)ts',
+        '**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)',
+        '**/*.config.?(c|m)[jt]s?(x)',
+        '**/*.stories.?(c|m)[jt]s?(x)'
+      ],
       include: ['apps/**/*', 'packages/**/*'],
       provider: 'v8',
       reportsDirectory: path.resolve(import.meta.dirname, 'coverage'),
