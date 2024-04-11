cca463e58dbb49652f91c3bf076c87c691d744ef
joshunrau <contact@joshuaunrau.com>
2024-04-11 02:08:25 -0400

    refactor: remove manual declaration for stats

--- packages/stats/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function sum(arr: number[]): number;
-export declare function mean(arr: number[]): number;
-export declare function std(arr: number[], isPopulation?: boolean): number;
-export declare function linearRegression(arr: [number, number][]): {
-  intercept: number;
-  slope: number;
-  stdErr: number;
-};
