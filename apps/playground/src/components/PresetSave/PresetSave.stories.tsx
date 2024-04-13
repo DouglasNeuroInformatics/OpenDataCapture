7e52d4d57521378de946929b37e39015d10a440a
joshunrau <contact@joshuaunrau.com>
2024-04-13 09:56:14 -0400

    feat: add preset save

---
+++ apps/playground/src/components/PresetSave/PresetSave.stories.tsx
@@ -0,0 +1,9 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { PresetSave } from './PresetSave';
+
+type Story = StoryObj<typeof PresetSave>;
+
+export default { component: PresetSave } as Meta<typeof PresetSave>;
+
+export const Default: Story = {};
---
+++ apps/playground/src/components/PresetSave/PresetSave.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { Button, Dialog, Input, Label } from '@douglasneuroinformatics/libui/components';
+
+export const PresetSave = () => {
+  return (
+    <Dialog>
+      <Dialog.Trigger asChild>
+        <Button variant="secondary">Save</Button>
+      </Dialog.Trigger>
+      <Dialog.Content className="sm:max-w-[475px]">
+        <Dialog.Header>
+          <Dialog.Title>Save preset</Dialog.Title>
+          <Dialog.Description>
+            This will save the current playground state as a preset which you can access later or share with others.
+          </Dialog.Description>
+        </Dialog.Header>
+        <div className="grid gap-4 py-4">
+          <div className="grid gap-2">
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="description">Description</Label>
+            <Input id="description" />
+          </div>
+        </div>
+        <Dialog.Footer>
+          <Button type="submit">Save</Button>
+        </Dialog.Footer>
+      </Dialog.Content>
+    </Dialog>
+  );
+};
