18327d80e86f8b70fafc88e7d0c242d17e18abfe
joshunrau <contact@joshuaunrau.com>
2024-04-14 18:38:11 -0400

    fix: handle unknown case in instrument icon

--- packages/react-core/src/components/InstrumentIcon/InstrumentIcon.tsx
+++ packages/react-core/src/components/InstrumentIcon/InstrumentIcon.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import type { InstrumentKind } from '@opendatacapture/schemas/instrument';
-import { ClipboardCheckIcon, type LucideIcon, MonitorCheckIcon } from 'lucide-react';
+import { ClipboardCheckIcon, FileQuestionIcon, type LucideIcon, MonitorCheckIcon } from 'lucide-react';
 
 export type InstrumentIconProps = {
   kind: InstrumentKind;
@@ -13,6 +13,8 @@ export const InstrumentIcon = ({ kind, ...props }: InstrumentIconProps) => {
       return <ClipboardCheckIcon {...props} />;
     case 'INTERACTIVE':
       return <MonitorCheckIcon {...props} />;
+    case 'UNKNOWN':
+      return <FileQuestionIcon {...props} />;
     default:
       console.error(`ERROR: Unhandled instrument kind: ${kind}`);
       return null;
