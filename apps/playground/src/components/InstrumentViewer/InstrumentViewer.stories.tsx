2b566f72549341039d8c1bec666d3191a78ebf0a
joshunrau <contact@joshuaunrau.com>
2024-04-14 12:58:16 -0400

    chore: remove old instrument viewer

--- apps/playground/src/components/InstrumentViewer/InstrumentViewer.stories.tsx
+++
@@ -1,9 +0,0 @@
-import type { Meta, StoryObj } from '@storybook/react';
-
-import { InstrumentViewer } from './InstrumentViewer';
-
-type Story = StoryObj<typeof InstrumentViewer>;
-
-export default { component: InstrumentViewer } as Meta<typeof InstrumentViewer>;
-
-export const Default: Story = {};
