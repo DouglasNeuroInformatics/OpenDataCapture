685f224a6bbf3d38e5ec982fdbb97cc63d1df4ed
joshunrau <contact@joshuaunrau.com>
2024-04-14 11:56:09 -0400

    refactor: use sourceRef in DownloadButton

--- apps/playground/src/components/DownloadButton/DownloadButton.tsx
+++ apps/playground/src/components/DownloadButton/DownloadButton.tsx
@@ -4,21 +4,20 @@ import { Button } from '@douglasneuroinformatics/libui/components';
 import { useDownload } from '@douglasneuroinformatics/libui/hooks';
 import { DownloadIcon } from 'lucide-react';
 
-import { useEditorStore } from '@/store/editor.store';
+import { useSourceRef } from '@/hooks/useSourceRef';
 
 export const DownloadButton = () => {
   const download = useDownload();
-  const source = useEditorStore((store) => store.source);
+  const sourceRef = useSourceRef();
 
   return (
     <Button
       className="h-9 w-9"
-      disabled={!source}
       size="icon"
       type="button"
       variant="outline"
       onClick={() => {
-        void download('instrument.js', () => source);
+        void download('instrument.js', () => sourceRef.current);
       }}
     >
       <DownloadIcon />
