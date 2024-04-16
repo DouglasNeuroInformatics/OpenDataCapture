caef4e53a653882005cf28180adda8b7dec2f6a8
Joshua Unrau <contact@joshuaunrau.com>
2024-04-16 17:11:34 -0400

    fix: add public domain license

--- packages/licenses/src/index.ts
+++ packages/licenses/src/index.ts
@@ -6,13 +6,19 @@ type License = {
   reference: string;
 };
 
-type LicenseIdentifier = 'UNLICENSED' | Extract<keyof typeof _licenses, string>;
+type LicenseIdentifier = 'PUBLIC_DOMAIN' | 'UNLICENSED' | Extract<keyof typeof _licenses, string>;
 
 const licenses = new Map(Object.entries(_licenses)) as Map<LicenseIdentifier, License>;
 
 licenses.set('UNLICENSED', {
   isOpenSource: false,
   name: 'Unlicensed/Unknown',
+  reference: 'https://choosealicense.com'
+});
+
+licenses.set('PUBLIC_DOMAIN', {
+  isOpenSource: true,
+  name: 'Public Domain',
   reference: 'https://choosealicense.com'
 });
 
