e202ce51e508e9d37d7613df0336c4b89fae2889
joshunrau <contact@joshuaunrau.com>
2024-04-19 23:08:29 -0400

    feat: css!

--- apps/playground/src/hooks/useInstrumentBundler.ts
+++ apps/playground/src/hooks/useInstrumentBundler.ts
@@ -4,10 +4,31 @@ import { InstrumentBundler } from '@opendatacapture/instrument-bundler';
 import esbuild from 'esbuild-wasm';
 import esbuildWasmUrl from 'esbuild-wasm/esbuild.wasm?url';
 
+import { useEditorStore } from '@/store/editor.store';
+
 await esbuild.initialize({
   wasmURL: esbuildWasmUrl
 });
 
 export function useInstrumentBundler() {
-  return useMemo(() => new InstrumentBundler(), []);
+  return useMemo(() => {
+    return new InstrumentBundler({
+      resolve: (id) => {
+        const editorStore = useEditorStore.getState();
+        let filename: string;
+        if (id.startsWith('./')) {
+          filename = id.slice(2);
+        } else {
+          return null;
+        }
+        const file = editorStore.files.find((file) => file.name === filename);
+        if (!file) {
+          return null;
+        } else if (file.language !== 'css') {
+          throw new Error(`Unsupported language: ${file.language}`);
+        }
+        return `String.raw\`${file.value}\``;
+      }
+    });
+  }, []);
 }
