f2a213b328c1fcb983715d056514d691724ff7ed
joshunrau <contact@joshuaunrau.com>
2024-04-22 18:05:55 -0400

    fix: use flex layout

--- apps/playground/src/components/Editor/Editor.tsx
+++ apps/playground/src/components/Editor/Editor.tsx
@@ -16,7 +16,7 @@ export const Editor = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const openFiles = useEditorStore((store) => store.openFiles);
   return (
-    <div className="h-full w-full border border-r-0 bg-slate-50 dark:bg-slate-800">
+    <div className="flex h-full w-full flex-col border border-r-0 bg-slate-50 dark:bg-slate-800">
       <div className="flex w-full border-b">
         <EditorButton icon={<FilesIcon />} tip="View Files" onClick={() => setIsSidebarOpen(!isSidebarOpen)} />
         {openFiles.map((file) => (
