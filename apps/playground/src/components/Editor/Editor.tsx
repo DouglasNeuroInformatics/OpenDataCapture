23607c71b6d616e9257336786d4fa4334bc4dc24
joshunrau <contact@joshuaunrau.com>
2024-04-22 18:33:37 -0400

    refactor: move sidebar into editor

--- apps/playground/src/components/Editor/Editor.tsx
+++ apps/playground/src/components/Editor/Editor.tsx
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
 
+import { cn } from '@douglasneuroinformatics/libui/utils';
+import { motion } from 'framer-motion';
 import { Columns3Icon, FilePlusIcon } from 'lucide-react';
+import { useShallow } from 'zustand/react/shallow';
 
 import { useEditorStore } from '@/store/editor.store';
 
 import { EditorButton } from './EditorButton';
 import { EditorEmptyState } from './EditorEmptyState';
+import { EditorFileIcon } from './EditorFileIcon';
 import { EditorPane } from './EditorPane';
-import { EditorSidebar } from './EditorSidebar';
 import { EditorTab } from './EditorTab';
 
 import './setup';
 
 export const Editor = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const openFiles = useEditorStore((store) => store.openFiles);
+  const { files, openFiles, selectFile, selectedFile } = useEditorStore(
+    useShallow(({ files, openFiles, selectFile, selectedFile }) => ({ files, openFiles, selectFile, selectedFile }))
+  );
   return (
     <div className="flex h-full w-full flex-col border border-r-0 bg-slate-50 dark:bg-slate-800">
       <div className="flex w-full border-b">
@@ -31,7 +36,30 @@ export const Editor = () => {
         ))}
       </div>
       <div className="flex h-full">
-        <EditorSidebar isOpen={isSidebarOpen} />
+        <motion.div
+          animate={{ width: isSidebarOpen ? 320 : 0 }}
+          className="flex-shrink-0 overflow-hidden shadow-sm"
+          initial={{ width: 0 }}
+        >
+          <div className="h-full w-full border-r border-slate-900/10 dark:border-slate-100/25">
+            {files.map((file) => (
+              <button
+                className={cn(
+                  'flex w-full items-center gap-2 p-2 text-sm',
+                  selectedFile?.name === file.name && 'bg-slate-100 dark:bg-slate-700'
+                )}
+                key={file.name}
+                type="button"
+                onClick={() => selectFile(file)}
+              >
+                <EditorFileIcon
+                  variant={file.name.endsWith('.jsx') || file.name.endsWith('tsx') ? 'react' : file.language}
+                />
+                <span className="truncate">{file.name}</span>
+              </button>
+            ))}
+          </div>
+        </motion.div>
         {openFiles.length ? <EditorPane /> : <EditorEmptyState />}
       </div>
     </div>
