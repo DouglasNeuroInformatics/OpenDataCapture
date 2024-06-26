137bdf2bfa68b3fa590dc615be3ea97710db21c1
joshunrau <contact@joshuaunrau.com>
2024-06-25 20:22:07 -0400

    feat: add 404 page with client-side translations

--- apps/outreach/src/env.d.ts
+++ apps/outreach/src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+interface Window {
+  __TRANSLATIONS__?: import('./i18n').Translations;
+}
