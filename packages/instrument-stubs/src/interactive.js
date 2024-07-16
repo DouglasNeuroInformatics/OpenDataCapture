04dcc41322faf9887de32656c2871988bc161870
joshunrau <contact@joshuaunrau.com>
2024-07-16 14:15:22 -0400

    test: add maliciousInstrument stub

--- packages/instrument-stubs/src/interactive.js
+++ packages/instrument-stubs/src/interactive.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable perfectionist/sort-objects */
 
 import { createInstrumentStub } from './utils.js';
@@ -8,7 +9,6 @@ export const interactiveInstrument = await createInstrumentStub(async () => {
   return {
     kind: 'INTERACTIVE',
     language: 'en',
-
     tags: ['Example', 'Useless'],
     internal: {
       edition: 1,
@@ -40,6 +40,54 @@ export const interactiveInstrument = await createInstrumentStub(async () => {
       instructions: [],
       license: 'UNLICENSED',
       title: 'Interactive Instrument'
+    },
+    measures: {},
+    validationSchema: z.object({
+      message: z.string()
+    })
+  };
+});
+
+/** @type {import('./utils.js').InstrumentStub<import('@opendatacapture/schemas/instrument').InteractiveInstrument<{ message: string }>>} */
+export const maliciousInstrument = await createInstrumentStub(async () => {
+  const { z } = await import('zod');
+
+  /** @type {import('node:child_process')} */
+  const { exec } = await (0, eval)(`import('node:child_process')`);
+
+  exec('echo hello!', (err, stdout, stderr) => {
+    if (err) {
+      console.error(`exec error: ${err}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+
+  return {
+    kind: 'INTERACTIVE',
+    language: 'en',
+    tags: ['<PLACEHOLDER>'],
+    internal: {
+      edition: 1,
+      name: '<PLACEHOLDER>'
+    },
+    content: {
+      render(done) {
+        const button = document.createElement('button');
+        button.textContent = 'Submit Instrument';
+        document.body.appendChild(button);
+        button.addEventListener('click', () => {
+          done({ message: 'Hello World' });
+        });
+      }
+    },
+    details: {
+      description: '<PLACEHOLDER>',
+      estimatedDuration: 1,
+      instructions: ['<PLACEHOLDER>'],
+      license: 'UNLICENSED',
+      title: '<PLACEHOLDER>'
     },
     measures: {},
     validationSchema: z.object({
