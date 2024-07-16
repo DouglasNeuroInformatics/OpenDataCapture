af4cf331b0ebb81c9c972c95a2861d09a5c3c8cd
joshunrau <contact@joshuaunrau.com>
2024-07-16 14:10:14 -0400

    fix: remove StrongPassword regex

--- packages/schemas/src/user/user.ts
+++ packages/schemas/src/user/user.ts
@@ -3,11 +3,6 @@ import { z } from 'zod';
 import { $BaseModel } from '../core/core.js';
 import { $Group } from '../group/group.js';
 
-export const $StrongPassword = z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{12,}$/, {
-  message:
-    'Password must be string of 12 or more characters, with a minimum of one upper case letter, lowercase letter, and number'
-});
-
 export const $BasePermissionLevel = z.enum(['ADMIN', 'GROUP_MANAGER', 'STANDARD']);
 
 export type BasePermissionLevel = z.infer<typeof $BasePermissionLevel>;
@@ -24,18 +19,14 @@ export const $User = $BaseModel.extend({
 });
 
 export type CreateUserData = z.infer<typeof $CreateUserData>;
-export const $CreateUserData = $User
-  .pick({
-    basePermissionLevel: true,
-    firstName: true,
-    groupIds: true,
-    lastName: true,
-    password: true,
-    username: true
-  })
-  .extend({
-    password: $StrongPassword
-  });
+export const $CreateUserData = $User.pick({
+  basePermissionLevel: true,
+  firstName: true,
+  groupIds: true,
+  lastName: true,
+  password: true,
+  username: true
+});
 
 export type UpdateUserData = z.infer<typeof $UpdateUserData>;
 export const $UpdateUserData = $CreateUserData.partial();
