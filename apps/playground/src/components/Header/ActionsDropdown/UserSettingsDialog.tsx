b864a3b92bb71cbc1568922e059d2d8a23d6f4cc
joshunrau <contact@joshuaunrau.com>
2024-08-07 16:45:22 -0400

    feat: implement user credentials in settings form

--- apps/playground/src/components/Header/ActionsDropdown/UserSettingsDialog.tsx
+++ apps/playground/src/components/Header/ActionsDropdown/UserSettingsDialog.tsx
@@ -1,10 +1,42 @@
+/* eslint-disable perfectionist/sort-objects */
 import React from 'react';
 
 import { Dialog, Form } from '@douglasneuroinformatics/libui/components';
 import { useNotificationsStore } from '@douglasneuroinformatics/libui/hooks';
+import { z } from 'zod';
 
 import { $Settings } from '@/models/settings.model';
 import { useAppStore } from '@/store';
+
+const $SettingsFormData = $Settings
+  .omit({ credentials: true })
+  .extend({
+    password: z.string().optional(),
+    username: z.string().optional()
+  })
+  .superRefine((val, ctx) => {
+    if (!val.apiBaseUrl) {
+      return;
+    }
+    if (!val.username) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.invalid_type,
+        expected: 'string',
+        received: 'undefined',
+        path: ['username'],
+        message: 'This is a required field when "API Base URL" is set'
+      });
+    }
+    if (!val.password) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.invalid_type,
+        expected: 'string',
+        received: 'undefined',
+        path: ['password'],
+        message: 'This is a required field when "API Base URL" is set'
+      });
+    }
+  });
 
 export type UserSettingsDialogProps = {
   isOpen: boolean;
@@ -24,19 +56,76 @@ export const UserSettingsDialog = ({ isOpen, setIsOpen }: UserSettingsDialogProp
         </Dialog.Header>
         <Form
           className="py-4"
-          content={{
-            refreshInterval: {
-              description: 'The interval, in milliseconds, between builds, assuming the source code has changed',
-              kind: 'number',
-              label: 'Refresh Interval',
-              variant: 'input'
+          content={[
+            {
+              title: 'Instance Settings',
+              fields: {
+                apiBaseUrl: {
+                  description:
+                    "The base path for your Open Data Capture REST API. If you're not using the functionality to submit instruments to your instance, you can leave this empty.",
+                  kind: 'string',
+                  label: 'API Base URL',
+                  variant: 'input'
+                },
+                username: {
+                  kind: 'dynamic',
+                  deps: ['apiBaseUrl'],
+                  render({ apiBaseUrl }) {
+                    if (!apiBaseUrl) {
+                      return null;
+                    }
+                    return {
+                      kind: 'string',
+                      label: 'Username',
+                      variant: 'input'
+                    };
+                  }
+                },
+                password: {
+                  kind: 'dynamic',
+                  deps: ['apiBaseUrl'],
+                  render({ apiBaseUrl }) {
+                    if (!apiBaseUrl) {
+                      return null;
+                    }
+                    return {
+                      kind: 'string',
+                      label: 'Password',
+                      variant: 'password'
+                    };
+                  }
+                }
+              }
+            },
+            {
+              title: 'Editor Settings',
+              fields: {
+                refreshInterval: {
+                  description: 'The interval, in milliseconds, between builds, assuming the source code has changed',
+                  kind: 'number',
+                  label: 'Refresh Interval',
+                  variant: 'input'
+                }
+              }
             }
+          ]}
+          initialValues={{
+            ...settings,
+            ...settings.credentials
           }}
-          initialValues={settings}
           submitBtnLabel="Save Changes"
-          validationSchema={$Settings.partial()}
-          onSubmit={(updatedSettings) => {
-            updateSettings(updatedSettings);
+          validationSchema={$SettingsFormData}
+          onSubmit={({ password, username, ...updatedSettings }) => {
+            updateSettings({
+              ...updatedSettings,
+              credentials:
+                username && password
+                  ? {
+                      username,
+                      password
+                    }
+                  : undefined
+            });
             addNotification({ type: 'success' });
             setIsOpen(false);
           }}
--- apps/playground/src/models/settings.model.ts
+++ apps/playground/src/models/settings.model.ts
@@ -1,6 +1,10 @@
+import { $LoginCredentials } from '@opendatacapture/schemas/auth';
 import { z } from 'zod';
 
 export const $Settings = z.object({
+  apiBaseUrl: z.string().optional(),
+  credentials: $LoginCredentials.optional(),
   refreshInterval: z.number().positive().int()
 });
+
 export type Settings = z.infer<typeof $Settings>;
