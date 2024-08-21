00b46fcf43238a7aa17484994542069658b89548
joshunrau <contact@joshuaunrau.com>
2024-08-20 20:38:37 -0400

    refactor: add change language and theme events

--- packages/instrument-renderer/src/index.ts
+++ packages/instrument-renderer/src/index.ts
@@ -1,7 +1,12 @@
+import type { Theme } from '@douglasneuroinformatics/libui/hooks';
+import type { Json, Language } from '@opendatacapture/runtime-core';
+
 declare global {
   // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface GlobalEventHandlersEventMap {
-    done: CustomEvent;
+    changeLanguage: CustomEvent<Language>;
+    changeTheme: CustomEvent<Theme>;
+    done: CustomEvent<Json>;
   }
 }
 
