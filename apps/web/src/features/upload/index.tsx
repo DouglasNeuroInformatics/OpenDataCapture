950fe3aa7f0d19338517ebb3b339ee275472542f
David Roper <davidroper321@gmail.com>
2024-08-29 16:35:05 -0400

    feat: add files for upload pages

--- apps/web/src/features/upload/index.tsx
+++ apps/web/src/features/upload/index.tsx
@@ -3,10 +3,6 @@ import { type RouteObject } from 'react-router-dom';
 export const uploadRoute: RouteObject = {
   children: [
     {
-      index: true
-      // element: <SelectUploadPage />
-    },
-    {
       path: ':id' //instrument id
       // element: <UploadDataPage/>
     }
