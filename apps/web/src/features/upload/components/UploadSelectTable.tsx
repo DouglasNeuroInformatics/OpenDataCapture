58a6a62e024acccb1203c9c30a30067c786f657e
david-roper <davidroper321@gmail.com>
2024-08-30 17:06:05 -0400

    feat: add base empty clientTable

--- apps/web/src/features/upload/components/UploadSelectTable.tsx
+++ apps/web/src/features/upload/components/UploadSelectTable.tsx
@@ -1,5 +1,6 @@
 import { useInstrumentInfoQuery } from '@/hooks/useInstrumentInfoQuery';
 import type { InstrumentInfo } from '@opendatacapture/schemas/instrument';
+import { ClientTable } from '@douglasneuroinformatics/libui/components';
 
 export type UploadSelectTableProps = {
   data: InstrumentInfo[];
@@ -8,5 +9,15 @@ export type UploadSelectTableProps = {
 
 export const UploadSelectTable = ({ data, onSelect }: UploadSelectTableProps) => {
   const insturmentList = useInstrumentInfoQuery();
-  return <></>;
+  return (
+    <>
+      <ClientTable<InstrumentInfo>
+        columns={[]}
+        data={data}
+        entriesPerPage={15}
+        minRows={15}
+        onEntryClick={onSelect}
+      ></ClientTable>
+    </>
+  );
 };
