60276a0f80a6b709c90231c4a5b2236d5f8dadf0
David Roper <davidroper321@gmail.com>
2024-09-03 11:03:49 -0400

    feat: rename upload pages

--- apps/web/src/features/upload/pages/UploadSelectPage.tsx
+++ apps/web/src/features/upload/pages/UploadSelectPage.tsx
@@ -1,11 +1,13 @@
-import { LoadingFallback } from '@/components/LoadingFallback';
-import { PageHeader } from '@/components/PageHeader';
-import { Heading } from '@douglasneuroinformatics/libui/components';
-import { useTranslation } from 'react-i18next';
 import React from 'react';
 
-export const UploadPage = () => {
-  const { t } = useTranslation(['datahub', 'core']);
+import { Heading } from '@douglasneuroinformatics/libui/components';
+import { useTranslation } from 'react-i18next';
+
+import { LoadingFallback } from '@/components/LoadingFallback';
+import { PageHeader } from '@/components/PageHeader';
+
+export const UploadSelectPage = () => {
+  const { t } = useTranslation(['upload', 'core']);
 
   return (
     <React.Fragment>
