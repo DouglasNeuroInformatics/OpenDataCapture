d86c324290d9a06c22cef34d26a6ea9d781dbd3e
David Roper <davidroper321@gmail.com>
2024-09-03 11:03:15 -0400

    feat: make UploadSelectPage index page of upload route

--- apps/web/src/features/upload/index.tsx
+++ apps/web/src/features/upload/index.tsx
@@ -1,7 +1,13 @@
 import { type RouteObject } from 'react-router-dom';
 
+import { UploadSelectPage } from './pages/UploadSelectPage';
+
 export const uploadRoute: RouteObject = {
   children: [
+    {
+      element: <UploadSelectPage />,
+      index: true
+    },
     {
       path: ':id' //instrument id
       // element: <UploadDataPage/>
