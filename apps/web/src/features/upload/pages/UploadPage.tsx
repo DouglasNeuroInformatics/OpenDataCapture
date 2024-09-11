904d17f4642bd665d5db8754cc65ec75fca3e0bf
David Roper <davidroper321@gmail.com>
2024-09-11 16:54:27 -0400

    feat: create handletemplatedownload function

--- apps/web/src/features/upload/pages/UploadPage.tsx
+++ apps/web/src/features/upload/pages/UploadPage.tsx
@@ -6,14 +6,20 @@ import { DownloadIcon } from 'lucide-react';
 
 export const UploadPage = () => {
   const [file, setFile] = useState<File | null>(null);
+
+  const handleTemplateDownload = async () => {
+    if (!file) {
+      return;
+    }
+  };
+
   return (
     <div className="align-center items-center justify-center">
       <FileDropzone file={file} setFile={setFile} />
       <div className="mt-4 flex justify-between space-x-2">
         <Button variant={'primary'}>Submit</Button>
         <Button variant={'primary'}>
-          {' '}
-          <span>
+          <span className="p-1">
             <DownloadIcon />
           </span>
           Download Template
