3555bd31748cde705f083e32e03dd83a49d3e947
David Roper <davidroper321@gmail.com>
2024-09-11 16:28:32 -0400

    feat: add download template button

--- apps/web/src/features/upload/pages/UploadPage.tsx
+++ apps/web/src/features/upload/pages/UploadPage.tsx
@@ -2,13 +2,23 @@ import { useState } from 'react';
 
 import { FileDropzone } from '@douglasneuroinformatics/libui/components';
 import { Button } from '@douglasneuroinformatics/libui/components';
+import { DownloadIcon } from 'lucide-react';
 
 export const UploadPage = () => {
   const [file, setFile] = useState<File | null>(null);
   return (
-    <div className="align-center justify-center">
+    <div className="align-center items-center justify-center">
       <FileDropzone file={file} setFile={setFile} />
-      <Button>Submit</Button>
+      <div className="mt-4 flex justify-between space-x-2">
+        <Button variant={'primary'}>Submit</Button>
+        <Button variant={'primary'}>
+          {' '}
+          <span>
+            <DownloadIcon />
+          </span>
+          Download Template
+        </Button>
+      </div>
     </div>
   );
 };
