dc8091159f20982fc6e2aefc7bcb50e544e5cdab
David Roper <davidroper321@gmail.com>
2024-09-11 12:06:48 -0400

    feat: add button to upload page

--- apps/web/src/features/upload/pages/UploadPage.tsx
+++ apps/web/src/features/upload/pages/UploadPage.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 
 import { FileDropzone } from '@douglasneuroinformatics/libui/components';
+import { Button } from '@douglasneuroinformatics/libui/components';
 
 export const UploadPage = () => {
   const [file, setFile] = useState<File | null>(null);
   return (
-    <div>
+    <div className="align-center justify-center">
       <FileDropzone file={file} setFile={setFile} />
+      <Button>Submit</Button>
     </div>
   );
 };
