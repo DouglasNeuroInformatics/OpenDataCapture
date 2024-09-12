dc255aa1ae1c9b37c4cd6fb46e0690ce5235a6b6
David Roper <davidroper321@gmail.com>
2024-09-12 11:49:31 -0400

    feat: useInstrument function to get insturment info

--- apps/web/src/features/upload/pages/UploadPage.tsx
+++ apps/web/src/features/upload/pages/UploadPage.tsx
@@ -3,11 +3,29 @@ import { useState } from 'react';
 import { FileDropzone } from '@douglasneuroinformatics/libui/components';
 import { Button } from '@douglasneuroinformatics/libui/components';
 import { DownloadIcon } from 'lucide-react';
+import { useParams } from 'react-router-dom';
+import { useInstrumentInfoQuery } from '@/hooks/useInstrumentInfoQuery';
+import { useInstrument } from '@/hooks/useInstrument';
 
 export const UploadPage = () => {
   const [file, setFile] = useState<File | null>(null);
+  const [selectedInstrumentId, setSelectedInstrumentId] = useState<string | null>(null);
 
-  const handleTemplateDownload = async () => {
+  const params = useParams();
+
+  if (typeof params.id === 'string') {
+    setSelectedInstrumentId(params.id);
+  }
+
+  const instrument = useInstrument(selectedInstrumentId);
+
+  const handleTemplateDownload = () => {
+    console.log(instrument);
+    alert(params.id);
+    return;
+  };
+
+  const processInstrumentCSV = () => {
     if (!file) {
       return;
     }
@@ -18,10 +36,8 @@ export const UploadPage = () => {
       <FileDropzone file={file} setFile={setFile} />
       <div className="mt-4 flex justify-between space-x-2">
         <Button variant={'primary'}>Submit</Button>
-        <Button variant={'primary'}>
-          <span className="p-1">
-            <DownloadIcon />
-          </span>
+        <Button variant={'primary'} onClick={handleTemplateDownload}>
+          <DownloadIcon />
           Download Template
         </Button>
       </div>
