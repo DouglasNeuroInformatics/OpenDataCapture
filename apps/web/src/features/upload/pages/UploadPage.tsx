81cd79642084b7c453f1f6a3ecd5f4fccea9fe1d
David Roper <davidroper321@gmail.com>
2024-09-16 16:19:00 -0400

    feat: fix typing issues

--- apps/web/src/features/upload/pages/UploadPage.tsx
+++ apps/web/src/features/upload/pages/UploadPage.tsx
@@ -1,20 +1,24 @@
 import { useState } from 'react';
 
+import fs from 'fs';
+
 import { FileDropzone } from '@douglasneuroinformatics/libui/components';
 import { Button } from '@douglasneuroinformatics/libui/components';
 import { useDownload } from '@douglasneuroinformatics/libui/hooks';
-import { DownloadIcon } from 'lucide-react';
-import { useParams } from 'react-router-dom';
-import { useInstrument } from '@/hooks/useInstrument';
 import type { AnyUnilingualScalarInstrument } from '@opendatacapture/runtime-core';
+import { DownloadIcon } from 'lucide-react';
+import Papa from 'papaparse';
+import { useParams } from 'react-router-dom';
 import { z } from 'zod';
+
+import { useInstrument } from '@/hooks/useInstrument';
 
 export const UploadPage = () => {
   const [file, setFile] = useState<File | null>(null);
   const download = useDownload();
 
   const params = useParams();
-  const instrument = useInstrument(params.id as string) as AnyUnilingualScalarInstrument | null;
+  const instrument = useInstrument(params.id!) as AnyUnilingualScalarInstrument | null;
 
   const handleTemplateDownload = () => {
     //to do
@@ -28,13 +32,13 @@ export const UploadPage = () => {
 
     const instrumentSchema = instrument.validationSchema as z.AnyZodObject;
 
-    const columnNames = Object.keys(instrumentSchema.shape);
+    const columnNames = Object.keys(instrumentSchema.shape as z.AnyZodObject);
 
     const csvColumns = 'Mouse ID,Date,' + columnNames.join(',') + '\n';
 
     const fileName = instrument.details.title + ' template';
 
-    void download(`${fileName}.csv`, async () => {
+    void download(`${fileName}.csv`, () => {
       return csvColumns;
     });
 
@@ -50,6 +54,29 @@ export const UploadPage = () => {
     //make the first two columns of the template csv id and date
     //add the validation schema/content variables as column headers for csv
     //add the first line of the csv to be the data type (string, number, boolean, etc), also say if its optional or not
+  };
+
+  const csvToJson = (csvFilePath: string): Promise<any[]> => {
+    return new Promise((resolve, reject) => {
+      // Read the CSV file
+      fs.readFile(csvFilePath, 'utf8', (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+
+        // Parse the CSV data using PapaParse
+        Papa.parse(data, {
+          complete: (result) => {
+            resolve(result.data);
+          },
+          error: (parseError) => {
+            reject(parseError);
+          },
+          header: true, // Use the first row as headers
+          skipEmptyLines: true // Skip empty lines
+        });
+      });
+    });
   };
 
   return (
