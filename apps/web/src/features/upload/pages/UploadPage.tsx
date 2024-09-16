2d096421e4bb828db4d52e11a0f05453daa587f1
David Roper <davidroper321@gmail.com>
2024-09-16 15:51:12 -0400

    feat: create template csv with headers in handleTemplateDownload

--- apps/web/src/features/upload/pages/UploadPage.tsx
+++ apps/web/src/features/upload/pages/UploadPage.tsx
@@ -2,29 +2,41 @@ import { useState } from 'react';
 
 import { FileDropzone } from '@douglasneuroinformatics/libui/components';
 import { Button } from '@douglasneuroinformatics/libui/components';
+import { useDownload } from '@douglasneuroinformatics/libui/hooks';
 import { DownloadIcon } from 'lucide-react';
 import { useParams } from 'react-router-dom';
 import { useInstrument } from '@/hooks/useInstrument';
 import type { AnyUnilingualScalarInstrument } from '@opendatacapture/runtime-core';
+import { z } from 'zod';
 
 export const UploadPage = () => {
   const [file, setFile] = useState<File | null>(null);
+  const download = useDownload();
 
   const params = useParams();
   const instrument = useInstrument(params.id as string) as AnyUnilingualScalarInstrument | null;
 
   const handleTemplateDownload = () => {
-    if (!instrument) {
-      return;
-    }
-
-    const instrumentSchema = instrument.validationSchema;
-
     //to do
     //make sure the first two columns of the csv file are id and date
     //convert the given csv files as a json object
     //parse the json object through the instrument validation schema
     // if succcess full send the data to the backend to be stored
+    if (!instrument) {
+      return;
+    }
+
+    const instrumentSchema = instrument.validationSchema as z.AnyZodObject;
+
+    const columnNames = Object.keys(instrumentSchema.shape);
+
+    const csvColumns = 'Mouse ID,Date,' + columnNames.join(',') + '\n';
+
+    const fileName = instrument.details.title + ' template';
+
+    void download(`${fileName}.csv`, async () => {
+      return csvColumns;
+    });
 
     return;
   };
