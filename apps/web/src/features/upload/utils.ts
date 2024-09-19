7fe0842f6ddfa01d3e1a2274bda62fe29533b323
David Roper <davidroper321@gmail.com>
2024-09-19 16:16:52 -0400

    feat: create getZodTypeName function

---
+++ apps/web/src/features/upload/utils.ts
@@ -0,0 +1,14 @@
+import { isPlainObject } from '@douglasneuroinformatics/libjs';
+import type { z } from 'zod';
+
+const ZOD_TYPE_NAMES = ['ZodNumber', 'ZodString'] as const;
+
+type ZodTypeName = (typeof ZOD_TYPE_NAMES)[number];
+
+export function getZodTypeName(schema: z.ZodTypeAny): null | ZodTypeName {
+  const def: unknown = schema._def;
+  if (isPlainObject(def) && ZOD_TYPE_NAMES.includes(def.typeName as ZodTypeName)) {
+    return def.typeName as ZodTypeName;
+  }
+  return null;
+}
