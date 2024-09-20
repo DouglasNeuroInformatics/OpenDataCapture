f352ad0b61640cb9bdcbd893070bd91289b50463
david-roper <davidroper321@gmail.com>
2024-09-20 15:15:42 -0400

    feat: output inner type if its an optional field

--- apps/web/src/features/upload/utils.ts
+++ apps/web/src/features/upload/utils.ts
@@ -1,13 +1,17 @@
 import { isPlainObject } from '@douglasneuroinformatics/libjs';
 import type { z } from 'zod';
 
-const ZOD_TYPE_NAMES = ['ZodNumber', 'ZodString', 'ZodBoolean'] as const;
+const ZOD_TYPE_NAMES = ['ZodNumber', 'ZodString', 'ZodBoolean', 'ZodOptional', 'ZodSet'] as const;
 
 type ZodTypeName = (typeof ZOD_TYPE_NAMES)[number];
 
 export function getZodTypeName(schema: z.ZodTypeAny): null | ZodTypeName {
   const def: unknown = schema._def;
+
   if (isPlainObject(def) && ZOD_TYPE_NAMES.includes(def.typeName as ZodTypeName)) {
+    if (def.typeName === 'ZodOptional') {
+      return (def.innerType as z.AnyZodObject)._def.typeName as ZodTypeName;
+    }
     return def.typeName as ZodTypeName;
   }
   return null;
