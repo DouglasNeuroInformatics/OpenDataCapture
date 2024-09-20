8add704ee427fc9ce80f34ebb69b27a5a938787c
joshunrau <contact@joshuaunrau.com>
2024-09-20 12:00:26 -0400

    chore: use defineProject in api vitest

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -1,40 +1,43 @@
 import path from 'path';
 
 import swc from 'unplugin-swc';
-import { mergeConfig } from 'vitest/config';
+import { defineProject, mergeConfig } from 'vitest/config';
 
 import baseConfig from '../../vitest.config';
 
-export default mergeConfig(baseConfig, {
-  plugins: [
-    swc.vite({
-      jsc: {
-        baseUrl: path.resolve(import.meta.dirname, 'src'),
-        externalHelpers: true,
-        keepClassNames: true,
-        parser: {
-          decorators: true,
-          dynamicImport: true,
-          syntax: 'typescript'
+export default mergeConfig(
+  baseConfig,
+  defineProject({
+    plugins: [
+      swc.vite({
+        jsc: {
+          baseUrl: path.resolve(import.meta.dirname, 'src'),
+          externalHelpers: true,
+          keepClassNames: true,
+          parser: {
+            decorators: true,
+            dynamicImport: true,
+            syntax: 'typescript'
+          },
+          paths: {
+            '@/*': ['*']
+          },
+          target: 'es2022',
+          transform: {
+            decoratorMetadata: true,
+            legacyDecorator: true
+          }
         },
-        paths: {
-          '@/*': ['*']
+        minify: false,
+        module: {
+          type: 'es6'
         },
-        target: 'es2022',
-        transform: {
-          decoratorMetadata: true,
-          legacyDecorator: true
-        }
-      },
-      minify: false,
-      module: {
-        type: 'es6'
-      },
-      sourceMaps: true
-    })
-  ],
-  test: {
-    globals: true,
-    root: import.meta.dirname
-  }
-});
+        sourceMaps: true
+      }) as any
+    ],
+    test: {
+      globals: true,
+      root: import.meta.dirname
+    }
+  })
+);
