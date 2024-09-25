91e3917c96b9cfc1d108f2421b0c2cdfc1396fb8
David Roper <davidroper321@gmail.com>
2024-09-25 14:24:29 -0400

    refactor: change value interpreter

--- apps/web/src/features/upload/utils.ts
+++ apps/web/src/features/upload/utils.ts
@@ -1,18 +1,77 @@
-import { isPlainObject } from '@douglasneuroinformatics/libjs';
+import { isNumberLike, isPlainObject, parseNumber } from '@douglasneuroinformatics/libjs';
+import type { FormTypes } from '@opendatacapture/runtime-core';
 import type { z } from 'zod';
 
 const ZOD_TYPE_NAMES = ['ZodNumber', 'ZodString', 'ZodBoolean', 'ZodOptional', 'ZodSet'] as const;
 
 type ZodTypeName = (typeof ZOD_TYPE_NAMES)[number];
 
-export function getZodTypeName(schema: z.ZodTypeAny): null | ZodTypeName {
-  const def: unknown = schema._def;
+type RequiredZodTypeName = Exclude<ZodTypeName, 'ZodOptional'>;
 
+type ZodTypeNameResult =
+  | {
+      isOptional: boolean;
+      success: true;
+      typeName: RequiredZodTypeName;
+    }
+  | {
+      message: string;
+      success: false;
+    };
+
+type ValueInterpreterResult =
+  | {
+      message: string;
+      success: false;
+    }
+  | {
+      success: true;
+      value: FormTypes.FieldValue;
+    };
+
+export function getZodTypeName(schema: z.ZodTypeAny, isOptional?: boolean): ZodTypeNameResult {
+  const def: unknown = schema._def;
   if (isPlainObject(def) && ZOD_TYPE_NAMES.includes(def.typeName as ZodTypeName)) {
     if (def.typeName === 'ZodOptional') {
-      return (def.innerType as z.AnyZodObject)._def.typeName as ZodTypeName;
+      return getZodTypeName(def.innerType as z.ZodTypeAny, true);
     }
-    return def.typeName as ZodTypeName;
+    return { isOptional: Boolean(isOptional), success: true, typeName: def.typeName as RequiredZodTypeName };
   }
-  return null;
+  console.error(`Cannot parse ZodType from schema: ${JSON.stringify(schema)}`);
+  return { message: 'Unexpected Error', success: false };
+}
+
+export function valueInterpreter(
+  entry: string,
+  zType: Exclude<ZodTypeName, 'ZodOptional'>,
+  isOptional: boolean
+): ValueInterpreterResult {
+  if (entry === '' && isOptional) {
+    return { success: true, value: undefined };
+  }
+  switch (zType) {
+    case 'ZodBoolean':
+      if (entry.toLowerCase() === 'true') {
+        return { success: true, value: true };
+      } else if (entry.toLowerCase() === 'false') {
+        return { success: true, value: false };
+      }
+      return { message: '', success: false };
+    case 'ZodNumber':
+      if (isNumberLike(entry)) {
+        return { success: true, value: parseNumber(entry) };
+      }
+      return { message: '', success: false };
+    case 'ZodSet':
+      if (entry.includes('SET(')) {
+        let setData = entry.slice(4, -1);
+        let setDataList = setData.split('\\,');
+        return { success: true, value: new Set(setDataList) };
+      }
+      return { message: '', success: false };
+    case 'ZodString':
+      return { success: true, value: entry };
+    default:
+      return { message: `Invalid ZodType: ${zType satisfies never}`, success: false };
+  }
 }
