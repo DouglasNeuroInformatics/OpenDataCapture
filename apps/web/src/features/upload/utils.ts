7aa20d1730c86392179e13e5396fbefed7e8ad31
David Roper <davidroper321@gmail.com>
2024-09-25 17:06:52 -0400

    feat: move all functions from upload page to utils

--- apps/web/src/features/upload/utils.ts
+++ apps/web/src/features/upload/utils.ts
@@ -1,8 +1,12 @@
 import { isNumberLike, isPlainObject, parseNumber } from '@douglasneuroinformatics/libjs';
-import type { FormTypes } from '@opendatacapture/runtime-core';
-import type { z } from 'zod';
+import type { AnyUnilingualFormInstrument, FormTypes } from '@opendatacapture/runtime-core';
+import { z } from 'zod';
 
-const ZOD_TYPE_NAMES = ['ZodNumber', 'ZodString', 'ZodBoolean', 'ZodOptional', 'ZodSet'] as const;
+const ZOD_TYPE_NAMES = ['ZodNumber', 'ZodString', 'ZodBoolean', 'ZodOptional', 'ZodSet', 'ZodDate'] as const;
+const INTERNAL_HEADERS = ['subjectID', 'date'];
+const MONGOLIAN_VOWEL_SEPARATOR = String.fromCharCode(32, 6158);
+const INTERNAL_HEADERS_SAMPLE_DATA = [MONGOLIAN_VOWEL_SEPARATOR + 'string', MONGOLIAN_VOWEL_SEPARATOR + 'yyyy-mm-dd'];
+// csvColumns += 'mouseNumber-LabHead-ProjectLead,yyyy-mm-dd' + sampleData + '\n';
 
 type ZodTypeName = (typeof ZOD_TYPE_NAMES)[number];
 
@@ -19,14 +23,14 @@ type ZodTypeNameResult =
       success: false;
     };
 
-type ValueInterpreterResult =
+type UploadOperationResult<T> =
   | {
       message: string;
       success: false;
     }
   | {
       success: true;
-      value: FormTypes.FieldValue;
+      value: T;
     };
 
 export function getZodTypeName(schema: z.ZodTypeAny, isOptional?: boolean): ZodTypeNameResult {
@@ -45,7 +49,7 @@ export function valueInterpreter(
   entry: string,
   zType: Exclude<ZodTypeName, 'ZodOptional'>,
   isOptional: boolean
-): ValueInterpreterResult {
+): UploadOperationResult<FormTypes.FieldValue> {
   if (entry === '' && isOptional) {
     return { success: true, value: undefined };
   }
@@ -57,6 +61,13 @@ export function valueInterpreter(
         return { success: true, value: false };
       }
       return { message: '', success: false };
+    case 'ZodDate':
+      try {
+        return { success: true, value: new Date(entry) };
+      } catch (err) {
+        console.error(err);
+      }
+      return { message: `Failed to parse date: ${entry}`, success: false };
     case 'ZodNumber':
       if (isNumberLike(entry)) {
         return { success: true, value: parseNumber(entry) };
@@ -80,4 +91,135 @@ export function applyLineTransforms(line: string) {
   return line.replaceAll(/SET\((.*?)\)/g, (match) => {
     return match.replaceAll(',', '\\,');
   });
+}
+
+function formatTypeInfo(s: string, isOptional: boolean) {
+  return isOptional ? `${s} (optional)` : s;
+}
+
+function sampleDataGenerator({ isOptional, typeName }: Extract<ZodTypeNameResult, { success: true }>) {
+  switch (typeName) {
+    case 'ZodBoolean':
+      return formatTypeInfo('true/false', isOptional);
+    case 'ZodDate':
+      return formatTypeInfo('yyyy-mm-dd', isOptional);
+    case 'ZodNumber':
+      return formatTypeInfo('number', isOptional);
+    case 'ZodSet':
+      return formatTypeInfo('SET(a,b,c)', isOptional);
+    case 'ZodString':
+      return formatTypeInfo('string', isOptional);
+    default:
+      throw new Error(`Invalid zod schema: unexpected type name '${typeName satisfies never}'`);
+  }
+}
+
+export function createUploadTemplateCSV(instrument: AnyUnilingualFormInstrument) {
+  //to do
+  //make sure the first two columns of the csv file are id and date
+  //convert the given csv files as a json object
+  //parse the json object through the instrument validation schema
+  // if succcess full send the data to the backend to be stored
+
+  const instrumentSchema = instrument.validationSchema as z.AnyZodObject;
+  const shape = instrumentSchema.shape as { [key: string]: z.ZodTypeAny };
+
+  const columnNames = Object.keys(instrumentSchema.shape as z.AnyZodObject);
+
+  const csvColumns = INTERNAL_HEADERS.concat(columnNames);
+
+  const sampleData = [...INTERNAL_HEADERS_SAMPLE_DATA];
+  for (const col of columnNames) {
+    const typeNameResult = getZodTypeName(shape[col]!);
+    if (!typeNameResult.success) {
+      throw new Error(typeNameResult.message);
+    }
+    sampleData.push(sampleDataGenerator(typeNameResult));
+  }
+
+  return {
+    content: csvColumns.join(',') + '\n' + sampleData.join(',') + '\n',
+    fileName: `${instrument.internal.name}_${instrument.internal.edition}_template.csv`
+  };
+}
+
+export async function processInstrumentCSV(
+  input: File,
+  instrument: AnyUnilingualFormInstrument
+): Promise<UploadOperationResult<FormTypes.Data[]>> {
+  const instrumentSchema = instrument.validationSchema as z.AnyZodObject;
+  // const shape = instrumentSchema.shape as { [key: string]: z.ZodTypeAny };
+  const instrumentSchemaWithInternal = instrumentSchema.extend({
+    date: z.coerce.date(),
+    subjectID: z.string()
+  });
+  const shape = instrumentSchemaWithInternal.shape as { [key: string]: z.ZodTypeAny };
+
+  return new Promise<UploadOperationResult<FormTypes.Data[]>>((resolve) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const text = reader.result as string;
+      let [headerLine, ...dataLines] = text.split('\n');
+
+      //remove sample data if included
+      if (dataLines[0]?.includes(INTERNAL_HEADERS_SAMPLE_DATA.join(','))) {
+        dataLines = dataLines.slice(1);
+      }
+
+      if (dataLines.length === 0) {
+        return resolve({ message: 'data lines is empty array', success: false });
+      }
+
+      const result: FormTypes.Data[] = [];
+
+      const headers: string[] = headerLine!.split(',');
+
+      if (headers.length === 0) {
+        return resolve({ message: 'headers is empty array', success: false });
+      }
+
+      for (let line of dataLines) {
+        line = applyLineTransforms(line);
+        let elements = line.split(',');
+        const jsonLine: { [key: string]: unknown } = {};
+        for (let j = 0; j < headers.length; j++) {
+          const key = headers[j]!;
+          const rawValue = elements[j]!;
+
+          if (rawValue === '\n') {
+            continue;
+          }
+          if (shape[key] === undefined) {
+            return resolve({ message: `Schema at '${key}' is not defined!`, success: false });
+          }
+          const typeNameResult = getZodTypeName(shape[key]);
+          if (!typeNameResult.success) {
+            return resolve({ message: typeNameResult.message, success: false });
+          }
+          const valueInterpreterResult = valueInterpreter(rawValue, typeNameResult.typeName, typeNameResult.isOptional);
+          if (!valueInterpreterResult.success) {
+            return resolve({ message: valueInterpreterResult.message, success: false });
+          }
+          jsonLine[headers[j]!] = valueInterpreterResult.value;
+        }
+        const zodCheck = instrumentSchemaWithInternal.safeParse(jsonLine);
+        if (!zodCheck.success) {
+          //create error message with zodcheck error messsage + zodcheck error path
+          //addNotification({ message: zodCheck.error.issues[0]?.message, type: 'error' });
+          console.error(zodCheck.error.issues);
+          console.error(`Failed to parse data: ${JSON.stringify(jsonLine)}`);
+          return resolve({ message: zodCheck.error.message, success: false });
+        }
+        result.push(zodCheck.data);
+      }
+      resolve({ success: true, value: result });
+    };
+    reader.readAsText(input);
+  });
+
+  //to do
+  //take validation schema types and title to use as column titles
+  //make the first two columns of the template csv id and date
+  //add the validation schema/content variables as column headers for csv
+  //add the first line of the csv to be the data type (string, number, boolean, etc), also say if its optional or not
 }
