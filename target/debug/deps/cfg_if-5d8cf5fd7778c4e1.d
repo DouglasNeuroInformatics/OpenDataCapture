b504f199b8628b0b1f508ea11664fa5c47b8610a
David Roper <davidroper321@gmail.com>
2024-10-01 10:02:30 -0400

    fix: remove target folder

--- target/debug/deps/cfg_if-5d8cf5fd7778c4e1.d
+++
@@ -1,7 +0,0 @@
-/home/davidroper/projects/forks/OpenDataCapture/target/debug/deps/libcfg_if-5d8cf5fd7778c4e1.rmeta: /home/davidroper/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/home/davidroper/projects/forks/OpenDataCapture/target/debug/deps/libcfg_if-5d8cf5fd7778c4e1.rlib: /home/davidroper/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/home/davidroper/projects/forks/OpenDataCapture/target/debug/deps/cfg_if-5d8cf5fd7778c4e1.d: /home/davidroper/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/home/davidroper/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs:
--- target/debug/deps/neon_macros-e5fb3d2a33d062d6.d
+++
@@ -1,5 +0,0 @@
-/home/davidroper/projects/forks/OpenDataCapture/target/debug/deps/libneon_macros-e5fb3d2a33d062d6.so: /home/davidroper/.cargo/registry/src/index.crates.io-6f17d22bba15001f/neon-macros-1.0.0/src/lib.rs
-
-/home/davidroper/projects/forks/OpenDataCapture/target/debug/deps/neon_macros-e5fb3d2a33d062d6.d: /home/davidroper/.cargo/registry/src/index.crates.io-6f17d22bba15001f/neon-macros-1.0.0/src/lib.rs
-
-/home/davidroper/.cargo/registry/src/index.crates.io-6f17d22bba15001f/neon-macros-1.0.0/src/lib.rs:
