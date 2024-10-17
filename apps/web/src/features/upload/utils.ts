a2fb42723b03fe2b1d0690ded910dd27eb4d3179
David Roper <davidroper321@gmail.com>
2024-10-17 17:40:46 -0400

    chore: remove todo

--- apps/web/src/features/upload/utils.ts
+++ apps/web/src/features/upload/utils.ts
@@ -260,7 +260,7 @@ export function interpretZodObjectValue(
     for (let i = 0; i < record.length; i++) {
       // TODO - make sure this is defined
       const recordValue = record[i]!.split(':')[1]!;
-      // TODO - how do we know that `zList` is the same length as record? What if the user forgets to add a element
+
       const zListResult = zList[i]!;
       if (!(zListResult.success && zListResult.typeName !== 'ZodArray' && zListResult.typeName !== 'ZodObject')) {
         return { message: `Failed to interpret field '${i}'`, success: false };
