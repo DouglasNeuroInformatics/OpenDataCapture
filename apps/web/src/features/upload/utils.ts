487e13a0e1e578584d55d44dfc50879e9de59678
David Roper <davidroper321@gmail.com>
2024-10-24 12:04:22 -0400

    feat: fix logic to not throw error when set isnt Enum

--- apps/web/src/features/upload/utils.ts
+++ apps/web/src/features/upload/utils.ts
@@ -141,7 +141,7 @@ export function getZodTypeName(schema: z.ZodTypeAny, isOptional?: boolean): ZodT
     } else if (isZodArrayDef(def)) {
       return interpretZodArray(schema, def.typeName, isOptional);
     } else if (isZodSetDef(def)) {
-      const innerDef = def.valueType._def as AnyZodTypeDef;
+      const innerDef: unknown = def.valueType._def;
 
       if (!isZodTypeDef(innerDef)) {
         return {
@@ -150,19 +150,14 @@ export function getZodTypeName(schema: z.ZodTypeAny, isOptional?: boolean): ZodT
         };
       }
 
-      if (!isZodEnumDef(innerDef)) {
+      if (isZodEnumDef(innerDef)) {
         return {
-          message: 'Invalid inner type: ZodSet value type must be a ZodEnum',
-          success: false
+          enumValues: innerDef.values,
+          isOptional: Boolean(isOptional),
+          success: true,
+          typeName: def.typeName
         };
       }
-
-      return {
-        enumValues: innerDef.values,
-        isOptional: Boolean(isOptional),
-        success: true,
-        typeName: def.typeName
-      };
     }
 
     return {
