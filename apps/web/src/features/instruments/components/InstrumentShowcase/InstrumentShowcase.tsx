7f90a97bf49d831653d36d565c79384b7d9e307c
joshunrau <contact@joshuaunrau.com>
2025-01-03 12:59:16 -0500

    refactor: instrument showcase base

--- apps/web/src/features/instruments/components/InstrumentShowcase/InstrumentShowcase.tsx
+++ apps/web/src/features/instruments/components/InstrumentShowcase/InstrumentShowcase.tsx
@@ -1,112 +1,50 @@
 import { useEffect, useState } from 'react';
 
-import { ListboxDropdown, SearchBar } from '@douglasneuroinformatics/libui/components';
-import type { ListboxDropdownOption } from '@douglasneuroinformatics/libui/components';
-import { useTranslation } from '@douglasneuroinformatics/libui/hooks';
-import type { Language } from '@douglasneuroinformatics/libui/i18n';
-import type { UnilingualInstrumentInfo } from '@opendatacapture/schemas/instrument';
-import { motion } from 'framer-motion';
-import { useNavigate } from 'react-router-dom';
-
-import { useInstrumentInfoQuery } from '@/hooks/useInstrumentInfoQuery';
-import { useAppStore } from '@/store';
+import { SearchBar } from '@douglasneuroinformatics/libui/components';
+import type { TranslatedInstrumentInfo } from '@opendatacapture/schemas/instrument';
+import { AnimatePresence, motion } from 'motion/react';
 
 import { InstrumentCard } from '../InstrumentCard';
 
-export const InstrumentsShowcase = () => {
-  const currentGroup = useAppStore((store) => store.currentGroup);
-  const instrumentInfoQuery = useInstrumentInfoQuery();
-  const navigate = useNavigate();
-  const { t } = useTranslation();
-  const [filteredInstruments, setFilteredInstruments] = useState<
-    (UnilingualInstrumentInfo & { supportedLanguages: Language[] })[]
-  >([]);
-  const [tagOptions, setTagOptions] = useState<ListboxDropdownOption[]>([]);
-  const [selectedLanguages, setSelectedLanguages] = useState<ListboxDropdownOption[]>([]);
-  const [selectedTags, setSelectedTags] = useState<ListboxDropdownOption[]>([]);
+export const InstrumentShowcase: React.FC<{
+  data: TranslatedInstrumentInfo[];
+  onSelect: (instrument: TranslatedInstrumentInfo) => void;
+}> = ({ data: availableInstruments, onSelect }) => {
+  const [filteredInstruments, setFilteredInstruments] = useState<TranslatedInstrumentInfo[]>(availableInstruments);
   const [searchTerm, setSearchTerm] = useState('');
-
-  const languageOptions = [
-    {
-      key: 'en',
-      label: t('core.languages.english')
-    },
-    {
-      key: 'fr',
-      label: t('core.languages.french')
-    }
-  ];
 
   useEffect(() => {
     setFilteredInstruments(
-      (instrumentInfoQuery.data ?? []).filter((info) => {
-        if (currentGroup && !currentGroup?.accessibleInstrumentIds.includes(info.id)) {
-          return false;
-        }
-        const matchesSearch = info.details.title.toUpperCase().includes(searchTerm.toUpperCase());
-        const matchesLanguages =
-          selectedLanguages.length === 0 || selectedLanguages.find(({ key }) => key === info.language);
-        const matchesTags =
-          selectedTags.length === 0 || info.tags.some((tag) => selectedTags.find(({ key }) => key === tag));
-        return matchesSearch && matchesLanguages && matchesTags;
+      availableInstruments.filter((instrument) => {
+        const matchesSearch = instrument.details.title.toUpperCase().includes(searchTerm.toUpperCase());
+        return matchesSearch;
       })
     );
-  }, [currentGroup?.accessibleInstrumentIds, instrumentInfoQuery.data, searchTerm, selectedLanguages, selectedTags]);
-
-  useEffect(() => {
-    setTagOptions(
-      Array.from(new Set(filteredInstruments.flatMap((item) => item.tags))).map((item) => ({
-        key: item,
-        label: item
-      }))
-    );
-  }, [filteredInstruments]);
+  }, [availableInstruments, searchTerm]);
 
   return (
-    <div>
-      <div className="my-5 flex flex-col justify-between gap-5 lg:flex-row">
-        <SearchBar className="w-full flex-grow" value={searchTerm} onValueChange={setSearchTerm} />
-        <div className="flex min-w-96 flex-grow gap-2 lg:flex-shrink">
-          <div className="flex w-full" data-cy="tags-btn-dropdown">
-            <ListboxDropdown
-              widthFull
-              options={tagOptions}
-              selected={selectedTags}
-              setSelected={setSelectedTags}
-              title={t('core.tags')}
-            />
-          </div>
-          <div className="flex w-full" data-cy="language-btn-dropdown">
-            <ListboxDropdown
-              widthFull
-              options={languageOptions}
-              selected={selectedLanguages}
-              setSelected={setSelectedLanguages}
-              title={t('core.language')}
-            />
-          </div>
-        </div>
+    <div className="flex flex-col gap-5">
+      <div>
+        <SearchBar className="flex-grow" value={searchTerm} onValueChange={setSearchTerm} />
       </div>
-      <div className="relative grid grid-cols-1 gap-5">
-        {filteredInstruments.map((instrument, i) => {
-          return (
-            <motion.div
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: 80 }}
-              initial={{ opacity: 0, y: 80 }}
-              key={instrument.id}
-              transition={{ bounce: 0.2, delay: 0.15 * i, duration: 1.2, type: 'spring' }}
-            >
-              <InstrumentCard
-                instrument={instrument}
-                onClick={() => {
-                  navigate(`/instruments/render/${instrument.id}`, { state: { info: instrument } });
-                }}
-              />
-            </motion.div>
-          );
-        })}
-      </div>
+      <ul className="flex flex-col gap-5">
+        <AnimatePresence mode="popLayout">
+          {filteredInstruments.map((instrument, i) => {
+            return (
+              <motion.li
+                layout
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 80 }}
+                initial={{ opacity: 0, y: 80 }}
+                key={instrument.id}
+                transition={{ bounce: 0.2, delay: 0.15 * i, duration: 1.5, type: 'spring' }}
+              >
+                <InstrumentCard instrument={instrument} onClick={() => onSelect(instrument)} />
+              </motion.li>
+            );
+          })}
+        </AnimatePresence>
+      </ul>
     </div>
   );
 };
--- apps/web/src/features/instruments/pages/AccessibleInstrumentsPage.tsx
+++ apps/web/src/features/instruments/pages/AccessibleInstrumentsPage.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 
 import { Heading } from '@douglasneuroinformatics/libui/components';
 import { useTranslation } from '@douglasneuroinformatics/libui/hooks';
+import { useNavigate } from 'react-router-dom';
 
-import { LoadingFallback } from '@/components/LoadingFallback';
 import { PageHeader } from '@/components/PageHeader';
+import { WithFallback } from '@/components/WithFallback';
+import { useInstrumentInfoQuery } from '@/hooks/useInstrumentInfoQuery';
+import { useAppStore } from '@/store';
 
-import { InstrumentsShowcase } from '../components/InstrumentShowcase';
+import { InstrumentShowcase } from '../components/InstrumentShowcase';
 
 export const AccessibleInstrumentsPage = () => {
+  const currentGroup = useAppStore((store) => store.currentGroup);
+  const navigate = useNavigate();
   const { t } = useTranslation();
+  const instrumentInfoQuery = useInstrumentInfoQuery();
+
   return (
     <React.Fragment>
       <PageHeader>
@@ -17,9 +24,19 @@ export const AccessibleInstrumentsPage = () => {
           {t('instruments.accessible.title')}
         </Heading>
       </PageHeader>
-      <React.Suspense fallback={<LoadingFallback />}>
-        <InstrumentsShowcase />
-      </React.Suspense>
+      <WithFallback
+        Component={InstrumentShowcase}
+        props={{
+          data: currentGroup
+            ? instrumentInfoQuery.data?.filter((instrument) => {
+                return currentGroup.accessibleInstrumentIds.includes(instrument.id);
+              })
+            : instrumentInfoQuery.data,
+          onSelect: (instrument) => {
+            navigate(`/instruments/render/${instrument.id}`, { state: { info: instrument } });
+          }
+        }}
+      />
     </React.Fragment>
   );
 };
