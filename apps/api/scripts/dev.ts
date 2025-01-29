c5ca482bc01c1781079a36bbd598cf73496bd0d9
joshunrau <contact@joshuaunrau.com>
2025-01-29 15:04:00 -0500

    fix: incorrect type in nodemon

--- apps/api/scripts/dev.ts
+++ apps/api/scripts/dev.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env tsx
 
-import nodemon from 'nodemon';
+import _nodemon, { type Nodemon, type NodemonSettings } from 'nodemon';
+
+const nodemon = _nodemon as any as (settings: NodemonSettings) => Nodemon;
 
 import { clean, outfile, watch } from './build.js';
 
