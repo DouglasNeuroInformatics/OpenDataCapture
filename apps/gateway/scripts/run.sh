eb4ee6b3953339faaa46652add906062c47865ca
Thomas Beaudry <waveflux@gmail.com>
2025-05-13 11:36:02 -0400

    change 'file doesn't exist' to 'file doesn't exist or size is <= 0'

--- apps/gateway/scripts/run.sh
+++ apps/gateway/scripts/run.sh
@@ -5,7 +5,7 @@ set -eu
 DATABASE_FILE=${GATEWAY_DATABASE_URL#file:}
 EMPTY_DATABASE_FILE="/app/gateway.tmpl.db"
 
-if [ ! -f "$DATABASE_FILE" ]; then
+if [ ! -s "$DATABASE_FILE" ]; then
   cp "$EMPTY_DATABASE_FILE" "$DATABASE_FILE"
 fi
 
