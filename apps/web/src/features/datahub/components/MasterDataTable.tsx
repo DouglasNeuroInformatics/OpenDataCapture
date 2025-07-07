44a1d5a8eb7aad5ff04b6f59316634da3b7e9bc2
david-roper <davidroper321@gmail.com>
2025-07-07 12:02:57 -0400

    feat: display two different tables depending on speciesVisilbe group setting

--- apps/web/src/features/datahub/components/MasterDataTable.tsx
+++ apps/web/src/features/datahub/components/MasterDataTable.tsx
@@ -14,6 +14,48 @@ type MasterDataTableProps = {
 export const MasterDataTable = ({ data, onSelect }: MasterDataTableProps) => {
   const { t } = useTranslation();
   const subjectIdDisplaySetting = useAppStore((store) => store.currentGroup?.settings.subjectIdDisplayLength);
+  const speciesVisible = useAppStore((store) => store.currentGroup?.settings.speciesVisible);
+  if (speciesVisible) {
+    return (
+      <ClientTable<Subject>
+        columns={[
+          {
+            field: (subject) => removeSubjectIdScope(subject.id).slice(0, subjectIdDisplaySetting ?? 9),
+            label: t('datahub.index.table.subject')
+          },
+          {
+            field: (subject) => (subject.dateOfBirth ? toBasicISOString(new Date(subject.dateOfBirth)) : 'NULL'),
+            label: t('core.identificationData.dateOfBirth.label')
+          },
+          {
+            field: (subject) => {
+              switch (subject.sex) {
+                case 'FEMALE':
+                  return t('core.identificationData.sex.female');
+                case 'MALE':
+                  return t('core.identificationData.sex.male');
+                default:
+                  return 'NULL';
+              }
+            },
+            label: t('core.identificationData.sex.label')
+          },
+          {
+            field: (subject) => subject.species ?? 'NULL',
+            label: t({
+              en: 'Species',
+              fr: 'Espece animale'
+            })
+          }
+        ]}
+        data={data}
+        data-cy="master-data-table"
+        entriesPerPage={15}
+        minRows={15}
+        onEntryClick={onSelect}
+      />
+    );
+  }
   return (
     <ClientTable<Subject>
       columns={[
