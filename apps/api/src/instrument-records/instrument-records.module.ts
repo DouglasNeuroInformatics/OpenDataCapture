c2b6c4101aa0c817f26fbfb6015e161867b2b348
David Roper <davidroper321@gmail.com>
2025-07-22 11:07:11 -0400

    feat: refactor instrument record  module and service to avoid circular dependency

--- apps/api/src/instrument-records/instrument-records.module.ts
+++ apps/api/src/instrument-records/instrument-records.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 
 import { GroupsModule } from '@/groups/groups.module';
 import { InstrumentsModule } from '@/instruments/instruments.module';
@@ -12,7 +12,7 @@ import { InstrumentRecordsService } from './instrument-records.service';
 @Module({
   controllers: [InstrumentRecordsController],
   exports: [InstrumentRecordsService],
-  imports: [GroupsModule, InstrumentsModule, SessionsModule, SubjectsModule],
+  imports: [GroupsModule, InstrumentsModule, SessionsModule, forwardRef(() => SubjectsModule)],
   providers: [InstrumentMeasuresService, InstrumentRecordsService]
 })
 export class InstrumentRecordsModule {}
