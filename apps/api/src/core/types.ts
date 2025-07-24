e3308225b450e6541a1559c6721559e5c0099f3e
David Roper <davidroper321@gmail.com>
2025-07-24 11:27:17 -0400

    fix: removed unused identity type

--- apps/api/src/core/types.ts
+++ apps/api/src/core/types.ts
@@ -3,7 +3,3 @@ import type { AppAbility } from '@douglasneuroinformatics/libnest';
 export type EntityOperationOptions = {
   ability?: AppAbility;
 };
-
-//This indentity type is used as a quick fix to resolve the swc compiling issue with fowardRef nestjs services
-// https://docs.nestjs.com/recipes/swc#common-pitfalls
-export type Identity<T> = T;
