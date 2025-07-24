23f5666140c39f6271301e0c03d2c95b1a655d6a
David Roper <davidroper321@gmail.com>
2025-07-24 11:30:43 -0400

    feat: unused foward ref of instrument service

--- apps/api/src/subjects/subjects.module.ts
+++ apps/api/src/subjects/subjects.module.ts
@@ -1,6 +1,4 @@
-import { forwardRef, Module } from '@nestjs/common';
-
-import { InstrumentRecordsModule } from '@/instrument-records/instrument-records.module';
+import { Module } from '@nestjs/common';
 
 import { SubjectsController } from './subjects.controller';
 import { SubjectsService } from './subjects.service';
@@ -8,7 +6,6 @@ import { SubjectsService } from './subjects.service';
 @Module({
   controllers: [SubjectsController],
   exports: [SubjectsService],
-  imports: [forwardRef(() => InstrumentRecordsModule)],
   providers: [SubjectsService]
 })
 export class SubjectsModule {}
