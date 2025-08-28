f5679f020385111602e3457e82bb590663ec090f
joshunrau <contact@joshuaunrau.com>
2025-08-28 16:36:42 -0400

    chore: update

--- apps/web/src/utils/upload2.ts
+++ apps/web/src/utils/upload2.ts
@@ -1,4 +1,5 @@
-import { isObjectLike, isZodType } from '@douglasneuroinformatics/libjs';
+/* eslint-disable @typescript-eslint/no-namespace */
+import { isObjectLike, isPlainObject, isZodType } from '@douglasneuroinformatics/libjs';
 import type { Language } from '@douglasneuroinformatics/libui/i18n';
 import type { AnyUnilingualFormInstrument, FormTypes } from '@opendatacapture/runtime-core';
 import { unparse } from 'papaparse';
@@ -10,6 +11,24 @@ const INTERNAL_HEADERS = ['subjectID', 'date'];
 const MONGOLIAN_VOWEL_SEPARATOR = String.fromCharCode(32, 6158);
 
 const INTERNAL_HEADERS_SAMPLE_DATA = [MONGOLIAN_VOWEL_SEPARATOR + 'string', MONGOLIAN_VOWEL_SEPARATOR + 'yyyy-mm-dd'];
+
+const ZOD_TYPE_NAMES = [
+  'ZodNumber',
+  'ZodString',
+  'ZodBoolean',
+  'ZodOptional',
+  'ZodSet',
+  'ZodDate',
+  'ZodEnum',
+  'ZodArray',
+  'ZodObject',
+  'ZodEffects'
+] as const;
+
+type CSVUploadTemplate = {
+  content: string;
+  filename: string;
+};
 
 class UploadError extends Error {
   description: {
@@ -27,37 +46,104 @@ class UploadError extends Error {
   }
 }
 
-type CSVUploadTemplate = {
-  content: string;
-  filename: string;
-};
-
-class Zod3TypeGuards {
-  static isZodEffectsObject(value: unknown): value is z3.ZodEffects<z3.AnyZodObject> {
-    return this.isZodEffects(value) && this.isZodObject(value.innerType());
-  }
-  static isZodObject(value: unknown): value is z3.AnyZodObject {
-    return isObjectLike(value) && value.constructor.name === 'ZodObject';
-  }
-  private static isZodEffects(value: unknown): value is z3.ZodEffects<z3.ZodType> {
-    return isObjectLike(value) && value.constructor.name === 'ZodEffects';
-  }
+function getTemplateFilename(instrumentInternal: AnyUnilingualFormInstrument['internal']) {
+  return `${instrumentInternal.name}_${instrumentInternal.edition}_template.csv`;
 }
 
-class CSVUploadTemplateBuilder {
-  static build(instrument: AnyUnilingualFormInstrument): CSVUploadTemplate {
-    const instrumentSchema = instrument.validationSchema;
-    if (isZodType(instrumentSchema, { version: 4 })) {
-      return this.forZod4(instrumentSchema, instrument.internal);
-    }
-    return this.forZod3(instrumentSchema, instrument.internal);
+namespace Zod3 {
+  type ZodTypeName = Extract<`${z3.ZodFirstPartyTypeKind}`, (typeof ZOD_TYPE_NAMES)[number]>;
+
+  type RequiredZodTypeName = Exclude<ZodTypeName, 'ZodEffects' | 'ZodOptional'>;
+
+  type AnyZodTypeDef = z3.ZodTypeDef & { typeName: ZodTypeName };
+
+  type ZodObjectArrayDef = z3.ZodArrayDef & { type: z3.AnyZodObject };
+
+  function isZodEffects(value: unknown): value is z3.ZodEffects<z3.ZodType> {
+    return isObjectLike(value) && value.constructor.name === 'ZodEffects';
   }
 
-  private static forZod3(
+  function isZodEffectsObject(value: unknown): value is z3.ZodEffects<z3.AnyZodObject> {
+    return isZodEffects(value) && isZodObject(value.innerType());
+  }
+
+  function isZodObject(value: unknown): value is z3.AnyZodObject {
+    return isObjectLike(value) && value.constructor.name === 'ZodObject';
+  }
+
+  function isZodTypeDef(value: unknown): value is AnyZodTypeDef {
+    return isPlainObject(value) && ZOD_TYPE_NAMES.includes(value.typeName as ZodTypeName);
+  }
+
+  function isZodOptionalDef(def: AnyZodTypeDef): def is z3.ZodOptionalDef<z3.ZodTypeAny> {
+    return def.typeName === z3.ZodFirstPartyTypeKind.ZodOptional;
+  }
+
+  function isZodEnumDef(def: AnyZodTypeDef): def is z3.ZodEnumDef {
+    return def.typeName === z3.ZodFirstPartyTypeKind.ZodEnum;
+  }
+
+  function isZodSetDef(def: AnyZodTypeDef): def is z3.ZodSetDef {
+    return def.typeName === z3.ZodFirstPartyTypeKind.ZodSet;
+  }
+
+  function isZodArrayDef(def: AnyZodTypeDef): def is z3.ZodArrayDef {
+    return def.typeName === z3.ZodFirstPartyTypeKind.ZodArray;
+  }
+
+  function isZodObjectArrayDef(def: AnyZodTypeDef): def is ZodObjectArrayDef {
+    return isZodArrayDef(def) && isZodObject(def.type);
+  }
+
+  function interpetZodArray(..._args: any[]) {
+    return null as unknown;
+  }
+
+  function getZodTypeName(schema: z3.ZodTypeAny, isOptional?: boolean): unknown {
+    const def: unknown = schema._def;
+    if (!isZodTypeDef(def)) {
+      console.error(`Cannot parse ZodType from schema: ${JSON.stringify(schema)}`);
+      throw new UploadError({ en: 'Unexpected Error', fr: 'Erreur inattendue' });
+    } else if (isZodOptionalDef(def)) {
+      return getZodTypeName(def.innerType, true);
+    } else if (isZodEnumDef(def)) {
+      return {
+        enumValues: def.values,
+        isOptional: Boolean(isOptional),
+        success: true,
+        typeName: def.typeName
+      };
+    } else if (isZodObjectArrayDef(def)) {
+      return interpetZodArray(schema, def.typeName, isOptional);
+    } else if (isZodSetDef(def)) {
+      const innerDef: unknown = def.valueType._def;
+      if (!isZodTypeDef(innerDef)) {
+        throw new UploadError({
+          en: 'Invalid inner type: ZodSet value type must have a valid type definition',
+          fr: 'Type interne invalide : le type de valeur ZodSet doit avoir une définition de type valide'
+        });
+      }
+      if (isZodEnumDef(innerDef)) {
+        return {
+          enumValues: innerDef.values,
+          isOptional: Boolean(isOptional),
+          success: true,
+          typeName: def.typeName
+        };
+      }
+    }
+    return {
+      isOptional: Boolean(isOptional),
+      success: true,
+      typeName: def.typeName satisfies ZodTypeName as RequiredZodTypeName
+    };
+  }
+
+  export function createUploadTemplateCSV(
     instrumentSchema: z3.ZodType<FormTypes.Data>,
     instrumentInternal: AnyUnilingualFormInstrument['internal']
   ): CSVUploadTemplate {
-    if (!(Zod3TypeGuards.isZodObject(instrumentSchema) || Zod3TypeGuards.isZodEffectsObject(instrumentSchema))) {
+    if (!(isZodObject(instrumentSchema) || isZodEffectsObject(instrumentSchema))) {
       throw new UploadError({
         en: 'Validation schema for this instrument is invalid',
         fr: 'Le schéma de validation de cet instrument est invalide'
@@ -73,10 +159,7 @@ class CSVUploadTemplateBuilder {
     const sampleData = [...INTERNAL_HEADERS_SAMPLE_DATA];
 
     for (const col of columnNames) {
-      const typeNameResult = getZodTypeName(shape[col]);
-      if (!typeNameResult.success) {
-        throw new Error(`${typeNameResult.message.en} / ${typeNameResult.message.fr}`);
-      }
+      const typeNameResult = getZodTypeName(shape[col]!);
       sampleData.push(generateSampleData(typeNameResult));
     }
 
@@ -84,14 +167,16 @@ class CSVUploadTemplateBuilder {
 
     return {
       content: unparse([csvColumns, sampleData]),
-      filename: this.getFilename(instrumentInternal)
+      filename: getTemplateFilename(instrumentInternal)
     };
   }
+}
 
-  private static forZod4(
+namespace Zod4 {
+  export function createUploadTemplateCSV(
     instrumentSchema: z4.ZodType<FormTypes.Data>,
     instrumentInternal: AnyUnilingualFormInstrument['internal']
-  ): CSVUploadTemplate {
+  ) {
     const jsonInstrumentSchema = z4.toJSONSchema(instrumentSchema) as z4.core.JSONSchema.Schema;
     if (jsonInstrumentSchema.type !== 'object') {
       throw new UploadError({
@@ -101,11 +186,15 @@ class CSVUploadTemplateBuilder {
     const zod4TemplateData = ''; // zod4Helper(jsonInstrumentSchema);
     return {
       content: zod4TemplateData,
-      filename: this.getFilename(instrumentInternal)
+      filename: getTemplateFilename(instrumentInternal)
     };
   }
+}
 
-  private static getFilename(instrumentInternal: AnyUnilingualFormInstrument['internal']) {
-    return `${instrumentInternal.name}_${instrumentInternal.edition}_template.csv`;
+export function createUploadTemplateCSV(instrument: AnyUnilingualFormInstrument) {
+  const instrumentSchema = instrument.validationSchema;
+  if (isZodType(instrumentSchema, { version: 4 })) {
+    return Zod4.createUploadTemplateCSV(instrumentSchema, instrument.internal);
   }
+  return Zod3.createUploadTemplateCSV(instrumentSchema, instrument.internal);
 }
