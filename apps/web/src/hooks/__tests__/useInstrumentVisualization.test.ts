8c753ff95ea0165525ef47f45139db01c11544ee
David Roper <davidroper321@gmail.com>
2025-10-15 14:39:13 -0400

    test: add mocks for all hooks and react components

--- apps/web/src/hooks/__tests__/useInstrumentVisualization.test.ts
+++ apps/web/src/hooks/__tests__/useInstrumentVisualization.test.ts
@@ -1,17 +1,82 @@
-// import { MockFactory } from '@douglasneuroinformatics/libnest/testing';
-// import type { MockedInstance } from '@douglasneuroinformatics/libnest/testing';
-import { describe, it, vi, expect, beforeEach } from 'vitest';
+import React from 'react';
+
+import { act } from '@testing-library/react';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+
 import { useInstrumentVisualization } from '../useInstrumentVisualization';
 
+const mockInstrument = {
+  useInstrument: vi.fn()
+};
+
+vi.mock('@/hooks/useInstrument', () => ({
+  useInstrument: () => mockInstrument
+}));
+
+const mockStore = {
+  useAppStore: vi.fn()
+};
+const mockDownload = {
+  useDownload: vi.fn()
+};
+const mockNotification = {
+  useNotificationsStore: vi.fn()
+};
+const mockTranslation = {
+  useTranslation: vi.fn()
+};
+
+const mockInfoQuery = {
+  useInstrumentInfoQuery: vi.fn()
+};
+
+const mockInstrumentRecords = {
+  useInstrumentRecords: vi.fn()
+};
+
+vi.mock('@/store', () => ({
+  useAppStore: () => mockStore
+}));
+
+vi.mock('@douglasneuroinformatics/libui/hooks', () => ({
+  useDownload: () => mockDownload,
+  useNotificationsStore: () => mockNotification,
+  useTranslation: () => mockTranslation
+}));
+
+vi.mock('react', async (importOriginal) => {
+  const actual = await importOriginal<typeof React>();
+  return {
+    ...actual,
+    useEffect: vi.fn(),
+    useMemo: vi.fn(),
+    useState: vi.fn(() => ['mockedRecords', vi.fn()])
+  };
+});
+
+vi.mock('@/hooks/useInstrumentInfoQuery', () => ({
+  useInstrumentInfoQuery: () => mockInfoQuery
+}));
+
+vi.mock('@/hooks/useInstrumentRecords', () => ({
+  useInstrumentRecords: () => mockInstrumentRecords
+}));
+
 describe('useInstrumentVisualization tests', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
   describe('CSV', () => {
     it('Should download', () => {
+      vi.spyOn(React, 'useEffect').mockImplementation((fn) => fn());
       const { dl, records } = useInstrumentVisualization({
         params: { subjectId: 'testId' }
       });
+      act(() => dl('CSV'));
+      expect(mockInstrument.useInstrument).toHaveBeenCalledOnce();
+      expect(mockStore.useAppStore).toHaveBeenCalled();
       expect(records).toBeDefined();
-      console.log(records);
-      dl('CSV');
     });
   });
 });
