712632863df8334e75733a568ccfff08e3b81833
David Roper <davidroper321@gmail.com>
2025-10-15 12:20:54 -0400

    chore: update vitest config

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,8 +1,15 @@
+import * as path from 'node:path';
+
 import { mergeConfig } from 'vitest/config';
 
 import baseConfig from '../../vitest.config';
 
 export default mergeConfig(baseConfig, {
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, 'src')
+    }
+  },
   test: {
     environment: 'happy-dom',
     root: import.meta.dirname
