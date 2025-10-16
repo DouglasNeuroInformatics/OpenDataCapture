b607bd8ba729841110065f71b5e0e2825bb5a85b
David Roper <davidroper321@gmail.com>
2025-10-16 10:36:08 -0400

    test: create tests to confirm contents of downloads

--- apps/web/src/hooks/__tests__/useInstrumentVisualization.test.ts
+++ apps/web/src/hooks/__tests__/useInstrumentVisualization.test.ts
@@ -4,7 +4,6 @@ import { act } from '@testing-library/react';
 import { afterEach, describe, expect, it, vi } from 'vitest';
 
 import { useInstrumentVisualization } from '../useInstrumentVisualization';
-import { toBasicISOString } from '@douglasneuroinformatics/libjs';
 
 const mockUseInstrument = vi.hoisted(() =>
   vi.fn(() => ({
@@ -105,6 +104,71 @@ describe('useInstrumentVisualization tests', () => {
       expect(filename).toContain('.csv');
       const csvContents = getContentFn();
       expect(csvContents).toMatch('subjectId,Date,someValue\r\ntestId,2025-04-30,abc');
+    });
+  });
+  describe('TSV', () => {
+    it('Should download', () => {
+      vi.spyOn(React, 'useEffect').mockImplementation((fn) => fn());
+      const { dl, records } = useInstrumentVisualization({
+        params: { subjectId: 'testId' }
+      });
+      act(() => dl('TSV'));
+      expect(records).toBeDefined();
+      expect(mockUseDownload).toHaveBeenCalledTimes(1);
+
+      const [filename, getContentFn] = mockUseDownload.mock.calls[0];
+      expect(filename).toContain('.tsv');
+      const tsvContents = getContentFn();
+      expect(tsvContents).toMatch('subjectId\tDate\tsomeValue\r\ntestId\t2025-04-30\tabc');
+    });
+  });
+  describe('CSV Long', () => {
+    it('Should download', () => {
+      vi.spyOn(React, 'useEffect').mockImplementation((fn) => fn());
+      const { dl, records } = useInstrumentVisualization({
+        params: { subjectId: 'testId' }
+      });
+      act(() => dl('CSV Long'));
+      expect(records).toBeDefined();
+      expect(mockUseDownload).toHaveBeenCalledTimes(1);
+
+      const [filename, getContentFn] = mockUseDownload.mock.calls[0];
+      expect(filename).toContain('.csv');
+      const csvLongContents = getContentFn();
+      expect(csvLongContents).toMatch('Date,SubjectID,Value,Variable\r\n2025-04-30,testId,abc,someValue');
+    });
+  });
+  describe('TSV Long', () => {
+    it('Should download', () => {
+      vi.spyOn(React, 'useEffect').mockImplementation((fn) => fn());
+      const { dl, records } = useInstrumentVisualization({
+        params: { subjectId: 'testId' }
+      });
+      act(() => dl('TSV Long'));
+      expect(records).toBeDefined();
+      expect(mockUseDownload).toHaveBeenCalledTimes(1);
+
+      const [filename, getContentFn] = mockUseDownload.mock.calls[0];
+      expect(filename).toMatch('.tsv');
+      const tsvLongContents = getContentFn();
+      expect(tsvLongContents).toMatch('Date\tSubjectID\tValue\tVariable\r\n2025-04-30\ttestId\tabc\tsomeValue');
+    });
+  });
+  describe('JSON', () => {
+    it('Should download', async () => {
+      vi.spyOn(React, 'useEffect').mockImplementation((fn) => fn());
+      const { dl, records } = useInstrumentVisualization({
+        params: { subjectId: 'testId' }
+      });
+      act(() => dl('JSON'));
+      expect(records).toBeDefined();
+      expect(mockUseDownload).toHaveBeenCalledTimes(1);
+
+      const [filename, getContentFn] = mockUseDownload.mock.calls[0];
+      expect(filename).toMatch('.json');
+      const jsonContents = await getContentFn();
+      expect(jsonContents).toContain('"someValue": "abc"');
+      expect(jsonContents).toContain('"subjectID": "testId"');
     });
   });
 });
