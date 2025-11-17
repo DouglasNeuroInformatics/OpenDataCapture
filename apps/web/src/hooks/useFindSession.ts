8d545ad58426ed6134e4ca449b8e6375a05bddaa
david-roper <davidroper321@gmail.com>
2025-11-17 14:58:26 -0500

    feat: use schema parsing to confirm contents instead of casting it

--- apps/web/src/hooks/useFindSession.ts
+++ apps/web/src/hooks/useFindSession.ts
@@ -1,10 +1,11 @@
-import type { Session } from '@opendatacapture/schemas/session';
+import { type Session, $Session } from '@opendatacapture/schemas/session';
 import axios from 'axios';
 
 export const sessionInfo = async (sessionId: string): Promise<null | Session> => {
   try {
     const response = await axios.get(`/v1/sessions/${sessionId}`);
-    return response.data ? (response.data as Session) : null;
+    const parsedResult = $Session.safeParse(response.data);
+    return parsedResult.success ? parsedResult.data : null;
   } catch (error) {
     console.error('Error fetching session:', error);
     throw error;
