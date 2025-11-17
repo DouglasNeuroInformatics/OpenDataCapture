ec36cf5d20033a9f83e2719a2fc40430da3acebc
david-roper <davidroper321@gmail.com>
2025-11-17 14:58:26 -0500

    feat: use schema parsing to confirm contents instead of casting it

--- apps/web/src/hooks/useFindUser.ts
+++ apps/web/src/hooks/useFindUser.ts
@@ -1,10 +1,11 @@
-import type { User } from '@opendatacapture/schemas/user';
+import { type User, $User } from '@opendatacapture/schemas/user';
 import axios from 'axios';
 
 export const userInfo = async (userId: string): Promise<null | User> => {
   try {
     const response = await axios.get(`/v1/users/${userId}`);
-    return response.data ? (response.data as User) : null;
+    const parsedResult = $User.safeParse(response.data);
+    return parsedResult.success ? parsedResult.data : null;
   } catch (error) {
     console.error('Error fetching user:', error);
     throw error;
