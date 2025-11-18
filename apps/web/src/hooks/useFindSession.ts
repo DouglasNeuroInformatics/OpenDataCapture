182463a576cf58608092731b924fc0e01e027c02
David Roper <davidroper321@gmail.com>
2025-11-18 14:35:32 -0500

    feat: fix not finding user id issue by making subject inclusion optional

--- apps/web/src/hooks/useFindSession.ts
+++ apps/web/src/hooks/useFindSession.ts
@@ -5,8 +5,8 @@ import axios from 'axios';
 export const sessionInfo = async (sessionId: string): Promise<null | Session> => {
   try {
     const response = await axios.get(`/v1/sessions/${sessionId}`);
-    const parsedResult = $Session.safeParse(response.data);
-    return parsedResult.success ? parsedResult.data : null;
+    const parsedResult = await $Session.parseAsync(response.data);
+    return parsedResult;
   } catch (error) {
     console.error('Error fetching session:', error);
     throw error;
