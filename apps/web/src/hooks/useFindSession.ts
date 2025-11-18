aaf44646c8f1ed4a7f366d5b36dd50702fbb23a9
David Roper <davidroper321@gmail.com>
2025-11-18 14:44:59 -0500

    chore: revert session schema and parse change

--- apps/web/src/hooks/useFindSession.ts
+++ apps/web/src/hooks/useFindSession.ts
@@ -1,12 +1,13 @@
-import { $Session } from '@opendatacapture/schemas/session';
 import type { Session } from '@opendatacapture/schemas/session';
 import axios from 'axios';
 
 export const sessionInfo = async (sessionId: string): Promise<null | Session> => {
   try {
     const response = await axios.get(`/v1/sessions/${sessionId}`);
-    const parsedResult = await $Session.parseAsync(response.data);
-    return parsedResult;
+    if (!response.data) {
+      throw new Error('Session data does not exist');
+    }
+    return response.data as Session;
   } catch (error) {
     console.error('Error fetching session:', error);
     throw error;
--- packages/schemas/src/session/session.ts
+++ packages/schemas/src/session/session.ts
@@ -10,7 +10,7 @@ export type Session = z.infer<typeof $Session>;
 export const $Session = $BaseModel.extend({
   date: z.coerce.date(),
   groupId: z.string().nullable(),
-  subject: $Subject.optional(),
+  subject: $Subject,
   subjectId: z.string(),
   type: $SessionType,
   userId: z.string().nullish()
