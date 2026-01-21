bb8b8b609039d8496c8ebe7475932b152c9fe987
joshunrau <contact@joshuaunrau.com>
2026-01-21 16:14:25 -0500

    update SetupPage

--- testing/e2e/src/pages/setup.page.ts
+++ testing/e2e/src/pages/setup.page.ts
@@ -4,7 +4,6 @@ import type { Locator, Page } from '@playwright/test';
 import { RootPage } from './__root.page';
 
 export class SetupPage extends RootPage {
-  protected readonly defaultUrl = '/setup';
   readonly setupForm: Locator;
 
   constructor(page: Page) {
