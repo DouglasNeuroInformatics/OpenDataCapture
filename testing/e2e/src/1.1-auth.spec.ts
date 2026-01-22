57e2249d32607c5632e80c5b31589ca0fafca2c2
joshunrau <contact@joshuaunrau.com>
2026-01-22 11:52:14 -0500

    update auth spec

--- testing/e2e/src/1.1-auth.spec.ts
+++ testing/e2e/src/1.1-auth.spec.ts
@@ -1,10 +1,18 @@
+import { users } from './helpers/data';
 import { expect, test } from './helpers/fixtures';
-
-// no need to test the actual login here as it is tested on every other page
 
 test.describe('redirects', () => {
   test('should redirect to login page from the index page', async ({ page }) => {
     await page.goto('/');
     await expect(page).toHaveURL('/auth/login');
+  });
+});
+
+test.describe('login page', () => {
+  test('should allow logging in', async ({ getPageModel, getProjectMetadata }) => {
+    const loginPage = await getPageModel('/auth/login');
+    const credentials = users[getProjectMetadata('browserTarget')];
+    await loginPage.fillLoginForm(credentials);
+    await loginPage.expect.toHaveURL('/dashboard');
   });
 });
