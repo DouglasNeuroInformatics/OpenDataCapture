9af3c6f6ff91d485ddf9a5f4be00bd65d5081e85
David Roper <davidroper321@gmail.com>
2026-01-27 16:00:44 -0500

    test: additions to start session form tests

--- testing/e2e/src/pages/start-session.page.ts
+++ testing/e2e/src/pages/start-session.page.ts
@@ -4,11 +4,22 @@ import { AppPage } from './_app.page';
 
 export class StartSessionPage extends AppPage {
   readonly pageHeader: Locator;
-  readonly subjectIdentificationInput: Locator;
+  readonly sessionForm: Locator;
 
   constructor(page: Page) {
     super(page);
     this.pageHeader = page.getByTestId('page-header');
-    this.subjectIdentificationInput = page.getByRole('textbox', { name: 'subjectFirstName' });
+    this.sessionForm = page.getByTestId('start-session-form');
+  }
+
+  async fillSessionForm(firstName: string) {
+    await this.sessionForm.getByLabel('subjectId').fill(firstName);
+  }
+
+  async selectIdentificationMethod(methodName: string) {
+    const methodTrigger = this.sessionForm.getByRole('combobox', { name: /identification method/i });
+    await methodTrigger.click();
+    // The options are usually rendered in a portal at the end of the body
+    await methodTrigger.getByRole('option', { name: methodName }).click();
   }
 }
