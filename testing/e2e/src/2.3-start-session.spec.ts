dc3add3992d689392defa6687922197d279ed260
David Roper <davidroper321@gmail.com>
2026-01-28 15:40:05 -0500

    test: add test ids to disclaimer, test local storage in session test

--- testing/e2e/src/2.3-start-session.spec.ts
+++ testing/e2e/src/2.3-start-session.spec.ts
@@ -1,6 +1,13 @@
 import { expect, test } from './helpers/fixtures';
 
 test.describe('start session', () => {
+  test.beforeAll(async ({ page }) => {
+    // Set disclaimer as accepted for persistence across tests
+    await page.addInitScript(() => {
+      localStorage.setItem('app', JSON.stringify({ state: { isDisclaimerAccepted: true }, version: 1 }));
+    });
+  });
+
   test('should display the start session form header', async ({ getPageModel }) => {
     const startSessionPage = await getPageModel('/session/start-session');
     await expect(startSessionPage.pageHeader).toBeVisible();
@@ -10,6 +17,7 @@ test.describe('start session', () => {
 
   test('should fill subject identification input', async ({ getPageModel }) => {
     const startSessionPage = await getPageModel('/session/start-session');
+
     await startSessionPage.sessionForm.waitFor({ state: 'visible' });
     const formType = startSessionPage.sessionForm.getByTestId('subjectIdentificationMethod-select-trigger');
     await formType.click();
@@ -19,8 +27,9 @@ test.describe('start session', () => {
     // await startSessionPage.selectIdentificationMethod('Personal Information')
     await expect(formType).toHaveText('Personal Information');
 
-    const identifier = startSessionPage.sessionForm.locator('#subjectFirstName');
-    await expect(identifier).toHaveText('');
+    // const identifier = startSessionPage.sessionForm.locator('#subjectFirstName');
+    // await identifier.fill('john')
+    // await expect(identifier).toHaveText('john');
     // await startSessionPage.fillSessionForm('john')
 
     // await startSessionPage.sessionForm.fill('John');
