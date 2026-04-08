5b61679145a9d6fd4923f7777789c67868b3742b
David Roper <davidroper321@gmail.com>
2026-04-08 13:41:01 -0400

    feat: add page header to preferencs page

--- apps/web/src/routes/_app/user.tsx
+++ apps/web/src/routes/_app/user.tsx
@@ -1,11 +1,14 @@
 import { Heading } from '@douglasneuroinformatics/libui/components';
+import { useTranslation } from '@douglasneuroinformatics/libui/hooks';
 import { createFileRoute } from '@tanstack/react-router';
 
+import { PageHeader } from '@/components/PageHeader';
 import { UserIcon } from '@/components/UserIcon';
 import { useAppStore } from '@/store';
 
 const RouteComponent = () => {
   const currentUser = useAppStore((store) => store.currentUser);
+  const { t } = useTranslation();
 
   let fullName: string;
   if (currentUser?.firstName && currentUser.lastName) {
@@ -17,10 +20,20 @@ const RouteComponent = () => {
   }
 
   return (
-    <div className="mt-4 flex flex-col items-center justify-center">
-      <UserIcon className="h-16 w-16" />
-      <Heading variant="h2">{fullName}</Heading>
-      <p className="text-sm">{currentUser?.username ?? fullName}</p>
+    <div>
+      <PageHeader>
+        <Heading className="text-center" variant="h2">
+          {t({
+            en: 'User Info',
+            fr: 'Informations utilisateur'
+          })}
+        </Heading>
+      </PageHeader>
+      <div className="mt-4 flex flex-col items-center justify-center">
+        <UserIcon className="h-16 w-16" />
+        <Heading variant="h2">{fullName}</Heading>
+        <p className="text-sm">{currentUser?.username ?? fullName}</p>
+      </div>
     </div>
   );
 };
