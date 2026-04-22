e31c8c457f8b1d65c9f6d4b8af606e10f9599dac
joshunrau <contact@joshuaunrau.com>
2026-04-22 10:40:40 -0400

    remove test file where there is no test

--- packages/runtime-meta/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { mergeConfig } from 'vitest/config';
-
-import baseConfig from '../../vitest.config';
-
-export default mergeConfig(baseConfig, {
-  test: {
-    root: import.meta.dirname
-  }
-});
